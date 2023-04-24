077c2e1956b24468bb7fa56bbda1621739b9f0b5
Daniel Lubarov <daniel@lubarov.com>
2023-04-23 18:28:56 -0700

    PseudoCompressionFunction

--- symmetric/src/compression.rs
+++ symmetric/src/compression.rs
@@ -1,10 +1,13 @@
 use crate::hasher::IterHasher;
 use core::marker::PhantomData;
 
-/// An `n`-to-1 compression function.
-pub trait CompressionFunction<T, const N: usize> {
+/// An `n`-to-1 compression function, like `CompressionFunction`, except that TODO.
+pub trait PseudoCompressionFunction<T, const N: usize> {
     fn compress(&self, input: [T; N]) -> T;
 }
+
+/// An `n`-to-1 compression function.
+pub trait CompressionFunction<T, const N: usize>: PseudoCompressionFunction<T, N> {}
 
 pub struct CompressionFunctionFromIterHasher<T, H, const N: usize, const CHUNK: usize>
 where
@@ -14,7 +17,7 @@ where
     hasher: H,
 }
 
-impl<T, H, const N: usize, const CHUNK: usize> CompressionFunction<[T; CHUNK], N>
+impl<T, H, const N: usize, const CHUNK: usize> PseudoCompressionFunction<[T; CHUNK], N>
     for CompressionFunctionFromIterHasher<T, H, N, CHUNK>
 where
     H: IterHasher<T, [T; CHUNK]>,
@@ -22,4 +25,11 @@ where
     fn compress(&self, input: [[T; CHUNK]; N]) -> [T; CHUNK] {
         self.hasher.hash_iter(input.into_iter().flatten())
     }
+}
+
+impl<T, H, const N: usize, const CHUNK: usize> CompressionFunction<[T; CHUNK], N>
+    for CompressionFunctionFromIterHasher<T, H, N, CHUNK>
+where
+    H: IterHasher<T, [T; CHUNK]>,
+{
 }
