a30a853fc1a21a92bbf471d0c86f78490d3e91f8
Nicholas Ward <npward@berkeley.edu>
2023-05-17 13:22:01 -0700

    Challenger impls

--- challenger/src/duplex_challenger.rs
+++ challenger/src/duplex_challenger.rs
@@ -1,8 +1,59 @@
 use core::marker::PhantomData;
+use alloc::vec;
+use alloc::vec::Vec;
 use p3_field::Field;
 use p3_symmetric::permutation::ArrayPermutation;
 
+use crate::Challenger;
+
 pub struct DuplexChallenger<F: Field, P: ArrayPermutation<F, WIDTH>, const WIDTH: usize> {
-    _permutation: P,
+    sponge_state: [F; WIDTH],
+    input_buffer: Vec<F>,
+    output_buffer: Vec<F>,
+    permutation: P,
     _phantom_f: PhantomData<F>,
+}
+
+impl<F: Field, P: ArrayPermutation<F, WIDTH>, const WIDTH: usize> DuplexChallenger<F, P, WIDTH> {
+    pub fn new(permutation: P) -> Self {
+        Self {
+            sponge_state: [F::ZERO; WIDTH],
+            input_buffer: vec![],
+            output_buffer: vec![],
+            permutation,
+            _phantom_f: PhantomData,
+        }
+    }
+
+    fn duplexing(&mut self) {
+        assert!(self.input_buffer.len() <= WIDTH);
+
+        // Overwrite the first r elements with the inputs.
+        for (i, val) in self.input_buffer.drain(..).enumerate() {
+            self.sponge_state[i] = val;
+        }
+
+        // Apply the permutation.
+        self.sponge_state = self.permutation.permute(self.sponge_state);
+
+        self.output_buffer.clear();
+        self.output_buffer.extend(self.sponge_state);
+    }
+}
+
+impl<F: Field, P: ArrayPermutation<F, WIDTH>, const WIDTH: usize> Challenger<F> for DuplexChallenger<F, P, WIDTH> {
+    fn observe_element(&mut self, element: F) {
+        // Any buffered output is now invalid.
+        self.output_buffer.clear();
+
+        self.input_buffer.push(element);
+    }
+
+    fn random_element(&mut self) -> F {
+        if self.output_buffer.is_empty() {
+            self.duplexing();
+        }
+
+        self.output_buffer.pop().expect("Output buffer should be non-empty")
+    }
 }
