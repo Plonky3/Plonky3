5561ba8723e981b89594d06db6d2f69cf2ed0e0f
Daniel Lubarov <daniel@lubarov.com>
2023-05-19 22:51:42 -0700

    VirtualPairCol

--- air/src/virtual_column.rs
+++ air/src/virtual_column.rs
@@ -3,21 +3,44 @@ use alloc::vec::Vec;
 use core::ops::Mul;
 use p3_field::{AbstractField, Field};
 
-/// An affine function over columns.
-pub struct VirtualColumn<F: Field> {
-    column_weights: Vec<(usize, F)>,
+/// An affine function over columns in a PAIR.
+pub struct VirtualPairCol<F: Field> {
+    column_weights: Vec<(PairCol, F)>,
     constant: F,
 }
 
-impl<F: Field> VirtualColumn<F> {
-    pub fn single(column: usize) -> Self {
+/// A column in a PAIR, i.e. either a preprocessed column or a main trace column.
+pub enum PairCol {
+    Preprocessed(usize),
+    Main(usize),
+}
+
+impl PairCol {
+    fn get<T: Copy>(&self, preprocessed: &[T], main: &[T]) -> T {
+        match self {
+            PairCol::Preprocessed(i) => preprocessed[*i],
+            PairCol::Main(i) => main[*i],
+        }
+    }
+}
+
+impl<F: Field> VirtualPairCol<F> {
+    pub fn single(column: PairCol) -> Self {
         Self {
             column_weights: vec![(column, F::ONE)],
             constant: F::ZERO,
         }
     }
 
-    pub fn apply<Exp, Var>(&self, row: &[Var]) -> Exp
+    pub fn single_preprocessed(column: usize) -> Self {
+        Self::single(PairCol::Preprocessed(column))
+    }
+
+    pub fn single_main(column: usize) -> Self {
+        Self::single(PairCol::Main(column))
+    }
+
+    pub fn apply<Exp, Var>(&self, preprocessed: &[Var], main: &[Var]) -> Exp
     where
         F: Into<Exp>,
         Exp: AbstractField + Mul<F, Output = Exp>,
@@ -25,7 +48,7 @@ impl<F: Field> VirtualColumn<F> {
     {
         let mut result = self.constant.into();
         for (column, weight) in self.column_weights.iter() {
-            result += row[*column].into() * *weight;
+            result += column.get(preprocessed, main).into() * *weight;
         }
         result
     }
