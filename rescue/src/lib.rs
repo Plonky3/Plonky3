b948ced7cca575c551cebd6ed28761362b12656d
Nicholas Ward <npward@berkeley.edu>
2023-06-23 09:41:31 -0700

    addressed comments

--- rescue/src/lib.rs
+++ rescue/src/lib.rs
@@ -1,9 +1,6 @@
-// use std::marker::PhantomData;
-
-// use p3_field::{Field, PrimeField, PrimeField64};
-// use p3_symmetric::permutation::{MDSPermutation, CryptographicPermutation, ArrayPermutation};
-
 mod inverse_sbox;
 mod mds_matrix_naive;
 mod rescue;
 mod util;
+
+pub use rescue::*;
