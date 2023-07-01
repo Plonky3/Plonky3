c557cc49b4aaf24422d930f1a2d56647b8557c51
jorgeantonio21 <matroid@outlook.com>
2023-07-01 18:16:05 +0100

    test coverage for duplex challenger functionality

--- challenger/src/duplex_challenger.rs
+++ challenger/src/duplex_challenger.rs
@@ -67,3 +67,75 @@ impl<F: Field, P: ArrayPermutation<F, WIDTH>, const WIDTH: usize> Challenger<F>
             .expect("Output buffer should be non-empty")
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use p3_field::AbstractField;
+    use p3_goldilocks::Goldilocks;
+    use p3_symmetric::permutation::CryptographicPermutation;
+    use rand::seq::{index::sample, SliceRandom};
+
+    use super::*;
+
+    type ByteArray = [F; 32];
+    type F = Goldilocks;
+
+    const WIDTH: usize = 32;
+    struct TestPermutation {}
+
+    impl CryptographicPermutation<ByteArray> for TestPermutation {
+        fn permute(&self, input: ByteArray) -> ByteArray {
+            let mut output = [F::ZERO; 32];
+            let mut rng = rand::thread_rng();
+            let mut shuffled_indexes: [usize; 32] =
+                (0..32).collect::<Vec<usize>>().try_into().unwrap();
+            shuffled_indexes.shuffle(&mut rng);
+            (0..32).for_each(|i| output[shuffled_indexes[i]] = input[i]);
+            output
+        }
+
+        fn permute_mut(&self, input: &mut ByteArray) {
+            let mut rng = rand::thread_rng();
+            let mut shuffled_indexes: [usize; 32] =
+                (0..32).collect::<Vec<usize>>().try_into().unwrap();
+            shuffled_indexes.shuffle(&mut rng);
+            (0..32).for_each(|i| input[i] = input[shuffled_indexes[i]]);
+        }
+    }
+
+    impl ArrayPermutation<F, WIDTH> for TestPermutation {}
+
+    #[test]
+    fn it_works_duplexing() {
+        let mut rng = rand::thread_rng();
+        let permutation = TestPermutation {};
+        let mut duplex_challenger = DuplexChallenger::new(permutation);
+        let sample_range = sample(&mut rng, u32::MAX as usize, 31);
+        let mut sample_iter = sample_range.iter();
+
+        (0..31).for_each(|_| {
+            let element = sample_iter.next().unwrap();
+            duplex_challenger.observe_element(Goldilocks::from_canonical_usize(element))
+        });
+
+        let input_buffer = duplex_challenger.input_buffer.clone();
+
+        assert_eq!(duplex_challenger.input_buffer.len(), 31);
+        duplex_challenger.duplexing();
+
+        assert_eq!(duplex_challenger.input_buffer.len(), 0);
+        assert_eq!(duplex_challenger.output_buffer.len(), 32);
+        for i in 0..31 {
+            assert!(
+                input_buffer.contains(&duplex_challenger.output_buffer[i])
+                    || duplex_challenger.output_buffer[i] == F::ZERO
+            );
+        }
+
+        assert_eq!(duplex_challenger.sponge_state.len(), 32);
+        assert_eq!(
+            duplex_challenger.sponge_state[..],
+            duplex_challenger.output_buffer[..]
+        );
+    }
+}
