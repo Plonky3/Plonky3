f0bb9af3d11ce5e72253d4a374545251442df6da
Nicholas Ward <npward@berkeley.edu>
2023-07-31 13:19:17 -0700

    fmt

--- monolith/src/lib.rs
+++ monolith/src/lib.rs
@@ -4,10 +4,9 @@
 
 extern crate alloc;
 
-use core::u64;
-
 use alloc::sync::Arc;
 use alloc::vec::Vec;
+use core::u64;
 
 use p3_field::{AbstractField, PrimeField32};
 use p3_mersenne_31::Mersenne31;
@@ -32,9 +31,19 @@ impl CryptographicPermutation<[Mersenne31; 16]> for Monolith31 {
     fn permute(&self, input: [Mersenne31; 16]) -> [Mersenne31; 16] {
         let m = ZKHashMonolith31::new(&Arc::new(Monolith31Params::new()));
 
-        let mut input_f: [F31; 16] = input.iter().map(m31_to_f31).collect::<Vec<_>>().try_into().unwrap();
+        let mut input_f: [F31; 16] = input
+            .iter()
+            .map(m31_to_f31)
+            .collect::<Vec<_>>()
+            .try_into()
+            .unwrap();
         m.permutation_u64(&mut input_f);
-        input_f.iter().map(f31_to_m31).collect::<Vec<_>>().try_into().unwrap()
+        input_f
+            .iter()
+            .map(f31_to_m31)
+            .collect::<Vec<_>>()
+            .try_into()
+            .unwrap()
     }
 }
 
@@ -46,10 +55,25 @@ impl PseudoCompressionFunction<[Mersenne31; 8], 2> for Monolith31Hash {
     fn compress(&self, input: [[Mersenne31; 8]; 2]) -> [Mersenne31; 8] {
         let m = ZKHashMonolith31::new(&Arc::new(Monolith31Params::new()));
 
-        let input_1 = input[0].iter().map(m31_to_f31).collect::<Vec<_>>().try_into().unwrap();
-        let input_2 = input[1].iter().map(m31_to_f31).collect::<Vec<_>>().try_into().unwrap();
+        let input_1 = input[0]
+            .iter()
+            .map(m31_to_f31)
+            .collect::<Vec<_>>()
+            .try_into()
+            .unwrap();
+        let input_2 = input[1]
+            .iter()
+            .map(m31_to_f31)
+            .collect::<Vec<_>>()
+            .try_into()
+            .unwrap();
         let output = m.hash(&input_1, &input_2);
 
-        output.iter().map(f31_to_m31).collect::<Vec<_>>().try_into().unwrap()
+        output
+            .iter()
+            .map(f31_to_m31)
+            .collect::<Vec<_>>()
+            .try_into()
+            .unwrap()
     }
 }
