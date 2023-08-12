a90b9877025febaf3bd292010599bbc332056191
Daniel Lubarov <daniel@lubarov.com>
2023-08-12 13:20:47 -0700

    Fix out-of-range inputs

--- baby-bear/benches/inverse.rs
+++ baby-bear/benches/inverse.rs
@@ -1,13 +1,13 @@
 use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
 use p3_baby_bear::BabyBear;
-use p3_field::{AbstractField, Field};
+use p3_field::Field;
 
 type F = BabyBear;
 
 fn try_inverse(c: &mut Criterion) {
     c.bench_function("try_inverse", |b| {
         b.iter_batched(
-            || F::from_canonical_u64(rand::random::<u64>()),
+            || rand::random::<F>(),
             |x| x.try_inverse(),
             BatchSize::SmallInput,
         )
--- goldilocks/benches/inverse.rs
+++ goldilocks/benches/inverse.rs
@@ -1,5 +1,5 @@
 use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
-use p3_field::{AbstractField, Field};
+use p3_field::Field;
 use p3_goldilocks::Goldilocks;
 
 type F = Goldilocks;
@@ -7,7 +7,7 @@ type F = Goldilocks;
 fn try_inverse(c: &mut Criterion) {
     c.bench_function("try_inverse", |b| {
         b.iter_batched(
-            || F::from_canonical_u64(rand::random::<u64>()),
+            || rand::random::<F>(),
             |x| x.try_inverse(),
             BatchSize::SmallInput,
         )
