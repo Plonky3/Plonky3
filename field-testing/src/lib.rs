27d964db980647ca31936aa704f5f2b5baf5d664
Hamish Ivey-Law <hamish@ivey-law.name>
2023-08-18 16:22:59 +0200

    More thorough testing of field inverses (from #53).

--- field-testing/src/lib.rs
+++ field-testing/src/lib.rs
@@ -5,12 +5,24 @@
 extern crate alloc;
 
 use p3_field::Field;
+use rand::distributions::{Distribution, Standard};
+use rand::Rng;
 
-pub fn test_inverse<F: Field>() {
+pub fn test_inverse<F: Field>()
+where
+    Standard: Distribution<F>,
+{
     assert_eq!(None, F::ZERO.try_inverse());
 
     assert_eq!(Some(F::ONE), F::ONE.try_inverse());
 
-    let five = F::from_canonical_u8(5);
-    assert_eq!(F::ONE, five.inverse() * five);
+    let mut rng = rand::thread_rng();
+    for _ in 0..1000 {
+        let x = rng.gen::<F>();
+        if !x.is_zero() && !x.is_one() {
+            let z = x.inverse();
+            assert_ne!(x, z);
+            assert_eq!(x * z, F::ONE);
+        }
+    }
 }
