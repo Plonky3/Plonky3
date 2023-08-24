5150e85c2af7f696adb12926c7ae0b2e866eef57
Daniel Lubarov <daniel@lubarov.com>
2023-08-23 19:58:39 -0700

    Fixes

--- poseidon/benches/poseidon.rs
+++ poseidon/benches/poseidon.rs
@@ -1,7 +1,11 @@
 use std::any::type_name;
 
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+use p3_baby_bear::BabyBear;
 use p3_field::PrimeField64;
+use p3_goldilocks::Goldilocks;
+use p3_mds::babybear::MdsMatrixBabyBear;
+use p3_mds::goldilocks::MdsMatrixGoldilocks;
 use p3_mds::mersenne31::MdsMatrixMersenne31;
 use p3_mds::MdsPermutation;
 use p3_mersenne_31::Mersenne31;
@@ -11,7 +15,16 @@ use rand::distributions::{Distribution, Standard};
 use rand::thread_rng;
 
 fn bench_poseidon(c: &mut Criterion) {
-    poseidon::<Mersenne31, MdsMatrixMersenne31, 12, 5>(c);
+    poseidon::<BabyBear, MdsMatrixBabyBear, 16, 7>(c);
+    poseidon::<BabyBear, MdsMatrixBabyBear, 24, 7>(c);
+    poseidon::<BabyBear, MdsMatrixBabyBear, 32, 7>(c);
+
+    poseidon::<Goldilocks, MdsMatrixGoldilocks, 8, 7>(c);
+    poseidon::<Goldilocks, MdsMatrixGoldilocks, 12, 7>(c);
+    poseidon::<Goldilocks, MdsMatrixGoldilocks, 16, 7>(c);
+
+    poseidon::<Mersenne31, MdsMatrixMersenne31, 16, 5>(c);
+    poseidon::<Mersenne31, MdsMatrixMersenne31, 32, 5>(c);
 }
 
 fn poseidon<F, Mds, const WIDTH: usize, const ALPHA: u64>(c: &mut Criterion)
@@ -23,7 +36,7 @@ where
     let mut rng = thread_rng();
     let mds = Mds::default();
 
-    // TODO: Should be field specific
+    // TODO: Should be calculated for the particular field, width and ALPHA.
     let half_num_full_rounds = 4;
     let num_partial_rounds = 22;
 
@@ -34,7 +47,7 @@ where
         &mut rng,
     );
     let input = [F::ZERO; WIDTH];
-    let name = format!("poseidon::<{}, {}>", type_name::<F>(), ALPHA,);
+    let name = format!("poseidon::<{}, {}>", type_name::<F>(), ALPHA);
     let id = BenchmarkId::new(name, WIDTH);
     c.bench_with_input(id, &input, |b, &input| b.iter(|| poseidon.permute(input)));
 }
--- rescue/benches/rescue.rs
+++ rescue/benches/rescue.rs
@@ -1,7 +1,11 @@
 use std::any::type_name;
 
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+use p3_baby_bear::BabyBear;
 use p3_field::PrimeField64;
+use p3_goldilocks::Goldilocks;
+use p3_mds::babybear::MdsMatrixBabyBear;
+use p3_mds::goldilocks::MdsMatrixGoldilocks;
 use p3_mds::mersenne31::MdsMatrixMersenne31;
 use p3_mds::MdsPermutation;
 use p3_mersenne_31::Mersenne31;
@@ -11,7 +15,16 @@ use rand::distributions::{Distribution, Standard};
 use rand::{thread_rng, Rng};
 
 fn bench_rescue(c: &mut Criterion) {
-    rescue::<Mersenne31, MdsMatrixMersenne31, BasicInverseSboxLayer, 12, 5>(c);
+    rescue::<BabyBear, MdsMatrixBabyBear, BasicInverseSboxLayer, 16, 7>(c);
+    rescue::<BabyBear, MdsMatrixBabyBear, BasicInverseSboxLayer, 24, 7>(c);
+    rescue::<BabyBear, MdsMatrixBabyBear, BasicInverseSboxLayer, 32, 7>(c);
+
+    rescue::<Goldilocks, MdsMatrixGoldilocks, BasicInverseSboxLayer, 8, 7>(c);
+    rescue::<Goldilocks, MdsMatrixGoldilocks, BasicInverseSboxLayer, 12, 7>(c);
+    rescue::<Goldilocks, MdsMatrixGoldilocks, BasicInverseSboxLayer, 16, 7>(c);
+
+    rescue::<Mersenne31, MdsMatrixMersenne31, BasicInverseSboxLayer, 16, 5>(c);
+    rescue::<Mersenne31, MdsMatrixMersenne31, BasicInverseSboxLayer, 32, 5>(c);
 }
 
 fn rescue<F, Mds, Isl, const WIDTH: usize, const ALPHA: u64>(c: &mut Criterion)
@@ -21,8 +34,9 @@ where
     Mds: MdsPermutation<F, WIDTH> + Default,
     Isl: InverseSboxLayer<F, WIDTH, ALPHA> + Default,
 {
-    // TODO: Should be field specific?
-    const NUM_ROUNDS: usize = 7;
+    // 8 rounds seems to work for the configs we use in practice. For benchmarking purposes we will
+    // assume it suffices; for real usage the Sage calculation in the paper should be used.
+    const NUM_ROUNDS: usize = 8;
 
     let rng = thread_rng();
     let num_constants = 2 * WIDTH * NUM_ROUNDS;
@@ -31,7 +45,7 @@ where
     let isl = Isl::default();
     let rescue = Rescue::<F, Mds, Isl, WIDTH, ALPHA>::new(NUM_ROUNDS, round_constants, mds, isl);
     let input = [F::ZERO; WIDTH];
-    let name = format!("rescue::<{}, {}>", type_name::<F>(), ALPHA,);
+    let name = format!("rescue::<{}, {}>", type_name::<F>(), ALPHA);
     let id = BenchmarkId::new(name, WIDTH);
     c.bench_with_input(id, &input, |b, &input| b.iter(|| rescue.permute(input)));
 }
