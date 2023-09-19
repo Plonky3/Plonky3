b563d4212605accf823bd93a57d49bcb261a4d32
liuky <stephenkobylky@gmail.com>
2023-09-19 14:32:07 +0900

    quadratic extension field for goldlilocks

---
+++ goldilocks/src/extension.rs
@@ -0,0 +1,27 @@
+use p3_field::extension::BinomiallyExtendable;
+
+use crate::Goldilocks;
+
+impl BinomiallyExtendable<2> for Goldilocks {
+    // Verifiable in Sage with
+    // `R.<x> = GF(p)[]; assert (x^2 - 7).is_irreducible()`.
+    const W: Self = Self::new(7);
+
+    // DTH_ROOT = W^((ORDER - 1)/2)
+    const DTH_ROOT: Self = Self::new(18446744069414584320);
+
+    fn ext_multiplicative_group_generator() -> [Self; 2] {
+        [
+            Self::new(18081566051660590251),
+            Self::new(16121475356294670766),
+        ]
+    }
+}
+
+#[cfg(test)]
+mod test_quadratic_extension {
+
+    use p3_field_testing::test_field;
+
+    test_field!(p3_field::extension::quadratic::QuadraticBef<crate::Goldilocks>);
+}
