188fbc3391601b3a5ee0ae145dd2ac6e3a6d0e42
John Guibas <jtguibas@gmail.com>
2023-09-18 21:53:33 -0700

    feat: poseidon2 (#104)
    * feat: poseidon2
    
    * feat: poseidon2
    
    * feat: poseidon2
    
    * feat: poseidon2
    
    * feat: poseidon2

---
+++ poseidon2/benches/poseidon2.rs
@@ -0,0 +1,55 @@
+use std::any::type_name;
+
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+use p3_baby_bear::BabyBear;
+use p3_field::PrimeField64;
+use p3_goldilocks::Goldilocks;
+use p3_mds::babybear::MdsMatrixBabyBear;
+use p3_mds::goldilocks::MdsMatrixGoldilocks;
+use p3_mds::MdsPermutation;
+use p3_poseidon2::{
+    DiffusionMatrixBabybear, DiffusionMatrixGoldilocks, DiffusionPermutation, Poseidon2,
+};
+use p3_symmetric::permutation::CryptographicPermutation;
+use rand::distributions::{Distribution, Standard};
+use rand::thread_rng;
+
+fn bench_poseidon2(c: &mut Criterion) {
+    poseidon2::<BabyBear, MdsMatrixBabyBear, DiffusionMatrixBabybear, 16, 7>(c);
+    poseidon2::<BabyBear, MdsMatrixBabyBear, DiffusionMatrixBabybear, 24, 7>(c);
+
+    poseidon2::<Goldilocks, MdsMatrixGoldilocks, DiffusionMatrixGoldilocks, 8, 7>(c);
+    poseidon2::<Goldilocks, MdsMatrixGoldilocks, DiffusionMatrixGoldilocks, 12, 7>(c);
+    poseidon2::<Goldilocks, MdsMatrixGoldilocks, DiffusionMatrixGoldilocks, 16, 7>(c);
+}
+
+fn poseidon2<F, Mds, Diffusion, const WIDTH: usize, const D: u64>(c: &mut Criterion)
+where
+    F: PrimeField64,
+    Standard: Distribution<F>,
+    Mds: MdsPermutation<F, WIDTH> + Default,
+    Diffusion: DiffusionPermutation<F, WIDTH> + Default,
+{
+    let mut rng = thread_rng();
+    let external_mds = Mds::default();
+    let internal_mds = Diffusion::default();
+
+    // TODO: Should be calculated for the particular field, width and ALPHA.
+    let rounds_f = 8;
+    let rounds_p = 22;
+
+    let poseidon = Poseidon2::<F, Mds, Diffusion, WIDTH, D>::new_from_rng(
+        rounds_f,
+        rounds_p,
+        external_mds,
+        internal_mds,
+        &mut rng,
+    );
+    let input = [F::ZERO; WIDTH];
+    let name = format!("poseidon2::<{}, {}>", type_name::<F>(), D);
+    let id = BenchmarkId::new(name, WIDTH);
+    c.bench_with_input(id, &input, |b, &input| b.iter(|| poseidon.permute(input)));
+}
+
+criterion_group!(benches, bench_poseidon2);
+criterion_main!(benches);
---
+++ poseidon2/src/lib.rs
@@ -0,0 +1,178 @@
+//! The Poseidon2 permutation.
+//!
+//! This implementation was based upon the following resources:
+//! - https://github.com/HorizenLabs/poseidon2/blob/main/plain_implementations/src/poseidon2/poseidon2.rs
+//! - https://eprint.iacr.org/2023/323.pdf
+
+#![no_std]
+
+mod babybear;
+mod diffusion;
+mod goldilocks;
+pub use babybear::DiffusionMatrixBabybear;
+pub use diffusion::DiffusionPermutation;
+pub use goldilocks::DiffusionMatrixGoldilocks;
+
+extern crate alloc;
+
+use alloc::borrow::ToOwned;
+use alloc::vec::Vec;
+
+use p3_field::Field;
+use p3_mds::MdsPermutation;
+use p3_symmetric::permutation::{ArrayPermutation, CryptographicPermutation};
+use rand::distributions::Standard;
+use rand::prelude::Distribution;
+use rand::Rng;
+
+/// The Poseidon2 permutation.
+#[derive(Clone)]
+pub struct Poseidon2<F, Mds, Diffusion, const WIDTH: usize, const D: u64>
+where
+    F: Field,
+    Mds: MdsPermutation<F, WIDTH>,
+    Diffusion: DiffusionPermutation<F, WIDTH>,
+{
+    /// The number of external rounds.
+    rounds_f: usize,
+
+    /// The number of internal rounds.
+    rounds_p: usize,
+
+    /// The round constants.
+    constants: Vec<[F; WIDTH]>,
+
+    /// The linear layer used in external rounds.
+    external_linear_layer: Mds,
+
+    /// The linear layer used in internal rounds (only needs diffusion property, not MDS).
+    internal_linear_layer: Diffusion,
+}
+
+impl<F, Mds, Diffusion, const WIDTH: usize, const D: u64> Poseidon2<F, Mds, Diffusion, WIDTH, D>
+where
+    F: Field,
+    Mds: MdsPermutation<F, WIDTH>,
+    Diffusion: DiffusionPermutation<F, WIDTH>,
+{
+    /// Create a new Poseidon2 configuration.
+    pub fn new(
+        rounds_f: usize,
+        rounds_p: usize,
+        constants: Vec<[F; WIDTH]>,
+        external_linear_layer: Mds,
+        internal_linear_layer: Diffusion,
+    ) -> Self {
+        Self {
+            rounds_f,
+            rounds_p,
+            constants,
+            external_linear_layer,
+            internal_linear_layer,
+        }
+    }
+
+    /// Create a new Poseidon2 configuration with random parameters.
+    pub fn new_from_rng<R: Rng>(
+        rounds_f: usize,
+        rounds_p: usize,
+        external_mds: Mds,
+        internal_mds: Diffusion,
+        rng: &mut R,
+    ) -> Self
+    where
+        Standard: Distribution<F>,
+    {
+        let mut constants = Vec::new();
+        let rounds = rounds_f + rounds_p;
+        for _ in 0..rounds {
+            let mut round_constant = [F::ZERO; WIDTH];
+            #[allow(clippy::needless_range_loop)]
+            for j in 0..WIDTH {
+                round_constant[j] = rng.sample(Standard);
+            }
+            constants.push(round_constant);
+        }
+
+        Self {
+            rounds_f,
+            rounds_p,
+            constants,
+            external_linear_layer: external_mds,
+            internal_linear_layer: internal_mds,
+        }
+    }
+
+    #[inline]
+    fn add_rc(&self, state: &[F], rc: &[F]) -> [F; WIDTH] {
+        let mut result = [F::ZERO; WIDTH];
+        for i in 0..WIDTH {
+            result[i] = state[i] + rc[i];
+        }
+        result
+    }
+
+    #[inline]
+    fn sbox_p(&self, input: &F) -> F {
+        input.exp_const_u64::<D>()
+    }
+
+    #[inline]
+    fn sbox(&self, state: &[F]) -> [F; WIDTH] {
+        let mut result = [F::ZERO; WIDTH];
+        for i in 0..WIDTH {
+            result[i] = self.sbox_p(&state[i]);
+        }
+        result
+    }
+}
+
+impl<F, Mds, Diffusion, const WIDTH: usize, const D: u64> CryptographicPermutation<[F; WIDTH]>
+    for Poseidon2<F, Mds, Diffusion, WIDTH, D>
+where
+    F: Field,
+    Mds: MdsPermutation<F, WIDTH>,
+    Diffusion: DiffusionPermutation<F, WIDTH>,
+{
+    fn permute(&self, state: [F; WIDTH]) -> [F; WIDTH] {
+        let mut state = state.to_owned();
+
+        // The initial linear layer.
+        self.external_linear_layer.permute_mut(&mut state);
+
+        // The first half of the external rounds.
+        let rounds = self.rounds_f + self.rounds_p;
+        let rounds_f_beggining = self.rounds_f / 2;
+        for r in 0..rounds_f_beggining {
+            state = self.add_rc(&state, &self.constants[r]);
+            state = self.sbox(&state);
+            self.external_linear_layer.permute_mut(&mut state);
+        }
+
+        // The internal rounds.
+        let p_end = rounds_f_beggining + self.rounds_p;
+        for r in self.rounds_f..p_end {
+            state[0] += self.constants[r][0];
+            state[0] = self.sbox_p(&state[0]);
+            self.internal_linear_layer.permute_mut(&mut state);
+        }
+
+        // The second half of the external rounds.
+        for r in p_end..rounds {
+            state = self.add_rc(&state, &self.constants[r]);
+            state = self.sbox(&state);
+            self.external_linear_layer.permute_mut(&mut state);
+        }
+
+        state
+    }
+}
+
+impl<F: Field, Mds, Diffusion, const T: usize, const D: u64> ArrayPermutation<F, T>
+    for Poseidon2<F, Mds, Diffusion, T, D>
+where
+    F: Field,
+    Mds: MdsPermutation<F, T>,
+    Diffusion: DiffusionPermutation<F, T>,
+{
+}
