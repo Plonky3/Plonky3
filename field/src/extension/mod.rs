11c809e2f73efe119a73b8e1d4f7a6d15815bdab
liuky <stephenkobylky@gmail.com>
2023-09-27 11:27:16 +0900

    generalize binomial extendable trait

--- field/src/extension/mod.rs
+++ field/src/extension/mod.rs
@@ -1,4 +1,5 @@
 use crate::field::Field;
+use crate::AbstractExtensionField;
 
 pub mod cubic;
 pub mod quadratic;
@@ -8,5 +9,48 @@ pub mod quadratic;
 /// such that the extension is `F[X]/(X^d-W)`.
 pub trait BinomiallyExtendable<const D: usize>: Field + Sized {
     const W: Self;
+
+    // DTH_ROOT = W^((n - 1)/D).
+    // n is the order of base field.
+    // Only works when exists k such that n = kD + 1.
+    const DTH_ROOT: Self;
+
     fn ext_multiplicative_group_generator() -> [Self; D];
+}
+
+pub trait Frobenius<F: BinomiallyExtendable<D>, const D: usize>:
+    Field + Sized + AbstractExtensionField<F>
+{
+    /// FrobeniusField automorphisms: x -> x^n, where n is the order of BaseField.
+    fn frobenius(&self) -> Self {
+        self.repeated_frobenius(1)
+    }
+
+    /// Repeated Frobenius automorphisms: x -> x^(n^count).
+    ///
+    /// Follows precomputation suggestion in Section 11.3.3 of the
+    /// Handbook of Elliptic and Hyperelliptic Curve Cryptography.
+    fn repeated_frobenius(&self, count: usize) -> Self {
+        if count == 0 {
+            return *self;
+        } else if count >= D {
+            // x |-> x^(n^D) is the identity, so x^(n^count) ==
+            // x^(n^(count % D))
+            return self.repeated_frobenius(count % D);
+        }
+        let arr: &[F] = self.as_base_slice();
+
+        // z0 = DTH_ROOT^count = W^(k * count) where k = floor((n-1)/D)
+        let mut z0 = F::DTH_ROOT;
+        for _ in 1..count {
+            z0 *= F::DTH_ROOT;
+        }
+
+        let mut res = [F::ZERO; D];
+        for (i, z) in z0.powers().take(D).enumerate() {
+            res[i] = arr[i] * z;
+        }
+
+        Self::from_base_slice(&res)
+    }
 }
