da9ce2780130f59b34e0765061a0c836834a36ad
Daniel Lubarov <daniel@lubarov.com>
2023-09-27 21:02:55 -0700

    Generalize Rescue impl (#127)
    * Generalize Rescue impl
    
    * rename
    
    * tweaks

--- rescue/src/lib.rs
+++ rescue/src/lib.rs
@@ -1,8 +1,8 @@
 #![allow(dead_code)] // TODO: remove when we settle on implementation details and publicly export
 
-mod inverse_sbox;
 mod rescue;
+mod sbox;
 mod util;
 
-pub use inverse_sbox::*;
 pub use rescue::*;
+pub use sbox::*;
