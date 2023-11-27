f640e4529d8326e6f21243482640b95f3178b97c
Nicholas Ward <npward@berkeley.edu>
2023-11-27 12:27:10 -0800

    poseidon2 tests

--- poseidon2/src/lib.rs
+++ poseidon2/src/lib.rs
@@ -161,3 +161,67 @@ where
     Diffusion: DiffusionPermutation<AF, WIDTH>,
 {
 }
+
+#[cfg(test)]
+mod tests {
+    use alloc::vec::Vec;
+    use p3_field::AbstractField;
+    use p3_symmetric::Permutation;
+    use rand::Rng;
+    use zkhash::fields::goldilocks::FpGoldiLocks;
+    use ark_ff::PrimeField;
+    use ark_ff::BigInteger;
+    use zkhash::poseidon2::poseidon2::Poseidon2 as Poseidon2Ref;
+    use zkhash::poseidon2::poseidon2_instance_goldilocks::{POSEIDON2_GOLDILOCKS_8_PARAMS, RC8};
+    use p3_goldilocks::Goldilocks;
+    use crate::goldilocks::DiffusionMatrixGoldilocks;
+
+    use crate::Poseidon2;
+
+    type F = Goldilocks;
+
+    fn goldilocks_from_ark_ff(input: FpGoldiLocks) -> F {
+        let as_bigint = input.into_bigint();
+        let mut as_bytes = as_bigint.to_bytes_le();
+        as_bytes.resize(8, 0);
+        let as_u64 = u64::from_le_bytes(as_bytes[0..8].try_into().unwrap());
+        F::from_wrapped_u64(as_u64)
+    }
+
+    #[test]
+    fn test_poseidon2_goldilocks() {
+        const WIDTH: usize = 8;
+        const D: u64 = 7;
+        const ROUNDS_F: usize = 8;
+        const ROUNDS_P: usize = 22;
+
+        let mut rng = rand::thread_rng();
+
+        let poseidon2_ref = Poseidon2Ref::new(&POSEIDON2_GOLDILOCKS_8_PARAMS);
+
+        let round_constants: Vec<[F; WIDTH]> = RC8.iter().map(|vec| {
+            vec.iter().cloned().map(goldilocks_from_ark_ff).collect::<Vec<_>>().try_into().unwrap()
+        }).collect();
+
+        let poseidon2: Poseidon2<Goldilocks, DiffusionMatrixGoldilocks, WIDTH, D> = Poseidon2::new(
+            ROUNDS_F,
+            ROUNDS_P,
+            round_constants,
+            DiffusionMatrixGoldilocks,
+        );
+
+        let random_input_u64 = rng.gen::<[u64; WIDTH]>();
+        let random_input_ref = random_input_u64.iter().cloned().map(FpGoldiLocks::from).collect::<Vec<_>>();
+        let random_input = random_input_u64.iter().cloned().map(F::from_wrapped_u64).collect::<Vec<_>>();
+
+        let ref_output = poseidon2_ref.permutation(&random_input_ref);
+        let ref_output_converted = ref_output.iter().cloned().map(goldilocks_from_ark_ff).collect::<Vec<_>>();
+        let ref_output_converted_arr: [F; WIDTH] = ref_output_converted.try_into().unwrap();
+
+        let mut output = random_input.clone().try_into().unwrap();
+        poseidon2.permute_mut(&mut output);
+
+        assert_eq!(output, ref_output_converted_arr);
+    }
+
+}
