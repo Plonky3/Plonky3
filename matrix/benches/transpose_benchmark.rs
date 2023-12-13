72aef6d1fc3962d1ca32b31f9893259904d116f8
Nicholas Ward <npward@berkeley.edu>
2023-12-13 15:42:10 -0800

    cleanup

--- matrix/benches/transpose_benchmark.rs
+++ matrix/benches/transpose_benchmark.rs
@@ -3,37 +3,37 @@ use p3_matrix::dense::RowMajorMatrix;
 use p3_matrix::MatrixTranspose;
 
 fn transpose_benchmark(c: &mut Criterion) {
-    // const WIDTH_10: usize = 10;
-    // const HEIGHT_10: usize = 10;
+    const WIDTH_10: usize = 10;
+    const HEIGHT_10: usize = 10;
 
-    // let matrix_10x10 = RowMajorMatrix::new(vec![0; WIDTH_10 * HEIGHT_10], WIDTH_10);
-    // c.bench_function("transpose 10x10", |b| {
-    //     b.iter(|| matrix_10x10.clone().transpose())
-    // });
+    let matrix_10x10 = RowMajorMatrix::new(vec![0; WIDTH_10 * HEIGHT_10], WIDTH_10);
+    c.bench_function("transpose 10x10", |b| {
+        b.iter(|| matrix_10x10.clone().transpose())
+    });
 
-    // const WIDTH_100: usize = 100;
-    // const HEIGHT_100: usize = 100;
+    const WIDTH_100: usize = 100;
+    const HEIGHT_100: usize = 100;
 
-    // let matrix_100x100 = RowMajorMatrix::new(vec![0; WIDTH_100 * HEIGHT_100], WIDTH_100);
-    // c.bench_function("transpose 100x100", |b| {
-    //     b.iter(|| matrix_100x100.clone().transpose())
-    // });
+    let matrix_100x100 = RowMajorMatrix::new(vec![0; WIDTH_100 * HEIGHT_100], WIDTH_100);
+    c.bench_function("transpose 100x100", |b| {
+        b.iter(|| matrix_100x100.clone().transpose())
+    });
 
-    // const WIDTH_200: usize = 200;
-    // const HEIGHT_200: usize = 200;
+    const WIDTH_200: usize = 200;
+    const HEIGHT_200: usize = 200;
 
-    // let matrix_200x200 = RowMajorMatrix::new(vec![0; WIDTH_200 * HEIGHT_200], WIDTH_200);
-    // c.bench_function("transpose 200x200", |b| {
-    //     b.iter(|| matrix_200x200.clone().transpose())
-    // });
+    let matrix_200x200 = RowMajorMatrix::new(vec![0; WIDTH_200 * HEIGHT_200], WIDTH_200);
+    c.bench_function("transpose 200x200", |b| {
+        b.iter(|| matrix_200x200.clone().transpose())
+    });
 
-    // const WIDTH_500: usize = 500;
-    // const HEIGHT_500: usize = 500;
+    const WIDTH_500: usize = 500;
+    const HEIGHT_500: usize = 500;
 
-    // let matrix_500x500 = RowMajorMatrix::new(vec![0; WIDTH_500 * HEIGHT_500], WIDTH_500);
-    // c.bench_function("transpose 500x500", |b| {
-    //     b.iter(|| matrix_500x500.clone().transpose())
-    // });
+    let matrix_500x500 = RowMajorMatrix::new(vec![0; WIDTH_500 * HEIGHT_500], WIDTH_500);
+    c.bench_function("transpose 500x500", |b| {
+        b.iter(|| matrix_500x500.clone().transpose())
+    });
 
     const WIDTH_1024: usize = 1024;
     const HEIGHT_1024: usize = 1024;
@@ -43,14 +43,14 @@ fn transpose_benchmark(c: &mut Criterion) {
         b.iter(|| matrix_1024x1024.clone().transpose())
     });
 
-    // const WIDTH_10_000: usize = 10_000;
-    // const HEIGHT_10_000: usize = 10_000;
+    const WIDTH_10_000: usize = 10_000;
+    const HEIGHT_10_000: usize = 10_000;
 
-    // let matrix_10_000x10_000 =
-    //     RowMajorMatrix::new(vec![0; WIDTH_10_000 * HEIGHT_10_000], WIDTH_10_000);
-    // c.bench_function("transpose 10_000x10_000", |b| {
-    //     b.iter(|| matrix_10_000x10_000.clone().transpose())
-    // });
+    let matrix_10_000x10_000 =
+        RowMajorMatrix::new(vec![0; WIDTH_10_000 * HEIGHT_10_000], WIDTH_10_000);
+    c.bench_function("transpose 10_000x10_000", |b| {
+        b.iter(|| matrix_10_000x10_000.clone().transpose())
+    });
 }
 
 criterion_group!(benches, transpose_benchmark);
