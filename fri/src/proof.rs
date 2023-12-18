c263aca1a8c6b91c148d5ba0f1aeacc38708cace
Daniel Lubarov <daniel@lubarov.com>
2023-12-17 21:22:11 -0800

    Remove unnecessary allows

--- fri/src/proof.rs
+++ fri/src/proof.rs
@@ -4,7 +4,6 @@ use p3_commit::Mmcs;
 
 use crate::FriConfig;
 
-#[allow(dead_code)] // TODO: fields should be used soon
 pub struct FriProof<FC: FriConfig> {
     pub(crate) commit_phase_commits: Vec<<FC::CommitPhaseMmcs as Mmcs<FC::Challenge>>::Commitment>,
     pub(crate) query_proofs: Vec<QueryProof<FC>>,
@@ -13,7 +12,6 @@ pub struct FriProof<FC: FriConfig> {
     pub(crate) final_poly: FC::Challenge,
 }
 
-#[allow(dead_code)] // TODO: fields should be used soon
 pub struct QueryProof<FC: FriConfig> {
     /// For each input commitment, this contains openings of each matrix at the queried location,
     /// along with an opening proof.
@@ -26,7 +24,6 @@ pub struct QueryProof<FC: FriConfig> {
 
 /// Openings of each input codeword at the queried location, along with an opening proof, for a
 /// single commitment round.
-#[allow(dead_code)] // TODO: fields should be used soon
 pub struct InputOpening<FC: FriConfig> {
     /// The opening of each input codeword at the queried location.
     pub(crate) opened_values: Vec<Vec<FC::Val>>,
@@ -34,7 +31,6 @@ pub struct InputOpening<FC: FriConfig> {
     pub(crate) opening_proof: <FC::InputMmcs as Mmcs<FC::Val>>::Proof,
 }
 
-#[allow(dead_code)] // TODO: fields should be used soon
 pub struct CommitPhaseProofStep<FC: FriConfig> {
     /// The opening of the commit phase codeword at the sibling location.
     // This may change to Vec<FC::Challenge> if the library is generalized to support other FRI
