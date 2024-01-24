1d5d0bf109d2d13ff9ac4c9789e815d5bd86417c
Daniel Lubarov <daniel@lubarov.com>
2024-01-24 15:38:57 -0800

    A couple FRI PoW improvements
    - Make proof-of-work bits configurable
    - Parallelize search for the proof-of-work witness

--- uni-stark/tests/mul_air.rs
+++ uni-stark/tests/mul_air.rs
@@ -102,7 +102,7 @@ fn test_prove_baby_bear() -> Result<(), VerificationError> {
 
     type Quotient = QuotientMmcs<Domain, Challenge, ValMmcs>;
     type MyFriConfig = FriConfigImpl<Val, Challenge, Quotient, ChallengeMmcs, Challenger>;
-    let fri_config = MyFriConfig::new(1, 40, challenge_mmcs);
+    let fri_config = MyFriConfig::new(1, 40, 8, challenge_mmcs);
     let ldt = FriLdt { config: fri_config };
 
     type Pcs = FriBasedPcs<MyFriConfig, ValMmcs, Dft, Challenger>;
