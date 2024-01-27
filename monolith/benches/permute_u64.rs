ac91c139d2bd4f68cdc3a37a3c0573624a72cbde
Nicholas Ward <npward@berkeley.edu>
2024-01-26 16:13:13 -0800

    fix

--- monolith/benches/permute_u64.rs
+++ monolith/benches/permute_u64.rs
@@ -1,9 +1,9 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use p3_monolith::{MonolithMdsMatrixMersenne31U64Width16, MonolithMersenne31U64Width16};
+use p3_monolith::{MonolithMdsMatrixMersenne31Width16, MonolithMersenne31Width16};
 
 fn permute_u64_benchmark(c: &mut Criterion) {
-    let mds = MonolithMdsMatrixMersenne31U64Width16;
-    let monolith: MonolithMersenne31U64Width16<5> = MonolithMersenne31U64Width16::new(mds);
+    let mds = MonolithMdsMatrixMersenne31Width16;
+    let monolith: MonolithMersenne31Width16<5> = MonolithMersenne31Width16::new(mds);
 
     let mut input: [u64; 16] = [0; 16];
     for (i, inp) in input.iter_mut().enumerate() {
