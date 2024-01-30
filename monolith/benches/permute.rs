0018f1fe252870c39c68e4e70c6ad2495f2b94b7
Nicholas Ward <npward@berkeley.edu>
2024-01-30 12:25:58 -0800

    fix

--- monolith/benches/permute.rs
+++ monolith/benches/permute.rs
@@ -1,11 +1,11 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 use p3_field::AbstractField;
 use p3_mersenne_31::Mersenne31;
-use p3_monolith::{MonolithMdsMatrixMersenne31, MonolithMersenne31};
+use p3_monolith::{MonolithMdsMatrixM31, MonolithM31};
 
 fn permute_benchmark(c: &mut Criterion) {
-    let mds = MonolithMdsMatrixMersenne31::<6>;
-    let monolith: MonolithMersenne31<_, 16, 5> = MonolithMersenne31::new(mds);
+    let mds = MonolithMdsMatrixM31::<6>;
+    let monolith: MonolithM31<_, 16, 5> = MonolithM31::new(mds);
 
     let mut input: [Mersenne31; 16] = [Mersenne31::zero(); 16];
     for (i, inp) in input.iter_mut().enumerate() {
--- monolith/benches/permute_width16.rs
+++ monolith/benches/permute_width16.rs
@@ -1,11 +1,11 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 use p3_field::AbstractField;
 use p3_mersenne_31::Mersenne31;
-use p3_monolith::{MonolithMdsMatrixMersenne31Width16, MonolithMersenne31Width16};
+use p3_monolith::{MonolithMdsMatrixM31Width16, MonolithM31Width16};
 
 fn permute_width16_benchmark(c: &mut Criterion) {
-    let mds = MonolithMdsMatrixMersenne31Width16;
-    let monolith: MonolithMersenne31Width16<5> = MonolithMersenne31Width16::new(mds);
+    let mds = MonolithMdsMatrixM31Width16;
+    let monolith: MonolithM31Width16<5> = MonolithM31Width16::new(mds);
 
     let mut input: [Mersenne31; 16] = [Mersenne31::zero(); 16];
     for (i, inp) in input.iter_mut().enumerate() {
