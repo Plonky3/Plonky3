0799f6c39e2aed1fb86da5c6d6716ebd25943ff4
Daniel Lubarov <daniel@lubarov.com>
2024-02-11 10:42:29 -0800

    Poseidon2MEMatrix tweaks

--- poseidon2/src/matrix.rs
+++ poseidon2/src/matrix.rs
@@ -43,24 +43,23 @@ where
     fn permute_mut(&self, state: &mut [AF; WIDTH]) {
         // First, we apply M_4 to each consecutive four elements of the state.
         // In Appendix B's terminology, this replaces each x_i with x_i'.
-        for i in 0..WIDTH / 4 {
-            apply_m_4(&mut state[4 * i..4 * i + 4]);
+        for i in (0..WIDTH).step_by(4) {
+            apply_m_4(&mut state[i..i + 4]);
         }
 
         // Now, we apply the outer circulant matrix (to compute the y_i values).
 
         // We first precompute the four sums of every four elements.
-        let sums: Vec<AF> = (0..4)
-            .map(|k| {
-                (0..state.len() / 4)
-                    .map(|j| state[4 * j + k].clone())
-                    .sum::<AF>()
-            })
-            .collect();
+        let sums: [AF; 4] = core::array::from_fn(|k| {
+            (0..WIDTH)
+                .step_by(4)
+                .map(|j| state[j + k].clone())
+                .sum::<AF>()
+        });
 
         // The formula for each y_i involves 2x_i' term and x_j' terms for each j that equals i mod 4.
         // In other words, we can add a single copy of x_i' to the appropriate one of our precomputed sums
-        for i in 0..state.len() {
+        for i in 0..WIDTH {
             state[i] += sums[i % 4].clone();
         }
     }
