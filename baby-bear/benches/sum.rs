9299bf2ff9a389bd75fe03420f0678c7060e00a0
AngusG <angusgruen@gmail.com>
2024-02-16 17:53:25 -0500

    Cleaning up Sum Benchmarking Code
    I've been informed that benchmarks in the single ns are a bit BS so added a loop to push all timings into the microseconds.

--- baby-bear/benches/sum.rs
+++ baby-bear/benches/sum.rs
@@ -1,41 +1,76 @@
-use p3_field_testing::benchmark_sum;
+use p3_field::AbstractField;
+use criterion::black_box;
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
-use p3_baby_bear::{BabyBear, sum_u64};
+use p3_baby_bear::{sum_u64, BabyBear};
 use rand::Rng;
 
 type F = BabyBear;
 
 fn bench_sum(c: &mut Criterion) {
-    let name = "BabyBear";
-
-    benchmark_sum::<F, 1>(c, name);
+    sum::<1>(c);
     sum_delayed::<1>(c);
 
-    benchmark_sum::<F, 2>(c, name);
+    sum::<2>(c);
     sum_delayed::<2>(c);
 
-    benchmark_sum::<F, 4>(c, name);
+    sum::<4>(c);
     sum_delayed::<4>(c);
 
-    benchmark_sum::<F, 8>(c, name);
+    sum::<8>(c);
     sum_delayed::<8>(c);
 
-    benchmark_sum::<F, 16>(c, name);
+    sum::<16>(c);
     sum_delayed::<16>(c);
 
-    benchmark_sum::<F, 32>(c, name);
+    sum::<32>(c);
     sum_delayed::<32>(c);
+}
+
+fn sum<const N: usize>(c: &mut Criterion) {
+    let mut rng = rand::thread_rng();
+    let mut input = Vec::new();
+    for _ in 0..1000 {
+        let mut row = Vec::new();
+        for _ in 0..N {
+            row.push(rng.gen::<F>())
+        }
+        input.push(row)
+    }
+    
+    let id = BenchmarkId::new("BabyBear sum", N);
+    c.bench_with_input(id, &input, |b, input| {
+        b.iter(|| {
+            black_box(input);
+            let mut res = [F::zero(); 1000];
+            for i in 0..1000 {
+                res[i] = input[i].iter().fold(F::zero(), |x, y| x + *y);
+            }
+            res
+        });
+    });
 }
 
 fn sum_delayed<const N: usize>(c: &mut Criterion) {
     let mut rng = rand::thread_rng();
     let mut input = Vec::new();
-    for _ in 0..N {
-        input.push(rng.gen::<F>())
+    for _ in 0..1000 {
+        let mut row = Vec::new();
+        for _ in 0..N {
+            row.push(rng.gen::<F>())
+        }
+        input.push(row)
     }
+    
     let id = BenchmarkId::new("BabyBear sum_delayed", N);
     c.bench_with_input(id, &input, |b, input| {
-        b.iter(|| sum_u64(&input));
+        b.iter(|| {
+            black_box(input);
+            let mut res = [F::zero(); 1000];
+            for i in 0..1000 {
+                res[i] = sum_u64(&input[i]);
+            }
+            res
+        });
     });
 }
 
