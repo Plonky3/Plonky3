8b5fb33bcd6dca01ebaf541fb3b5f6d788f1b3bc
AngusG <angusgruen@gmail.com>
2024-02-21 14:45:19 -0500

    Cleaning up Benchmarks
    Added some functions into field-testing\src\bench_func.rs which allows us to simplify the benchmarking code for BabyBear, Mersenne31 and Goldilocks.

--- field-testing/src/bench_func.rs
+++ field-testing/src/bench_func.rs
@@ -1,3 +1,5 @@
+use criterion::BatchSize;
+use alloc::vec::Vec;
 use alloc::format;
 
 use criterion::{black_box, Criterion};
@@ -37,5 +39,151 @@ where
     let y = rng.gen::<F>();
     c.bench_function(&format!("{} mul", name), |b| {
         b.iter(|| black_box(black_box(x) * black_box(y)))
+    });
+}
+
+pub fn benchmark_iter_sum<F: Field, const REPS: usize, const N: usize>(c: &mut Criterion, name: &str)
+where
+    Standard: Distribution<F>,
+{
+    let mut rng = rand::thread_rng();
+    let mut input = Vec::new();
+    for _ in 0..REPS {
+        input.push(rng.gen::<[F; N]>())
+    }
+    let mut output = [F::zero(); REPS];
+    c.bench_function(&format!("{} sum/{}, {}", name, REPS, N), |b| {
+        b.iter(|| {
+            for i in 0..REPS {
+                output[i] = input[i].iter().cloned().sum()
+            }
+            output
+        })
+    });
+}
+
+pub fn benchmark_add_latency<F: Field, const N: usize>(c: &mut Criterion, name: &str)
+where
+    Standard: Distribution<F>,
+{
+    c.bench_function(&format!("{} add-latency/{}", name, N), |b| {
+        b.iter_batched(
+            || {
+                let mut rng = rand::thread_rng();
+                let mut vec = Vec::new();
+                for _ in 0..N {
+                    vec.push(rng.gen::<F>())
+                }
+                vec
+            },
+            |x| x.iter().fold(F::zero(), |x, y| x + *y),
+            BatchSize::SmallInput,
+        )
+    });
+}
+
+pub fn benchmark_add_throughput<F: Field, const N: usize>(c: &mut Criterion, name: &str)
+where
+    Standard: Distribution<F>,
+{
+    c.bench_function(&format!("{} add-throughput/{}", name, N), |b| {
+        b.iter_batched(
+            || {
+                let mut rng = rand::thread_rng();
+                (
+                    rng.gen::<F>(),
+                    rng.gen::<F>(),
+                    rng.gen::<F>(),
+                    rng.gen::<F>(),
+                    rng.gen::<F>(),
+                    rng.gen::<F>(),
+                    rng.gen::<F>(),
+                    rng.gen::<F>(),
+                    rng.gen::<F>(),
+                    rng.gen::<F>(),
+                )
+            },
+            |(mut a, mut b, mut c, mut d, mut e, mut f, mut g, mut h, mut i, mut j)| {
+                for _ in 0..N {
+                    (a, b, c, d, e, f, g, h, i, j) = (
+                        a + b,
+                        b + c,
+                        c + d,
+                        d + e,
+                        e + f,
+                        f + g,
+                        g + h,
+                        h + i,
+                        i + j,
+                        j + a,
+                    );
+                }
+                (a, b, c, d, e, f, g, h, i, j)
+            },
+            BatchSize::SmallInput,
+        )
+    });
+}
+
+pub fn benchmark_sub_latency<F: Field, const N: usize>(c: &mut Criterion, name: &str)
+where
+    Standard: Distribution<F>,
+{
+    c.bench_function(&format!("{} sub-latency/{}", name, N), |b| {
+        b.iter_batched(
+            || {
+                let mut rng = rand::thread_rng();
+                let mut vec = Vec::new();
+                for _ in 0..N {
+                    vec.push(rng.gen::<F>())
+                }
+                vec
+            },
+            |x| x.iter().fold(F::zero(), |x, y| x - *y),
+            BatchSize::SmallInput,
+        )
+    });
+}
+
+pub fn benchmark_sub_throughput<F: Field, const N: usize>(c: &mut Criterion, name: &str)
+where
+    Standard: Distribution<F>,
+{
+    c.bench_function(&format!("{} sub-throughput/{}", name, N), |b| {
+        b.iter_batched(
+            || {
+                let mut rng = rand::thread_rng();
+                (
+                    rng.gen::<F>(),
+                    rng.gen::<F>(),
+                    rng.gen::<F>(),
+                    rng.gen::<F>(),
+                    rng.gen::<F>(),
+                    rng.gen::<F>(),
+                    rng.gen::<F>(),
+                    rng.gen::<F>(),
+                    rng.gen::<F>(),
+                    rng.gen::<F>(),
+                )
+            },
+            |(mut a, mut b, mut c, mut d, mut e, mut f, mut g, mut h, mut i, mut j)| {
+                for _ in 0..N {
+                    (a, b, c, d, e, f, g, h, i, j) = (
+                        a - b,
+                        b - c,
+                        c - d,
+                        d - e,
+                        e - f,
+                        f - g,
+                        g - h,
+                        h - i,
+                        i - j,
+                        j - a,
+                    );
+                }
+                (a, b, c, d, e, f, g, h, i, j)
+            },
+            BatchSize::SmallInput,
+        )
     });
 }
--- goldilocks/benches/bench_field.rs
+++ goldilocks/benches/bench_field.rs
@@ -1,12 +1,12 @@
 use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
 use p3_field_testing::bench_func::{benchmark_iter_sum, benchmark_inv, benchmark_add_latency, benchmark_add_throughput, benchmark_sub_latency, benchmark_sub_throughput};
 use p3_field::{AbstractField};
-use p3_mersenne_31::Mersenne31;
+use p3_goldilocks::Goldilocks;
 
-type F = Mersenne31;
+type F = Goldilocks;
 
 fn bench_field(c: &mut Criterion) {
-    let name = "Mersenne31";
+    let name = "Goldilocks";
     benchmark_inv::<F>(c, name);
     benchmark_iter_sum::<F, 1000, 4>(c, name);
     benchmark_iter_sum::<F, 1000, 8>(c, name);
@@ -17,14 +17,14 @@ fn bench_field(c: &mut Criterion) {
     benchmark_sub_latency::<F, 10000>(c, name);
     benchmark_sub_throughput::<F, 10000>(c, name);
 
-    c.bench_function("5th_root", |b| {
+    c.bench_function("7th_root", |b| {
         b.iter_batched(
             rand::random::<F>,
-            |x| x.exp_u64(1717986917),
+            |x| x.exp_u64(10540996611094048183),
             BatchSize::SmallInput,
         )
     });
 }
 
-criterion_group!(mersenne31_arithmetics, bench_field);
-criterion_main!(mersenne31_arithmetics);
+criterion_group!(goldilocks_arithmetic, bench_field);
+criterion_main!(goldilocks_arithmetic);
