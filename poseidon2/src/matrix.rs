f29af2553fd34c300ab665ffb679a3f95d7345b2
AngusG <angusgruen@gmail.com>
2024-02-21 09:43:07 -0500

    Update poseidon2/src/matrix.rs
    Co-authored-by: Hamish Ivey-Law <426294+unzvfu@users.noreply.github.com>

--- poseidon2/src/matrix.rs
+++ poseidon2/src/matrix.rs
@@ -38,7 +38,7 @@ where
 // [ 1 2 3 1 ]
 // [ 1 1 2 3 ]
 // [ 3 1 1 2 ].
-// This is the more efficient than the one above (11 additions vs 16 additions) and leads to a ~5% speed up.
+// This is more efficient than the one above (11 additions vs 16 additions) and leads to a ~5% speed up.
 // Unfortunately it breaks all the tests as we are tesing against the implementation from zkhash.
 // Hence will leave this as a comment for now and implement later.
 // fn apply_m_4<AF>(x: &mut [AF])
