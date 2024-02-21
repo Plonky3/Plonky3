330800ff6c1a7c9c5689563ecfea60518199fbbb
AngusG <agruen@polygon.technology>
2024-02-21 17:46:55 -0500

    Cleaning up field benching (#259)
    * Cleaning up Benchmarks
    
    Added some functions into field-testing\src\bench_func.rs which allows us to simplify the benchmarking code for BabyBear, Mersenne31 and Goldilocks.
    
    * fmt and clippy
    
    * Minor typos
    
    Good to keep benchmarks for add_throughput, sub_throughput comparable but using the save number of repetitions.
    
    * Comments
    
    Adding a few comments and adding in a simple constant controlling the number of repations.

--- baby-bear/benches/bench_field.rs
+++ baby-bear/benches/bench_field.rs
@@ -1,15 +1,15 @@
 use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
+use p3_baby_bear::BabyBear;
 use p3_field::AbstractField;
 use p3_field_testing::bench_func::{
     benchmark_add_latency, benchmark_add_throughput, benchmark_inv, benchmark_iter_sum,
     benchmark_sub_latency, benchmark_sub_throughput,
 };
-use p3_mersenne_31::Mersenne31;
 
-type F = Mersenne31;
+type F = BabyBear;
 
 fn bench_field(c: &mut Criterion) {
-    let name = "Mersenne31";
+    let name = "BabyBear";
     const REPS: usize = 1000;
     benchmark_inv::<F>(c, name);
     benchmark_iter_sum::<F, 4, REPS>(c, name);
@@ -24,14 +24,14 @@ fn bench_field(c: &mut Criterion) {
     benchmark_sub_latency::<F, L_REPS>(c, name);
     benchmark_sub_throughput::<F, REPS>(c, name);
 
-    c.bench_function("5th_root", |b| {
+    c.bench_function("7th_root", |b| {
         b.iter_batched(
             rand::random::<F>,
-            |x| x.exp_u64(1717986917),
+            |x| x.exp_u64(1725656503),
             BatchSize::SmallInput,
         )
     });
 }
 
-criterion_group!(mersenne31_arithmetics, bench_field);
-criterion_main!(mersenne31_arithmetics);
+criterion_group!(baby_bear_arithmetic, bench_field);
+criterion_main!(baby_bear_arithmetic);
