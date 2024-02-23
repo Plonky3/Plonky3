ed16a4a8608e5d09e01c9a38cd1635406bc44fcc
Tamir Hemo <tamir@succinct.xyz>
2024-02-23 11:45:31 -0800

    keccak air unrelated changes

--- keccak-air/src/lib.rs
+++ keccak-air/src/lib.rs
@@ -13,11 +13,10 @@ mod round_flags;
 
 pub use air::*;
 pub use columns::*;
-pub use constants::*;
 pub use generation::*;
 
-pub const NUM_ROUNDS: usize = 24;
+const NUM_ROUNDS: usize = 24;
 const BITS_PER_LIMB: usize = 16;
-pub const U64_LIMBS: usize = 64 / BITS_PER_LIMB;
+const U64_LIMBS: usize = 64 / BITS_PER_LIMB;
 const RATE_BITS: usize = 1088;
 const RATE_LIMBS: usize = RATE_BITS / BITS_PER_LIMB;
