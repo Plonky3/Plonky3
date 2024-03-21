95ca6b2434b3178520ef0a084584e741847a860f
patrickmao <patrickmao93@gmail.com>
2024-03-22 04:26:13 +0800

    remove test mod because it is an integration test

--- uni-stark/tests/fib_air.rs
+++ uni-stark/tests/fib_air.rs
@@ -1,9 +1,24 @@
 use std::borrow::Borrow;
 
+use rand::thread_rng;
+
 use p3_air::{Air, AirBuilder, AirBuilderWithPublicValues, BaseAir};
+use p3_baby_bear::{BabyBear, DiffusionMatrixBabybear};
+use p3_challenger::DuplexChallenger;
+use p3_commit::ExtensionMmcs;
+use p3_dft::Radix2DitParallel;
+use p3_field::{AbstractField, Field};
+use p3_field::extension::BinomialExtensionField;
 use p3_field::PrimeField64;
+use p3_fri::{FriConfig, TwoAdicFriPcs};
 use p3_matrix::dense::RowMajorMatrix;
+use p3_matrix::Matrix;
 use p3_matrix::MatrixRowSlices;
+use p3_merkle_tree::FieldMerkleTreeMmcs;
+use p3_poseidon2::Poseidon2;
+use p3_symmetric::{PaddingFreeSponge, TruncatedPermutation};
+use p3_uni_stark::{prove, StarkConfig, verify};
+use p3_util::log2_ceil_usize;
 
 /// For testing the public values feature
 
@@ -89,110 +104,70 @@ impl<F> Borrow<FibonacciRow<F>> for [F] {
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use rand::thread_rng;
+type Val = BabyBear;
+type Perm = Poseidon2<Val, DiffusionMatrixBabybear, 16, 7>;
+type MyHash = PaddingFreeSponge<Perm, 16, 8, 8>;
+type MyCompress = TruncatedPermutation<Perm, 2, 8, 16>;
+type ValMmcs =
+    FieldMerkleTreeMmcs<<Val as Field>::Packing, <Val as Field>::Packing, MyHash, MyCompress, 8>;
+type Challenge = BinomialExtensionField<Val, 4>;
+type ChallengeMmcs = ExtensionMmcs<Val, Challenge, ValMmcs>;
+type Challenger = DuplexChallenger<Val, Perm, 16>;
+type Dft = Radix2DitParallel;
+type Pcs = TwoAdicFriPcs<Val, Dft, ValMmcs, ChallengeMmcs>;
+type MyConfig = StarkConfig<Pcs, Challenge, Challenger>;
 
-    use p3_baby_bear::{BabyBear, DiffusionMatrixBabybear};
-    use p3_challenger::DuplexChallenger;
-    use p3_commit::ExtensionMmcs;
-    use p3_dft::Radix2DitParallel;
-    use p3_field::{AbstractField, Field};
-    use p3_field::extension::BinomialExtensionField;
-    use p3_fri::{FriConfig, TwoAdicFriPcs};
-    use p3_matrix::{Matrix, MatrixGet};
-    use p3_merkle_tree::FieldMerkleTreeMmcs;
-    use p3_poseidon2::Poseidon2;
-    use p3_symmetric::{PaddingFreeSponge, TruncatedPermutation};
-    use p3_uni_stark::{Proof, prove, StarkConfig, verify};
-    use p3_util::log2_ceil_usize;
+#[test]
+fn test_public_value() {
+    let perm = Perm::new_from_rng(8, 22, DiffusionMatrixBabybear, &mut thread_rng());
+    let hash = MyHash::new(perm.clone());
+    let compress = MyCompress::new(perm.clone());
+    let val_mmcs = ValMmcs::new(hash, compress);
+    let challenge_mmcs = ChallengeMmcs::new(val_mmcs.clone());
+    let dft = Dft {};
+    let trace = generate_trace_rows::<Val>(0, 1, 1 << 3);
+    let fri_config = FriConfig {
+        log_blowup: 2,
+        num_queries: 28,
+        proof_of_work_bits: 8,
+        mmcs: challenge_mmcs,
+    };
+    let pcs = Pcs::new(log2_ceil_usize(trace.height()), dft, val_mmcs, fri_config);
+    let config = MyConfig::new(pcs);
+    let mut challenger = Challenger::new(perm.clone());
+    let pis = vec![
+        BabyBear::from_canonical_u64(0),
+        BabyBear::from_canonical_u64(1),
+        BabyBear::from_canonical_u64(21),
+    ];
+    let proof = prove(&config, &FibonacciAir {}, &mut challenger, trace, &pis);
+    let mut challenger = Challenger::new(perm);
+    verify(&config, &FibonacciAir {}, &mut challenger, &proof, &pis).expect("verification failed");
+}
 
-    use super::*;
-
-    type Val = BabyBear;
-    type Perm = Poseidon2<Val, DiffusionMatrixBabybear, 16, 7>;
-    type MyHash = PaddingFreeSponge<Perm, 16, 8, 8>;
-    type MyCompress = TruncatedPermutation<Perm, 2, 8, 16>;
-    type ValMmcs = FieldMerkleTreeMmcs<
-        <Val as Field>::Packing,
-        <Val as Field>::Packing,
-        MyHash,
-        MyCompress,
-        8,
-    >;
-    type Challenge = BinomialExtensionField<Val, 4>;
-    type ChallengeMmcs = ExtensionMmcs<Val, Challenge, ValMmcs>;
-    type Challenger = DuplexChallenger<Val, Perm, 16>;
-    type Dft = Radix2DitParallel;
-    type Pcs = TwoAdicFriPcs<Val, Dft, ValMmcs, ChallengeMmcs>;
-    type MyConfig = StarkConfig<Pcs, Challenge, Challenger>;
-
-    #[test]
-    fn test_public_value() {
-        let perm = Perm::new_from_rng(8, 22, DiffusionMatrixBabybear, &mut thread_rng());
-        let hash = MyHash::new(perm.clone());
-        let compress = MyCompress::new(perm.clone());
-        let val_mmcs = ValMmcs::new(hash, compress);
-        let challenge_mmcs = ChallengeMmcs::new(val_mmcs.clone());
-        let dft = Dft {};
-        let trace = generate_trace_rows::<Val>(0, 1, 1 << 3);
-        let fri_config = FriConfig {
-            log_blowup: 2,
-            num_queries: 28,
-            proof_of_work_bits: 8,
-            mmcs: challenge_mmcs,
-        };
-        let pcs = Pcs::new(log2_ceil_usize(trace.height()), dft, val_mmcs, fri_config);
-        let config = MyConfig::new(pcs);
-        let mut challenger = Challenger::new(perm.clone());
-        let pis = vec![
-            BabyBear::from_canonical_u64(0),
-            BabyBear::from_canonical_u64(1),
-            BabyBear::from_canonical_u64(21),
-        ];
-        let proof = prove::<MyConfig, _>(&config, &FibonacciAir {}, &mut challenger, trace, &pis);
-        let mut challenger = Challenger::new(perm);
-        verify(&config, &FibonacciAir {}, &mut challenger, &proof, &pis)
-            .expect("verification failed");
-    }
-
-    #[test]
-    #[should_panic]
-    fn test_incorrect_public_value() {
-        let perm = Perm::new_from_rng(8, 22, DiffusionMatrixBabybear, &mut thread_rng());
-        let hash = MyHash::new(perm.clone());
-        let compress = MyCompress::new(perm.clone());
-        let val_mmcs = ValMmcs::new(hash, compress);
-        let challenge_mmcs = ChallengeMmcs::new(val_mmcs.clone());
-        let dft = Dft {};
-        let fri_config = FriConfig {
-            log_blowup: 2,
-            num_queries: 28,
-            proof_of_work_bits: 8,
-            mmcs: challenge_mmcs,
-        };
-        let trace = generate_trace_rows::<Val>(0, 1, 1 << 3);
-        let pcs = Pcs::new(log2_ceil_usize(trace.height()), dft, val_mmcs, fri_config);
-        let config = MyConfig::new(pcs);
-        let mut challenger = Challenger::new(perm.clone());
-        let pis = vec![
-            BabyBear::from_canonical_u64(0),
-            BabyBear::from_canonical_u64(1),
-            BabyBear::from_canonical_u64(123123), // incorrect result
-        ];
-        prove::<MyConfig, _>(&config, &FibonacciAir {}, &mut challenger, trace, &pis);
-    }
-
-    #[test]
-    fn generate() {
-        let m = generate_trace_rows::<BabyBear>(0, 1, 2);
-        let f = BabyBear::from_canonical_u64;
-
-        // left right values
-        assert_eq!(m.get(0, 0), f(0));
-        assert_eq!(m.get(0, 1), f(1)); // 0 1
-
-        assert_eq!(m.get(1, 0), f(1));
-        assert_eq!(m.get(1, 1), f(1)); // 1 1
-    }
+#[test]
+#[should_panic]
+fn test_incorrect_public_value() {
+    let perm = Perm::new_from_rng(8, 22, DiffusionMatrixBabybear, &mut thread_rng());
+    let hash = MyHash::new(perm.clone());
+    let compress = MyCompress::new(perm.clone());
+    let val_mmcs = ValMmcs::new(hash, compress);
+    let challenge_mmcs = ChallengeMmcs::new(val_mmcs.clone());
+    let dft = Dft {};
+    let fri_config = FriConfig {
+        log_blowup: 2,
+        num_queries: 28,
+        proof_of_work_bits: 8,
+        mmcs: challenge_mmcs,
+    };
+    let trace = generate_trace_rows::<Val>(0, 1, 1 << 3);
+    let pcs = Pcs::new(log2_ceil_usize(trace.height()), dft, val_mmcs, fri_config);
+    let config = MyConfig::new(pcs);
+    let mut challenger = Challenger::new(perm.clone());
+    let pis = vec![
+        BabyBear::from_canonical_u64(0),
+        BabyBear::from_canonical_u64(1),
+        BabyBear::from_canonical_u64(123123), // incorrect result
+    ];
+    prove(&config, &FibonacciAir {}, &mut challenger, trace, &pis);
 }
