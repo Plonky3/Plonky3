1c59af0de0648f6b02ec68639b43dba435c29ecd
AngusG <angusgruen@gmail.com>
2024-03-25 19:47:15 -0400

    AVX and NEON implementations

--- koala-bear/src/lib.rs
+++ koala-bear/src/lib.rs
@@ -19,25 +19,25 @@ pub use koala_bear::*;
 // pub use mds::*;
 pub use poseidon2::DiffusionMatrixKoalabearScalar;
 
-// #[cfg(all(target_arch = "aarch64", target_feature = "neon"))]
-// mod aarch64_neon;
-// #[cfg(all(target_arch = "aarch64", target_feature = "neon"))]
-// pub use aarch64_neon::*;
+#[cfg(all(target_arch = "aarch64", target_feature = "neon"))]
+mod aarch64_neon;
+#[cfg(all(target_arch = "aarch64", target_feature = "neon"))]
+pub use aarch64_neon::*;
 
-// #[cfg(all(target_arch = "x86_64", target_feature = "avx2"))]
-// mod x86_64_avx2;
-// #[cfg(all(target_arch = "x86_64", target_feature = "avx2"))]
-// pub use x86_64_avx2::*;
+#[cfg(all(target_arch = "x86_64", target_feature = "avx2"))]
+mod x86_64_avx2;
+#[cfg(all(target_arch = "x86_64", target_feature = "avx2"))]
+pub use x86_64_avx2::*;
 
-// #[cfg(all(
-//     feature = "nightly-features",
-//     target_arch = "x86_64",
-//     target_feature = "avx512f"
-// ))]
-// mod x86_64_avx512;
-// #[cfg(all(
-//     feature = "nightly-features",
-//     target_arch = "x86_64",
-//     target_feature = "avx512f"
-// ))]
-// pub use x86_64_avx512::*;
+#[cfg(all(
+    feature = "nightly-features",
+    target_arch = "x86_64",
+    target_feature = "avx512f"
+))]
+mod x86_64_avx512;
+#[cfg(all(
+    feature = "nightly-features",
+    target_arch = "x86_64",
+    target_feature = "avx512f"
+))]
+pub use x86_64_avx512::*;
