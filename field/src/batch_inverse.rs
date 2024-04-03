0571f21e07987edc89ee625b6cca463541ef0942
Chris Tian <chris@succinct.xyz>
2024-04-03 14:11:11 -0700

    batch_multiplicative_inverse_inplace

--- field/src/batch_inverse.rs
+++ field/src/batch_inverse.rs
@@ -97,3 +97,25 @@ pub fn batch_multiplicative_inverse<F: Field>(x: &[F]) -> Vec<F> {
 
     buf
 }
+
+/// An implementation of `batch_multiplicative_inverse` that operates in place.
+pub fn batch_multiplicative_inverse_inplace<F: Field>(values: &mut [F]) {
+    // Check if values are zero and construct a new vector with only nonzero values.
+    let mut nonzero_values = Vec::with_capacity(values.len());
+    let mut indices = Vec::with_capacity(values.len());
+    for (i, value) in values.iter().cloned().enumerate() {
+        if value.is_zero() {
+            continue;
+        }
+        nonzero_values.push(value);
+        indices.push(i);
+    }
+
+    // Compute the multiplicative inverse of nonzero values.
+    let inverse_nonzero_values = batch_multiplicative_inverse(&nonzero_values);
+
+    // Reconstruct the original vector.
+    for (i, index) in indices.into_iter().enumerate() {
+        values[index] = inverse_nonzero_values[i];
+    }
+}
