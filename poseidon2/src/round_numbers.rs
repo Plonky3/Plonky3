34648443fe9b51814222e296aeeb18bea297adb6
AngusG <angusgruen@gmail.com>
2024-04-04 16:54:36 -0400

    Adding Comments
    Adding comments for round_numbers and removing the redefined primes. Made it clear that the computed round numbers will work for all 31 and 64 bit primes respectively.

--- poseidon2/src/round_numbers.rs
+++ poseidon2/src/round_numbers.rs
@@ -1,24 +1,42 @@
+// As the security analysis of Poseidon2 is identical to that of Poseidon,
+// the relevant constraints regarding the number of full/partial rounds required can be found in
+// the original paper: https://eprint.iacr.org/2019/458.pdf and the associated codebase:
+// https://extgit.iaik.tugraz.at/krypto/hadeshash (See generate_params_poseidon.sage)
+
+// These constraints are broken down into 6 equations:
+// statistical, interpolation, groebner 1, 2, 3 and
+// an extra constraint coming from the paper https://eprint.iacr.org/2023/537.pdf.
+
+// For our parameters (M = 128, p > 2^30, WIDTH = t >= 8, D = alpha < 12),
+// the statistical constraint always simplifies to requiring RF >= 6.
+// Additionally p does not appear in Groebner 3 or the constraint coming from https://eprint.iacr.org/2023/537.pdf.
+// The remaining 3 constraints all can be rearranged into the form:
+// F(RF, RP) >= G(p) where G is a function which is non-decreasing with respect to p.
+
+// Thus, if some tuple (M, p, WIDTH, D, RF, RP) satisfies all constraints, then so will
+// the tuple (M, q, WIDTH, D, RF, RP) for any 2^30 < q < p.
+// Moreover if RF, RP are the "optimal" round numbers (Optimal meaning minimising the number of s-box operations we need to perform)
+// for two tuples (M, p, WIDTH, D) and (M, q, WIDTH, D), then
+// they will also be optimal for (M, r, WIDTH, D) for any q < r < p.
+
 // We compute the optimal required number of external (full) and internal (partial) rounds using:
-// See https://github.com/0xPolygonZero/hash-constants
-// Note the the number of rounds required is the same for fields of approximately the same size.
+// https://github.com/0xPolygonZero/hash-constants
+// Using the above analysis we can conclude that the round numbers are equal
+// for all 31 bit primes and 64 bit primes respectively.
 
+use gcd::Gcd;
 use p3_field::PrimeField64;
-// Mersenne31, KoalaBear and BabyBear fields
-const PRIMES_31_BIT: [u64; 3] = [
-    (1 << 31) - 1,
-    (1 << 31) - (1 << 24) + 1,
-    (1 << 31) - (1 << 27) + 1,
-];
 
-// Goldilocks and Crandall Primes
-const PRIMES_64_BIT: [u64; 2] = [0xFFFFFFFF00000001, 0xFFFFFFFF70000001];
+/// Given a field, a width and an D return the number of full and partial rounds needed to achieve 128 bit security.
+pub fn poseidon2_round_numbers_128<F: PrimeField64>(width: usize, d: u64) -> (usize, usize) {
+    // Start by checking that d is a valid permutation.
+    assert!(d.gcd(F::ORDER_U64 - 1) == 1);
 
-/// Given a field, a width and an alpha return the number of full and partial rounds needed to achieve 128 bit security.
-pub fn poseidon2_round_numbers_128<F: PrimeField64>(width: usize, alpha: u64) -> (usize, usize) {
-    assert!(PRIMES_31_BIT.contains(&F::ORDER_U64) || PRIMES_64_BIT.contains(&F::ORDER_U64));
+    // Next compute the number of bits in p.
+    let prime_bit_number = (F::ORDER_U64).ilog2() + 1;
 
-    if PRIMES_31_BIT.contains(&F::ORDER_U64) {
-        match (width, alpha) {
+    match prime_bit_number {
+        31 => match (width, d) {
             (16, 3) => (8, 20),
             (16, 5) => (8, 14),
             (16, 7) => (8, 13),
@@ -29,10 +47,9 @@ pub fn poseidon2_round_numbers_128<F: PrimeField64>(width: usize, alpha: u64) ->
             (24, 7) => (8, 21),
             (24, 9) => (8, 21),
             (24, 11) => (8, 21),
-            _ => panic!("The given pair of width and alpha has not been checked for these fields"),
-        }
-    } else {
-        match (width, alpha) {
+            _ => panic!("The given pair of width and D has not been checked for these fields"),
+        },
+        64 => match (width, d) {
             (8, 3) => (8, 41),
             (8, 5) => (8, 27),
             (8, 7) => (8, 22),
@@ -48,7 +65,8 @@ pub fn poseidon2_round_numbers_128<F: PrimeField64>(width: usize, alpha: u64) ->
             (16, 7) => (8, 22),
             (16, 9) => (8, 20),
             (16, 11) => (8, 18),
-            _ => panic!("The given pair of width and alpha has not been checked for these fields"),
-        }
+            _ => panic!("The given pair of width and D has not been checked for these fields"),
+        },
+        _ => panic!("The optimal parameters for that size of prime have not been computed."),
     }
 }
