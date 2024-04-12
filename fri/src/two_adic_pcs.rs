b7da409b254120f2a1e9d66c37fa745762e04e61
Chris Tian <chris@succinct.xyz>
2024-04-12 14:17:18 -0700

    chore: make Proof clone

--- fri/src/two_adic_pcs.rs
+++ fri/src/two_adic_pcs.rs
@@ -52,7 +52,7 @@ pub enum VerificationError<InputMmcsError, FriMmcsError> {
     FriError(FriError<FriMmcsError>),
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 #[serde(bound = "")]
 pub struct TwoAdicFriPcsProof<
     Val: Field,
@@ -65,7 +65,7 @@ pub struct TwoAdicFriPcsProof<
     pub query_openings: Vec<Vec<BatchOpening<Val, InputMmcs>>>,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 #[serde(bound = "")]
 pub struct BatchOpening<Val: Field, InputMmcs: Mmcs<Val>> {
     pub opened_values: Vec<Vec<Val>>,
