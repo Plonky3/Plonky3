bc2c4aa143a3c9e02ebdefe138e274f8793f243e
AngusG <angusgruen@gmail.com>
2024-05-29 21:33:49 -0400

    Creating a collection of functions which encapsulate the Packed field testing we do.
    Next step is to make some macro's...

--- field-testing/src/packedfield_testing.rs
+++ field-testing/src/packedfield_testing.rs
@@ -10,7 +10,7 @@ where
     fn array_from_valid_reps(vals: [u32; WIDTH]) -> [F; WIDTH];
     fn packed_from_valid_reps(vals: [u32; WIDTH]) -> PF;
     fn array_from_random(seed: u64) -> [F; WIDTH];
-    fn packed_from_random(seed: u64);
+    fn packed_from_random(seed: u64) -> PF;
     const SPECIAL_VALS: [F; WIDTH];
 }
 
@@ -68,415 +68,147 @@ where
     assert_eq!(res1, expected1);
 }
 
-// #[test]
-// fn test_add_associative() {
-//     let vec0 = packed_from_random(0x8b078c2b693c893f);
-//     let vec1 = packed_from_random(0x4ff5dec04791e481);
-//     let vec2 = packed_from_random(0x5806c495e9451f8e);
-
-//     let res0 = (vec0 + vec1) + vec2;
-//     let res1 = vec0 + (vec1 + vec2);
-
-//     assert_eq!(res0, res1);
-// }
-
-// #[test]
-// fn test_add_commutative() {
-//     let vec0 = packed_from_random(0xe1bf9cac02e9072a);
-//     let vec1 = packed_from_random(0xb5061e7de6a6c677);
-
-//     let res0 = vec0 + vec1;
-//     let res1 = vec1 + vec0;
-
-//     assert_eq!(res0, res1);
-// }
-
-// #[test]
-// fn test_additive_identity_right() {
-//     let vec = packed_from_random(0xbcd56facf6a714b5);
-//     let res = vec + P::zero();
-//     assert_eq!(res, vec);
-// }
-
-// #[test]
-// fn test_additive_identity_left() {
-//     let vec = packed_from_random(0xb614285cd641233c);
-//     let res = P::zero() + vec;
-//     assert_eq!(res, vec);
-// }
-
-// #[test]
-// fn test_additive_inverse_add_neg() {
-//     let vec = packed_from_random(0x4b89c8d023c9c62e);
-//     let neg_vec = -vec;
-//     let res = vec + neg_vec;
-//     assert_eq!(res, P::zero());
-// }
-
-// #[test]
-// fn test_additive_inverse_sub() {
-//     let vec = packed_from_random(0x2c94652ee5561341);
-//     let res = vec - vec;
-//     assert_eq!(res, P::zero());
-// }
-
-// #[test]
-// fn test_sub_anticommutative() {
-//     let vec0 = packed_from_random(0xf3783730a14b460e);
-//     let vec1 = packed_from_random(0x5b6f827a023525ee);
-
-//     let res0 = vec0 - vec1;
-//     let res1 = -(vec1 - vec0);
-
-//     assert_eq!(res0, res1);
-// }
-
-// #[test]
-// fn test_sub_zero() {
-//     let vec = packed_from_random(0xc1a526f8226ec1e5);
-//     let res = vec - P::zero();
-//     assert_eq!(res, vec);
-// }
-
-// #[test]
-// fn test_zero_sub() {
-//     let vec = packed_from_random(0x4444b9c090519333);
-//     let res0 = P::zero() - vec;
-//     let res1 = -vec;
-//     assert_eq!(res0, res1);
-// }
-
-// #[test]
-// fn test_neg_own_inverse() {
-//     let vec = packed_from_random(0xee4df174b850a35f);
-//     let res = -(-vec);
-//     assert_eq!(res, vec);
-// }
-
-// #[test]
-// fn test_sub_is_add_neg() {
-//     let vec0 = packed_from_random(0x18f4b5c3a08e49fe);
-//     let vec1 = packed_from_random(0x39bd37a1dc24d492);
-//     let res0 = vec0 - vec1;
-//     let res1 = vec0 + (-vec1);
-//     assert_eq!(res0, res1);
-// }
-
-// #[test]
-// fn test_mul_associative() {
-//     let vec0 = packed_from_random(0x0b1ee4d7c979d50c);
-//     let vec1 = packed_from_random(0x39faa0844a36e45a);
-//     let vec2 = packed_from_random(0x08fac4ee76260e44);
-
-//     let res0 = (vec0 * vec1) * vec2;
-//     let res1 = vec0 * (vec1 * vec2);
-
-//     assert_eq!(res0, res1);
-// }
-
-// #[test]
-// fn test_mul_commutative() {
-//     let vec0 = packed_from_random(0x10debdcbd409270c);
-//     let vec1 = packed_from_random(0x927bc073c1c92b2f);
-
-//     let res0 = vec0 * vec1;
-//     let res1 = vec1 * vec0;
-
-//     assert_eq!(res0, res1);
-// }
-
-// #[test]
-// fn test_multiplicative_identity_right() {
-//     let vec = packed_from_random(0xdf0a646b6b0c2c36);
-//     let res = vec * P::one();
-//     assert_eq!(res, vec);
-// }
-
-// #[test]
-// fn test_multiplicative_identity_left() {
-//     let vec = packed_from_random(0x7b4d890bf7a38bd2);
-//     let res = P::one() * vec;
-//     assert_eq!(res, vec);
-// }
-
-// #[test]
-// fn test_multiplicative_inverse() {
-//     let arr = array_from_random(0xb0c7a5153103c5a8);
-//     let arr_inv = arr.map(|x| x.inverse());
-
-//     let vec = PackedKoalaBearAVX2(arr);
-//     let vec_inv = PackedKoalaBearAVX2(arr_inv);
-
-//     let res = vec * vec_inv;
-//     assert_eq!(res, P::one());
-// }
-
-// #[test]
-// fn test_mul_zero() {
-//     let vec = packed_from_random(0x7f998daa72489bd7);
-//     let res = vec * P::zero();
-//     assert_eq!(res, P::zero());
-// }
-
-// #[test]
-// fn test_zero_mul() {
-//     let vec = packed_from_random(0x683bc2dd355b06e5);
-//     let res = P::zero() * vec;
-//     assert_eq!(res, P::zero());
-// }
-
-// #[test]
-// fn test_mul_negone() {
-//     let vec = packed_from_random(0x97cb9670a8251202);
-//     let res0 = vec * P::neg_one();
-//     let res1 = -vec;
-//     assert_eq!(res0, res1);
-// }
-
-// #[test]
-// fn test_negone_mul() {
-//     let vec = packed_from_random(0xadae69873b5d3baf);
-//     let res0 = P::neg_one() * vec;
-//     let res1 = -vec;
-//     assert_eq!(res0, res1);
-// }
-
-// #[test]
-// fn test_neg_distributivity_left() {
-//     let vec0 = packed_from_random(0xd0efd6f272c7de93);
-//     let vec1 = packed_from_random(0xd5dd2cf5e76dd694);
-
-//     let res0 = vec0 * -vec1;
-//     let res1 = -(vec0 * vec1);
-
-//     assert_eq!(res0, res1);
-// }
-
-// #[test]
-// fn test_neg_distributivity_right() {
-//     let vec0 = packed_from_random(0x0da9b03cd4b79b09);
-//     let vec1 = packed_from_random(0x9964d3f4beaf1857);
-
-//     let res0 = -vec0 * vec1;
-//     let res1 = -(vec0 * vec1);
-
-//     assert_eq!(res0, res1);
-// }
-
-// #[test]
-// fn test_add_distributivity_left() {
-//     let vec0 = packed_from_random(0x278d9e202925a1d1);
-//     let vec1 = packed_from_random(0xf04cbac0cbad419f);
-//     let vec2 = packed_from_random(0x76976e2abdc5a056);
-
-//     let res0 = vec0 * (vec1 + vec2);
-//     let res1 = vec0 * vec1 + vec0 * vec2;
-
-//     assert_eq!(res0, res1);
-// }
-
-// #[test]
-// fn test_add_distributivity_right() {
-//     let vec0 = packed_from_random(0xbe1b606eafe2a2b8);
-//     let vec1 = packed_from_random(0x552686a0978ab571);
-//     let vec2 = packed_from_random(0x36f6eec4fd31a460);
-
-//     let res0 = (vec0 + vec1) * vec2;
-//     let res1 = vec0 * vec2 + vec1 * vec2;
-
-//     assert_eq!(res0, res1);
-// }
-
-// #[test]
-// fn test_sub_distributivity_left() {
-//     let vec0 = packed_from_random(0x817d4a27febb0349);
-//     let vec1 = packed_from_random(0x1eaf62a921d6519b);
-//     let vec2 = packed_from_random(0xfec0fb8d3849465a);
-
-//     let res0 = vec0 * (vec1 - vec2);
-//     let res1 = vec0 * vec1 - vec0 * vec2;
-
-//     assert_eq!(res0, res1);
-// }
-
-// #[test]
-// fn test_sub_distributivity_right() {
-//     let vec0 = packed_from_random(0x5a4a82e8e2394585);
-//     let vec1 = packed_from_random(0x6006b1443a22b102);
-//     let vec2 = packed_from_random(0x5a22deac65fcd454);
-
-//     let res0 = (vec0 - vec1) * vec2;
-//     let res1 = vec0 * vec2 - vec1 * vec2;
-
-//     assert_eq!(res0, res1);
-// }
-
-// #[test]
-// fn test_one_plus_one() {
-//     assert_eq!(P::one() + P::one(), P::two());
-// }
-
-// #[test]
-// fn test_negone_plus_two() {
-//     assert_eq!(P::neg_one() + P::two(), P::one());
-// }
-
-// #[test]
-// fn test_double() {
-//     let vec = packed_from_random(0x2e61a907650881e9);
-//     let res0 = P::two() * vec;
-//     let res1 = vec + vec;
-//     assert_eq!(res0, res1);
-// }
-
-// #[test]
-// fn test_add_vs_scalar() {
-//     let arr0 = array_from_random(0xac23b5a694dabf70);
-//     let arr1 = array_from_random(0xd249ec90e8a6e733);
-
-//     let vec0 = PackedKoalaBearAVX2(arr0);
-//     let vec1 = PackedKoalaBearAVX2(arr1);
-//     let vec_res = vec0 + vec1;
-
-//     for i in 0..WIDTH {
-//         assert_eq!(vec_res.0[i], arr0[i] + arr1[i]);
-//     }
-// }
-
-// #[test]
-// fn test_add_vs_scalar_special_vals_left() {
-//     let arr0 = SPECIAL_VALS;
-//     let arr1 = array_from_random(0x1e2b153f07b64cf3);
-
-//     let vec0 = PackedKoalaBearAVX2(arr0);
-//     let vec1 = PackedKoalaBearAVX2(arr1);
-//     let vec_res = vec0 + vec1;
-
-//     for i in 0..WIDTH {
-//         assert_eq!(vec_res.0[i], arr0[i] + arr1[i]);
-//     }
-// }
-
-// #[test]
-// fn test_add_vs_scalar_special_vals_right() {
-//     let arr0 = array_from_random(0xfcf974ac7625a260);
-//     let arr1 = SPECIAL_VALS;
-
-//     let vec0 = PackedKoalaBearAVX2(arr0);
-//     let vec1 = PackedKoalaBearAVX2(arr1);
-//     let vec_res = vec0 + vec1;
-
-//     for i in 0..WIDTH {
-//         assert_eq!(vec_res.0[i], arr0[i] + arr1[i]);
-//     }
-// }
-
-// #[test]
-// fn test_sub_vs_scalar() {
-//     let arr0 = array_from_random(0x167ce9d8e920876e);
-//     let arr1 = array_from_random(0x52ddcdd3461e046f);
-
-//     let vec0 = PackedKoalaBearAVX2(arr0);
-//     let vec1 = PackedKoalaBearAVX2(arr1);
-//     let vec_res = vec0 - vec1;
-
-//     for i in 0..WIDTH {
-//         assert_eq!(vec_res.0[i], arr0[i] - arr1[i]);
-//     }
-// }
-
-// #[test]
-// fn test_sub_vs_scalar_special_vals_left() {
-//     let arr0 = SPECIAL_VALS;
-//     let arr1 = array_from_random(0x358498640bfe1375);
-
-//     let vec0 = PackedKoalaBearAVX2(arr0);
-//     let vec1 = PackedKoalaBearAVX2(arr1);
-//     let vec_res = vec0 - vec1;
-
-//     for i in 0..WIDTH {
-//         assert_eq!(vec_res.0[i], arr0[i] - arr1[i]);
-//     }
-// }
-
-// #[test]
-// fn test_sub_vs_scalar_special_vals_right() {
-//     let arr0 = array_from_random(0x05d81ebfb8f0005c);
-//     let arr1 = SPECIAL_VALS;
-
-//     let vec0 = PackedKoalaBearAVX2(arr0);
-//     let vec1 = PackedKoalaBearAVX2(arr1);
-//     let vec_res = vec0 - vec1;
-
-//     for i in 0..WIDTH {
-//         assert_eq!(vec_res.0[i], arr0[i] - arr1[i]);
-//     }
-// }
-
-// #[test]
-// fn test_mul_vs_scalar() {
-//     let arr0 = array_from_random(0x4242ebdc09b74d77);
-//     let arr1 = array_from_random(0x9937b275b3c056cd);
-
-//     let vec0 = PackedKoalaBearAVX2(arr0);
-//     let vec1 = PackedKoalaBearAVX2(arr1);
-//     let vec_res = vec0 * vec1;
-
-//     for i in 0..WIDTH {
-//         assert_eq!(vec_res.0[i], arr0[i] * arr1[i]);
-//     }
-// }
-
-// #[test]
-// fn test_mul_vs_scalar_special_vals_left() {
-//     let arr0 = SPECIAL_VALS;
-//     let arr1 = array_from_random(0x5285448b835458a3);
-
-//     let vec0 = PackedKoalaBearAVX2(arr0);
-//     let vec1 = PackedKoalaBearAVX2(arr1);
-//     let vec_res = vec0 * vec1;
-
-//     for i in 0..WIDTH {
-//         assert_eq!(vec_res.0[i], arr0[i] * arr1[i]);
-//     }
-// }
-
-// #[test]
-// fn test_mul_vs_scalar_special_vals_right() {
-//     let arr0 = array_from_random(0x22508dc80001d865);
-//     let arr1 = SPECIAL_VALS;
-
-//     let vec0 = PackedKoalaBearAVX2(arr0);
-//     let vec1 = PackedKoalaBearAVX2(arr1);
-//     let vec_res = vec0 * vec1;
-
-//     for i in 0..WIDTH {
-//         assert_eq!(vec_res.0[i], arr0[i] * arr1[i]);
-//     }
-// }
-
-// #[test]
-// fn test_neg_vs_scalar() {
-//     let arr = array_from_random(0xc3c273a9b334372f);
-
-//     let vec = PackedKoalaBearAVX2(arr);
-//     let vec_res = -vec;
-
-//     #[allow(clippy::needless_range_loop)]
-//     for i in 0..WIDTH {
-//         assert_eq!(vec_res.0[i], -arr[i]);
-//     }
-// }
-
-// #[test]
-// fn test_neg_vs_scalar_special_vals() {
-//     let arr = SPECIAL_VALS;
-
-//     let vec = PackedKoalaBearAVX2(arr);
-//     let vec_res = -vec;
-
-//     #[allow(clippy::needless_range_loop)]
-//     for i in 0..WIDTH {
-//         assert_eq!(vec_res.0[i], -arr[i]);
-//     }
-// }
+#[allow(clippy::eq_op)]
+pub fn test_add_neg<const WIDTH: usize, F, PF, PTH>()
+where
+    F: Field,
+    PF: PackedField<Scalar = F> + PackedValue + Eq,
+    PTH: PackedTestingHelpers<WIDTH, F, PF>,
+{
+    let vec0 = PTH::packed_from_random(0x8b078c2b693c893f);
+    let vec1 = PTH::packed_from_random(0x4ff5dec04791e481);
+    let vec2 = PTH::packed_from_random(0x5806c495e9451f8e);
+
+    assert_eq!((vec0 + vec1) + vec2, vec0 + (vec1 + vec2), "Error when testing associativity of add.");
+    assert_eq!(vec0 + vec1, vec1 + vec0, "Error when testing commutativity of add.");
+    assert_eq!(vec0, vec0 + PF::zero(), "Error when testing additive identity right.");
+    assert_eq!(vec0, PF::zero() + vec0, "Error when testing additive identity left.");
+    assert_eq!(vec0 + (-vec0), PF::zero(), "Error when testing additive inverse.");
+    assert_eq!(vec0 - vec0, PF::zero(), "Error when testing subtracting of self.");
+    assert_eq!(vec0 - vec1, -(vec1 - vec0), "Error when testing anticommutativity of sub.");
+    assert_eq!(vec0, vec0 - PF::zero(), "Error when testing subtracting zero.");
+    assert_eq!(-vec0, PF::zero() - vec0, "Error when testing subtracting from zero");
+    assert_eq!(vec0, -(-vec0), "Error when testing double negation");
+    assert_eq!(vec0 - vec1, vec0 + (-vec1), "Error when testing addition of negation");
+    assert_eq!(PF::one() + PF::one(), PF::two(), "Error 1 + 1 =/= 2");
+    assert_eq!(PF::neg_one() + PF::two(), PF::one(), "Error -1 + 2 =/= 1");
+    assert_eq!(vec0.double(), vec0 + vec0, "Error when comparing x.double() to x + x");
+}
+
+
+#[allow(clippy::eq_op)]
+pub fn test_mul<const WIDTH: usize, F, PF, PTH>()
+where
+    F: Field,
+    PF: PackedField<Scalar = F> + PackedValue + Eq,
+    PTH: PackedTestingHelpers<WIDTH, F, PF>,
+{
+    let vec0 = PTH::packed_from_random(0x0b1ee4d7c979d50c);
+    let vec1 = PTH::packed_from_random(0x39faa0844a36e45a);
+    let vec2 = PTH::packed_from_random(0x08fac4ee76260e44);
+
+    assert_eq!((vec0 * vec1) * vec2, vec0 * (vec1 * vec2), "Error when testing associativity of mul.");
+    assert_eq!(vec0 * vec1, vec1 * vec0, "Error when testing commutativity of mul.");
+    assert_eq!(vec0, vec0 * PF::one(), "Error when testing multiplicative identity right.");
+    assert_eq!(vec0, PF::one() *  vec0, "Error when testing multiplicative identity left.");
+    assert_eq!(vec0 * PF::zero(), PF::zero(), "Error when testing right multiplication by 0.");
+    assert_eq!(PF::zero() * vec0, PF::zero(), "Error when testing left multiplication by 0.");
+    assert_eq!(vec0 * PF::neg_one(), -(vec0), "Error when testing right multiplication by -1.");
+    assert_eq!(PF::neg_one() * vec0, -(vec0), "Error when testing left multiplication by -1.");
+    assert_eq!(vec0.double(), PF::two() * vec0, "Error when comparing x.double() to 2 * x");
+}
+
+#[allow(clippy::eq_op)]
+pub fn test_distributivity<const WIDTH: usize, F, PF, PTH>()
+where
+    F: Field,
+    PF: PackedField<Scalar = F> + PackedValue + Eq,
+    PTH: PackedTestingHelpers<WIDTH, F, PF>,
+{
+    let vec0 = PTH::packed_from_random(0x278d9e202925a1d1);
+    let vec1 = PTH::packed_from_random(0xf04cbac0cbad419f);
+    let vec2 = PTH::packed_from_random(0x76976e2abdc5a056);
+
+    assert_eq!(vec0 * (-vec1), -(vec0 * vec1), "Error when testing distributivity of mul and right neg.");
+    assert_eq!((-vec0) * vec1, -(vec0 * vec1), "Error when testing distributivity of mul and left neg.");
+
+    assert_eq!(vec0 * (vec1 + vec2), vec0 * vec1 + vec0 * vec2, "Error when testing distributivity of add and left mul.");
+    assert_eq!((vec0 + vec1) * vec2, vec0 * vec2 + vec1 * vec2, "Error when testing distributivity of add and right mul.");
+    assert_eq!(vec0 * (vec1 - vec2), vec0 * vec1 - vec0 * vec2, "Error when testing distributivity of sub and left mul.");
+    assert_eq!((vec0 - vec1) * vec2, vec0 * vec2 - vec1 * vec2, "Error when testing distributivity of sub and right mul.");
+}
+
+#[allow(clippy::eq_op)]
+pub fn test_vs_scalar<const WIDTH: usize, F, PF, PTH>()
+where
+    F: Field,
+    PF: PackedField<Scalar = F> + PackedValue + Eq,
+    PTH: PackedTestingHelpers<WIDTH, F, PF>,
+{
+    let arr0 = PTH::array_from_random(0x278d9e202925a1d1);
+    let arr1 = PTH::array_from_random(0xf04cbac0cbad419f);
+
+    let vec0 = *(PF::from_slice(&arr0));
+    let vec1 = *(PF::from_slice(&arr1));
+    let vec_special = *(PF::from_slice(&PTH::SPECIAL_VALS));
+
+    let vec_sum = vec0 + vec1;
+    let arr_sum = vec_sum.as_slice();
+    let vec_special_sum_left = vec_special + vec0;
+    let arr_special_sum_left = vec_special_sum_left.as_slice();
+    let vec_special_sum_right = vec1 + vec_special;
+    let arr_special_sum_right = vec_special_sum_right.as_slice();
+
+    let vec_sub = vec0 - vec1;
+    let arr_sub = vec_sub.as_slice();
+    let vec_special_sub_left = vec_special - vec0;
+    let arr_special_sub_left = vec_special_sub_left.as_slice();
+    let vec_special_sub_right = vec1 - vec_special;
+    let arr_special_sub_right = vec_special_sub_right.as_slice();
+
+    let vec_mul = vec0 * vec1;
+    let arr_mul = vec_mul.as_slice();
+    let vec_special_mul_left = vec_special * vec0;
+    let arr_special_mul_left = vec_special_mul_left.as_slice();
+    let vec_special_mul_right = vec1 * vec_special;
+    let arr_special_mul_right = vec_special_mul_right.as_slice();
+
+    let vec_neg = -vec0;
+    let arr_neg = vec_neg.as_slice();
+    let vec_special_neg = -vec_special;
+    let arr_special_neg = vec_special_neg.as_slice();
+
+    for i in 0..WIDTH {
+        assert_eq!(arr_sum[i], arr0[i] + arr1[i]);
+        assert_eq!(arr_special_sum_left[i], PTH::SPECIAL_VALS[i] + arr0[i]);
+        assert_eq!(arr_special_sum_right[i], arr1[i] + PTH::SPECIAL_VALS[i]);
+
+        assert_eq!(arr_sub[i], arr0[i] - arr1[i]);
+        assert_eq!(arr_special_sub_left[i], PTH::SPECIAL_VALS[i] - arr0[i]);
+        assert_eq!(arr_special_sub_right[i], arr1[i] - PTH::SPECIAL_VALS[i]);
+
+        assert_eq!(arr_mul[i], arr0[i] * arr1[i]);
+        assert_eq!(arr_special_mul_left[i], PTH::SPECIAL_VALS[i] * arr0[i]);
+        assert_eq!(arr_special_mul_right[i], arr1[i] * PTH::SPECIAL_VALS[i]);
+
+        assert_eq!(arr_neg[i], -arr0[i]);
+        assert_eq!(arr_special_neg[i], -PTH::SPECIAL_VALS[i]);
+    }
+}
+
+pub fn test_multiplicative_inverse<const WIDTH: usize, F, PF, PTH>()
+    where
+        F: Field,
+        PF: PackedField<Scalar = F> + PackedValue + Eq,
+        PTH: PackedTestingHelpers<WIDTH, F, PF>,
+    {
+
+    let arr = PTH::array_from_random(0xb0c7a5153103c5a8);
+    let arr_inv = arr.map(|x| x.inverse());
+
+    let vec = *(PF::from_slice(&arr));
+    let vec_inv = *(PF::from_slice(&arr_inv));
+
+    let res = vec * vec_inv;
+    assert_eq!(res, PF::one());
+}
