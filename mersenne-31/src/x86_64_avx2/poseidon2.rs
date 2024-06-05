ce4949a997859795d60cdb89047a282374f6d873
AngusG <angusgruen@gmail.com>
2024-06-04 23:21:45 -0400

    Vectorizing Poseidon2, initial work.
    Setting up functions to do the External layers efficiently.

--- mersenne-31/src/x86_64_avx2/poseidon2.rs
+++ mersenne-31/src/x86_64_avx2/poseidon2.rs
@@ -1,3 +1,5 @@
+use core::arch::x86_64::{self, __m256i};
+use core::mem::transmute;
 use p3_poseidon2::{matmul_internal, DiffusionPermutation};
 use p3_symmetric::Permutation;
 
@@ -27,6 +29,125 @@ impl Permutation<[PackedMersenne31AVX2; 24]> for DiffusionMatrixMersenne31 {
 }
 
 impl DiffusionPermutation<PackedMersenne31AVX2, 24> for DiffusionMatrixMersenne31 {}
+
+/// A 4x4 Matrix of M31 elements with each element stored in 64-bits and each row saved as 256bit packed vector.
+#[derive(Clone, Copy, Debug)]
+pub struct Packed64bitM31Matrix(__m256i, __m256i, __m256i, __m256i);
+
+/// Compute the transpose of the given matrix.
+fn _transpose(x: Packed64bitM31Matrix) -> Packed64bitM31Matrix {
+    unsafe {
+        // Safety: If this code got compiled then AVX2 intrinsics are available.
+        let i0 = x86_64::_mm256_unpacklo_epi64(x.0, x.1);
+        let i1 = x86_64::_mm256_unpackhi_epi64(x.0, x.1);
+        let i2 = x86_64::_mm256_unpacklo_epi64(x.2, x.3);
+        let i3 = x86_64::_mm256_unpackhi_epi64(x.2, x.3);
+
+        let y0 = x86_64::_mm256_permute2x128_si256::<0x20>(i0, i2);
+        let y1 = x86_64::_mm256_permute2x128_si256::<0x20>(i1, i3);
+        let y2 = x86_64::_mm256_permute2x128_si256::<0x31>(i0, i2);
+        let y3 = x86_64::_mm256_permute2x128_si256::<0x31>(i1, i3);
+        Packed64bitM31Matrix(y0, y1, y2, y3)
+    }
+}
+
+/// Left Multiply by the AES matrix:
+/// [ 2 3 1 1 ]
+/// [ 1 2 3 1 ]
+/// [ 1 1 2 3 ]
+/// [ 3 1 1 2 ].
+fn _mat_mul_aes(x: Packed64bitM31Matrix) -> Packed64bitM31Matrix {
+    unsafe {
+        // Safety: If the inputs are <= L, the outputs are <= 7L.
+        // Hence if L < 2^61, overflow will not occur.
+        let t01 = x86_64::_mm256_add_epi64(x.0, x.1);
+        let t23 = x86_64::_mm256_add_epi64(x.2, x.3);
+        let t0123 = x86_64::_mm256_add_epi64(t01, t23);
+        let t01123 = x86_64::_mm256_add_epi64(t0123, x.1);
+        let t01233 = x86_64::_mm256_add_epi64(t0123, x.3);
+
+        let t00 = x86_64::_mm256_slli_epi64::<1>(x.0);
+        let t22 = x86_64::_mm256_slli_epi64::<1>(x.2);
+
+        let y0 = x86_64::_mm256_add_epi64(t01, t01123);
+        let y1 = x86_64::_mm256_add_epi64(t22, t01123);
+        let y2 = x86_64::_mm256_add_epi64(t23, t01233);
+        let y3 = x86_64::_mm256_add_epi64(t00, t01233);
+        Packed64bitM31Matrix(y0, y1, y2, y3)
+    }
+}
+
+/// Left Multiply by the matrix I + 1:
+/// [ 2 1 1 1 ]
+/// [ 1 2 1 1 ]
+/// [ 1 1 2 1 ]
+/// [ 1 1 1 2 ].
+fn _mat_mul_i_plus_1(x: Packed64bitM31Matrix) -> Packed64bitM31Matrix {
+    unsafe {
+        // Safety: If the inputs are <= L, the outputs are <= 5L.
+        // Hence if L < 2^61, overflow will not occur.
+        let t01 = x86_64::_mm256_add_epi64(x.0, x.1);
+        let t23 = x86_64::_mm256_add_epi64(x.2, x.3);
+        let t0123 = x86_64::_mm256_add_epi64(t01, t23);
+
+        let y0 = x86_64::_mm256_add_epi64(x.0, t0123);
+        let y1 = x86_64::_mm256_add_epi64(x.1, t0123);
+        let y2 = x86_64::_mm256_add_epi64(x.2, t0123);
+        let y3 = x86_64::_mm256_add_epi64(x.3, t0123);
+        Packed64bitM31Matrix(y0, y1, y2, y3)
+    }
+}
+
+/// Do a single round of M31 reduction on each element.
+fn _reduce(x: __m256i) -> __m256i {
+    unsafe {
+        // Safety: If inputs are < L, output is < max(2^{-30}L, 2^32)
+
+        // Get the high 31 bits shifted down
+        let high_bits_lo = x86_64::_mm256_srli_epi64::<31>(x);
+
+        // Add the high bits back to the value
+        let input_plus_high_lo = x86_64::_mm256_add_epi64(x, high_bits_lo);
+
+        // Clear the bottom 31 bits of the x's
+        let high_bits_hi = x86_64::_mm256_srli_epi64::<31>(high_bits_lo);
+
+        // subtract off the high bits
+        x86_64::_mm256_sub_epi64(input_plus_high_lo, high_bits_hi)
+    }
+}
+
+/// Computex x -> x^5 for each element of the vector.
+/// The input must be in canoncial form.
+/// The output will not be in canonical form.
+fn _joint_sbox(x: __m256i) -> __m256i {
+    unsafe {
+        // Safety: If input is in canonical form, no overflow will occur and the output will be < 2^33.
+
+        const P: __m256i = unsafe { transmute::<[u64; 4], _>([0x7fffffff; 4]) };
+
+        // Square x. If it starts in canoncical form then x^2 < 2^62
+        let x2 = x86_64::_mm256_mul_epu32(x, x);
+
+        // Reduce and then subtract P. The result will then lie in (-2^31, 2^31).
+        let x2_red = _reduce(x2);
+        let x2_red_sub_p = x86_64::_mm256_sub_epi64(x2_red, P);
+
+        // Square again. The result is again < 2^62.
+        let x4 = x86_64::_mm256_mul_epi32(x2_red_sub_p, x2_red_sub_p);
+
+        // Reduce again so the result is < 2^32
+        let x4_red = _reduce(x4);
+
+        // Now when we multiply our result is < 2^63
+        let x5 = x86_64::_mm256_mul_epu32(x, x4_red);
+
+        // Now we reduce again and return the result which is < 2^33.
+        _reduce(x5)
+    }
+}
+
+/// External Poseidon Rounds
 
 #[cfg(test)]
 mod tests {
