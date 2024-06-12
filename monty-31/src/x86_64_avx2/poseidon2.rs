7a0cd8b16bc151512889919399dab4cb6bb66415
AngusG <angusgruen@gmail.com>
2024-06-12 16:45:50 -0400

    Cleaning up impls for monty31 packed fields

--- monty-31/src/x86_64_avx2/poseidon2.rs
+++ monty-31/src/x86_64_avx2/poseidon2.rs
@@ -2,19 +2,21 @@ use p3_poseidon2::{matmul_internal, DiffusionPermutation};
 use p3_symmetric::Permutation;
 
 use crate::{
-    DiffusionMatrixMontyField31, FieldParameters, MontyField31, MultipleDiffusionMatrixParameters,
-    PackedMontyField31AVX2,
+    DiffusionMatrixMontyField31, DiffusionMatrixParameters, FieldParameters, MontyField31,
+    MultipleDiffusionMatrixParameters, PackedMontyField31AVX2,
 };
 
 // We need to change from the standard implementation as we are interpreting the matrix (1 + D(v)) as the monty form of the matrix not the raw form.
 // matmul_internal internal performs a standard matrix multiplication so we need to additional rescale by the inverse monty constant.
 // These will be removed once we have architecture specific implementations.
 
-impl<FP: FieldParameters, MD: MultipleDiffusionMatrixParameters<FP>>
-    Permutation<[PackedMontyField31AVX2<FP>; 16]> for DiffusionMatrixMontyField31<FP, MD>
+impl<FP: FieldParameters, const WIDTH: usize, MD: MultipleDiffusionMatrixParameters<FP>>
+    Permutation<[PackedMontyField31AVX2<FP>; WIDTH]> for DiffusionMatrixMontyField31<FP, MD>
+where
+    MD: DiffusionMatrixParameters<FP, WIDTH>,
 {
-    fn permute_mut(&self, state: &mut [PackedMontyField31AVX2<FP>; 16]) {
-        matmul_internal::<MontyField31<FP>, PackedMontyField31AVX2<FP>, 16>(
+    fn permute_mut(&self, state: &mut [PackedMontyField31AVX2<FP>; WIDTH]) {
+        matmul_internal::<MontyField31<FP>, PackedMontyField31AVX2<FP>, WIDTH>(
             state,
             MD::INTERNAL_DIAG_MONTY,
         );
@@ -22,24 +24,9 @@ impl<FP: FieldParameters, MD: MultipleDiffusionMatrixParameters<FP>>
     }
 }
 
-impl<FP: FieldParameters, MD: MultipleDiffusionMatrixParameters<FP>>
-    DiffusionPermutation<PackedMontyField31AVX2<FP>, 16> for DiffusionMatrixMontyField31<FP, MD>
-{
-}
-
-impl<FP: FieldParameters, MD: MultipleDiffusionMatrixParameters<FP>>
-    Permutation<[PackedMontyField31AVX2<FP>; 24]> for DiffusionMatrixMontyField31<FP, MD>
-{
-    fn permute_mut(&self, state: &mut [PackedMontyField31AVX2<FP>; 24]) {
-        matmul_internal::<MontyField31<FP>, PackedMontyField31AVX2<FP>, 24>(
-            state,
-            MD::INTERNAL_DIAG_MONTY,
-        );
-        state.iter_mut().for_each(|i| *i *= MD::MONTY_INVERSE);
-    }
-}
-
-impl<FP: FieldParameters, MD: MultipleDiffusionMatrixParameters<FP>>
-    DiffusionPermutation<PackedMontyField31AVX2<FP>, 24> for DiffusionMatrixMontyField31<FP, MD>
+impl<FP: FieldParameters, const WIDTH: usize, MD: MultipleDiffusionMatrixParameters<FP>>
+    DiffusionPermutation<PackedMontyField31AVX2<FP>, WIDTH> for DiffusionMatrixMontyField31<FP, MD>
+where
+    MD: DiffusionMatrixParameters<FP, WIDTH>,
 {
 }
