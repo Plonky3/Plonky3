66f496171a7ad2bb8fd50fb3ecf49e759d9303d3
AngusG <angusgruen@gmail.com>
2024-08-21 13:40:58 -0400

    Continuing small fixes

--- monty-31/src/poseidon2.rs
+++ monty-31/src/poseidon2.rs
@@ -1,6 +1,10 @@
 use core::marker::PhantomData;
 
-use p3_poseidon2::{ExternalLayer, InternalLayer, NoPackedImplementation};
+use p3_field::AbstractField;
+use p3_poseidon2::{
+    external_final_permute_state, external_initial_permute_state, ExternalLayer, InternalLayer,
+    MDSMat4,
+};
 
 use crate::{monty_reduce, FieldParameters, MontyField31, MontyParameters};
 
@@ -12,10 +16,6 @@ pub trait Poseidon2Parameters<FP: FieldParameters, const WIDTH: usize>: Clone + 
 
     // We only need to save the powers and can ignore the initial element.
     const INTERNAL_DIAG_SHIFTS: Self::ArrayLike;
-
-    // Long term INTERNAL_DIAG_MONTY will be removed.
-    // Currently it is needed for the Packed field implementations.
-    const INTERNAL_DIAG_MONTY: [MontyField31<FP>; WIDTH];
 
     /// Implements multiplication by the diffusion matrix 1 + Diag(vec) using a delayed reduction strategy.
     fn permute_state(state: &mut [MontyField31<FP>; WIDTH]) {
@@ -30,8 +30,6 @@ pub trait Poseidon2Parameters<FP: FieldParameters, const WIDTH: usize>: Clone + 
             state[i + 1] = MontyField31::new_monty(monty_reduce::<FP>(si));
         }
     }
-
-    fn s_box(input: MontyField31<FP>) -> MontyField31<FP>;
 }
 
 /// Some code needed by the PackedField implementation can be shared between the different WIDTHS and architectures.
@@ -41,70 +39,76 @@ pub trait PackedFieldPoseidon2Helpers<MP: MontyParameters> {
 }
 
 #[derive(Debug, Clone, Default)]
-pub struct Poseidon2InternalLayerMonty31<MP>
+pub struct Poseidon2InternalLayerMonty31<P2P>
 where
-    MP: Clone,
+    P2P: Clone,
 {
-    _phantom: PhantomData<MP>,
+    _phantom: PhantomData<P2P>,
 }
 
-impl<MP> NoPackedImplementation for Poseidon2InternalLayerMonty31<MP> where MP: Clone + Sync {}
-
-impl<FP, const WIDTH: usize, MP, const D: u64> InternalLayer<MontyField31<FP>, WIDTH, D>
-    for Poseidon2InternalLayerMonty31<MP>
+impl<FP, const WIDTH: usize, P2P, const D: u64> InternalLayer<MontyField31<FP>, WIDTH, D>
+    for Poseidon2InternalLayerMonty31<P2P>
 where
     FP: FieldParameters,
-    MP: Poseidon2Parameters<FP, WIDTH>,
+    P2P: Poseidon2Parameters<FP, WIDTH>,
 {
-    type InternalState = [MontyField31<FP>; 16];
+    type InternalState = [MontyField31<FP>; WIDTH];
 
     fn permute_state(
         &self,
         state: &mut Self::InternalState,
         internal_constants: &[MontyField31<FP>],
-        _packed_internal_constants: &[()],
+        _packed_internal_constants: &[Self::ConstantsType],
     ) {
         internal_constants.iter().for_each(|rc| {
-            state[0] = state[0] + rc;
-            state[0] = MP::s_box(state[0]);
-            MP::permute_state(state);
+            state[0] += *rc;
+            state[0] = state[0].exp_const_u64::<D>();
+            P2P::permute_state(state);
         })
     }
 }
 
 #[derive(Debug, Clone, Default)]
-pub struct Poseidon2ExternalLayerMonty31<MP>
+pub struct Poseidon2ExternalLayerMonty31<P2P, const WIDTH: usize>
 where
-    MP: Clone,
+    P2P: Clone,
 {
-    _phantom: PhantomData<MP>,
+    _phantom: PhantomData<P2P>,
 }
 
-impl<MP> NoPackedImplementation for Poseidon2ExternalLayerMonty31<MP> where MP: Clone + Sync {}
-
-impl<FP, const WIDTH: usize, MP, const D: u64> ExternalLayer<MontyField31<FP>, WIDTH, D>
-    for Poseidon2InternalLayerMonty31<MP>
+impl<FP, const WIDTH: usize, P2P, const D: u64> ExternalLayer<MontyField31<FP>, WIDTH, D>
+    for Poseidon2ExternalLayerMonty31<P2P, WIDTH>
 where
     FP: FieldParameters,
-    MP: Poseidon2Parameters<FP, WIDTH>,
+    P2P: Poseidon2Parameters<FP, WIDTH>,
 {
-    type InternalState = [MontyField31<FP>; 16];
+    type InternalState = [MontyField31<FP>; WIDTH];
 
     fn permute_state_initial(
         &self,
-        state: [MontyField31<FP>; WIDTH],
+        mut state: [MontyField31<FP>; WIDTH],
         initial_external_constants: &[[<MontyField31<FP> as p3_field::AbstractField>::F; WIDTH]],
-        initial_external_packed_constants: &[Self::ConstantsType],
+        _initial_external_packed_constants: &[Self::ConstantsType],
     ) -> Self::InternalState {
-        todo!()
+        external_initial_permute_state::<MontyField31<FP>, MDSMat4, WIDTH, 5>(
+            &mut state,
+            initial_external_constants,
+            &MDSMat4,
+        );
+        state
     }
 
     fn permute_state_final(
         &self,
-        state: Self::InternalState,
+        mut state: Self::InternalState,
         final_external_constants: &[[<MontyField31<FP> as p3_field::AbstractField>::F; WIDTH]],
-        final_external_packed_constants: &[Self::ConstantsType],
+        _final_external_packed_constants: &[Self::ConstantsType],
     ) -> [MontyField31<FP>; WIDTH] {
-        todo!()
+        external_final_permute_state::<MontyField31<FP>, MDSMat4, WIDTH, 5>(
+            &mut state,
+            final_external_constants,
+            &MDSMat4,
+        );
+        state
     }
 }
--- monty-31/src/x86_64_avx2/poseidon2.rs
+++ monty-31/src/x86_64_avx2/poseidon2.rs
@@ -1,26 +1,65 @@
-use p3_poseidon2::matmul_internal;
+use core::arch::x86_64::{self, __m256i};
+use p3_poseidon2::{
+    matmul_internal, Poseidon2ExternalPackedConstants, Poseidon2InternalPackedConstants,
+};
 use p3_symmetric::Permutation;
 
 use crate::{
-    DiffusionMatrixParameters, FieldParameters, MontyField31, PackedFieldPoseidon2Helpers,
-    PackedMontyField31AVX2, Poseidon2InternalLayerMonty31,
+    FieldParameters, MontyField31, MontyParameters, PackedFieldPoseidon2Helpers,
+    PackedMontyField31AVX2, Poseidon2ExternalLayerMonty31, Poseidon2InternalLayerMonty31,
+    Poseidon2Parameters,
 };
 
-// We need to change from the standard implementation as we are interpreting the matrix (1 + Diag(vec)) as the monty form of the matrix not the raw form.
-// matmul_internal internal performs a standard matrix multiplication so we need to additional rescale by the inverse monty constant.
-// These will be removed once we have architecture specific implementations.
+/// A trait containing the specific information needed to
+/// implement the Poseidon2 Permutation for Monty31 Fields.
+pub trait PackedPoseidon2Parameters<FP: FieldParameters, const WIDTH: usize>: Clone + Sync {
+    /// Implements multiplication by the diffusion matrix 1 + Diag(vec) using a delayed reduction strategy.
+    fn internal_shifts(state: &mut [MontyField31<FP>; WIDTH]);
 
-impl<FP, const WIDTH: usize, MP> Permutation<[PackedMontyField31AVX2<FP>; WIDTH]>
-    for Poseidon2InternalLayerMonty31<MP>
+    fn s_box_plus_rc(input: MontyField31<FP>) -> MontyField31<FP>;
+
+    // Possibly an extra couple of things? s_box internal/external might be different.
+}
+
+/// Convert elements from the standard form {0, ..., P} to {-P, ..., 0} and copy into a vector
+fn convert_to_vec_neg_form<MP: MontyParameters>(input: i32) -> __m256i {
+    let input_sub_p = input - (MP::PRIME as i32);
+    unsafe {
+        // Safety: If this code got compiled then AVX2 intrinsics are available.
+        x86_64::_mm256_set1_epi32(input_sub_p)
+    }
+}
+
+/// We save the round constants in the {-P, ..., 0} representation instead of the standard
+/// {0, ..., P} one. This saves several instructions later.
+impl<P2P, MP> Poseidon2InternalPackedConstants<MontyField31<MP>>
+    for Poseidon2InternalLayerMonty31<P2P>
 where
-    FP: FieldParameters,
-    MP: DiffusionMatrixParameters<FP, WIDTH> + PackedFieldPoseidon2Helpers<FP>,
+    P2P: Clone + Sync,
+    MP: MontyParameters,
 {
-    fn permute_mut(&self, state: &mut [PackedMontyField31AVX2<FP>; WIDTH]) {
-        matmul_internal::<MontyField31<FP>, PackedMontyField31AVX2<FP>, WIDTH>(
-            state,
-            MP::INTERNAL_DIAG_MONTY,
-        );
-        state.iter_mut().for_each(|i| *i *= MP::MONTY_INVERSE);
+    type ConstantsType = __m256i;
+
+    fn convert_from_field(internal_constant: &MontyField31<MP>) -> Self::ConstantsType {
+        convert_to_vec_neg_form::<MP>(internal_constant.value as i32)
+    }
+}
+
+/// We save the round constants in the {-P, ..., 0} representation instead of the standard
+/// {0, ..., P} one. This saves several instructions later.
+impl<P2P, MP, const WIDTH: usize> Poseidon2ExternalPackedConstants<MontyField31<MP>, WIDTH>
+    for Poseidon2ExternalLayerMonty31<P2P, WIDTH>
+where
+    P2P: Clone + Sync,
+    MP: MontyParameters,
+{
+    type ConstantsType = [__m256i; WIDTH];
+
+    /// Convert elements from the standard form {0, ..., P} to {-P, ..., 0}.
+    fn convert_from_field_array(
+        external_constants: &[MontyField31<MP>; WIDTH],
+    ) -> [__m256i; WIDTH] {
+        external_constants
+            .map(|external_constant| convert_to_vec_neg_form::<MP>(external_constant.value as i32))
     }
 }
