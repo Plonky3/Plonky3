ee29aa2f57c834647dea3aeeb8e298e14fa64c5d
AngusG <angusgruen@gmail.com>
2024-08-22 12:30:30 -0400

    Adding custom AVX2 implementations for x^3, x^5 and x^7 for Monty31Fields.
    This is part of the work to speed up Poseidon2 which can be done immediately without needing any API changes. At some point I'll implement this for AVX512/NEON but there is no need to wait for that in order to merge this.
    
    Looking at GodBolt: https://godbolt.org/z/Ksx1z1Whe we see that in pure number of instructions, we get a little less than a 10% improvement for x^3 and this climbs to a 20% improvement for x^7.
    
    As x^3 and x^7 are the sbox used by KoalaBear and BabyBear respectively for their Poseidon2 hash, this should give a mild improvement to that.
    
    Indeed we find that we get close to a 10% speed up for Poseidon2 BabyBear but broadly an unnoticeable speed up for Poseidon2 KoalaBear. This is likely due to the x -> x^7 being a major bottle neck for Poseidon2 BabyBear whereas Poseidon2 KoalaBear is currently being slowed down more by the current (poor) implementation of the internal layer with the map x -> x^3 already being reasonably quick.

--- baby-bear/src/x86_64_avx2/packing.rs
+++ baby-bear/src/x86_64_avx2/packing.rs
@@ -16,10 +16,12 @@ impl MontyParametersAVX2 for BabyBearParameters {
 
 #[cfg(test)]
 mod tests {
+    use p3_field::AbstractField;
     use p3_field_testing::test_packed_field;
+    use p3_monty_31::PackedMontyField31AVX2;
 
     use super::WIDTH;
-    use crate::BabyBear;
+    use crate::{BabyBear, BabyBearParameters};
 
     const SPECIAL_VALS: [BabyBear; WIDTH] = BabyBear::new_array([
         0x00000000, 0x00000001, 0x78000000, 0x77ffffff, 0x3c000000, 0x0ffffffe, 0x68000003,
@@ -31,4 +33,118 @@ mod tests {
         crate::PackedBabyBearAVX2::zero(),
         p3_monty_31::PackedMontyField31AVX2::<crate::BabyBearParameters>(super::SPECIAL_VALS)
     );
+
+    #[test]
+    fn test_cube_vs_mul() {
+        let vec = PackedMontyField31AVX2::<BabyBearParameters>(BabyBear::new_array([
+            0x4efd5eaf, 0x311b8e0c, 0x74dd27c1, 0x449613f0, 0x017fa32b, 0xfedcba98, 0x12345678,
+            0x55555555,
+        ]));
+        let res0 = vec * vec.square();
+        let res1 = vec.cube();
+        assert_eq!(res0, res1);
+    }
+
+    #[test]
+    fn test_cube_vs_scalar() {
+        let arr = BabyBear::new_array([
+            0x57155037, 0x71bdcc8e, 0x301f94d, 0x435938a6, 0x017fa32b, 0xfedcba98, 0x12345678,
+            0x55555555,
+        ]);
+
+        let vec = PackedMontyField31AVX2::<BabyBearParameters>(arr);
+        let vec_res = vec.cube();
+
+        #[allow(clippy::needless_range_loop)]
+        for i in 0..WIDTH {
+            assert_eq!(vec_res.0[i], arr[i].cube());
+        }
+    }
+
+    #[test]
+    fn test_cube_vs_scalar_special_vals() {
+        let vec = PackedMontyField31AVX2::<BabyBearParameters>(SPECIAL_VALS);
+        let vec_res = vec.cube();
+
+        #[allow(clippy::needless_range_loop)]
+        for i in 0..WIDTH {
+            assert_eq!(vec_res.0[i], SPECIAL_VALS[i].cube());
+        }
+    }
+
+    #[test]
+    fn test_exp_5_vs_mul() {
+        let vec = PackedMontyField31AVX2::<BabyBearParameters>(BabyBear::new_array([
+            0x4efd5eaf, 0x311b8e0c, 0x74dd27c1, 0x449613f0, 0x017fa32b, 0xfedcba98, 0x12345678,
+            0x55555555,
+        ]));
+        let res0 = vec * vec.square() * vec.square();
+        let res1 = vec.exp_const_u64::<5>();
+        assert_eq!(res0, res1);
+    }
+
+    #[test]
+    fn test_exp_5_vs_scalar() {
+        let arr = BabyBear::new_array([
+            0x57155037, 0x71bdcc8e, 0x301f94d, 0x435938a6, 0x017fa32b, 0xfedcba98, 0x12345678,
+            0x55555555,
+        ]);
+
+        let vec = PackedMontyField31AVX2::<BabyBearParameters>(arr);
+        let vec_res = vec.exp_const_u64::<5>();
+
+        #[allow(clippy::needless_range_loop)]
+        for i in 0..WIDTH {
+            assert_eq!(vec_res.0[i], arr[i].exp_const_u64::<5>());
+        }
+    }
+
+    #[test]
+    fn test_exp_5_vs_scalar_special_vals() {
+        let vec = PackedMontyField31AVX2::<BabyBearParameters>(SPECIAL_VALS);
+        let vec_res = vec.exp_const_u64::<5>();
+
+        #[allow(clippy::needless_range_loop)]
+        for i in 0..WIDTH {
+            assert_eq!(vec_res.0[i], SPECIAL_VALS[i].exp_const_u64::<5>());
+        }
+    }
+
+    #[test]
+    fn test_exp_7_vs_mul() {
+        let vec = PackedMontyField31AVX2::<BabyBearParameters>(BabyBear::new_array([
+            0x4efd5eaf, 0x311b8e0c, 0x74dd27c1, 0x449613f0, 0x017fa32b, 0xfedcba98, 0x12345678,
+            0x55555555,
+        ]));
+        let res0 = vec * vec.square() * vec.square() * vec.square();
+        let res1 = vec.exp_const_u64::<7>();
+        assert_eq!(res0, res1);
+    }
+
+    #[test]
+    fn test_exp_7_vs_scalar() {
+        let arr = BabyBear::new_array([
+            0x57155037, 0x71bdcc8e, 0x301f94d, 0x435938a6, 0x017fa32b, 0xfedcba98, 0x12345678,
+            0x55555555,
+        ]);
+
+        let vec = PackedMontyField31AVX2::<BabyBearParameters>(arr);
+        let vec_res = vec.exp_const_u64::<7>();
+
+        #[allow(clippy::needless_range_loop)]
+        for i in 0..WIDTH {
+            assert_eq!(vec_res.0[i], arr[i].exp_const_u64::<7>());
+        }
+    }
+
+    #[test]
+    fn test_exp_7_vs_scalar_special_vals() {
+        let vec = PackedMontyField31AVX2::<BabyBearParameters>(SPECIAL_VALS);
+        let vec_res = vec.exp_const_u64::<7>();
+
+        #[allow(clippy::needless_range_loop)]
+        for i in 0..WIDTH {
+            assert_eq!(vec_res.0[i], SPECIAL_VALS[i].exp_const_u64::<7>());
+        }
+    }
 }
