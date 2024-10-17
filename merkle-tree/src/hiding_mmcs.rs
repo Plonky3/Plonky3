bc07f53037948f907798746bf4db2b2e963cca57
Daniel Lubarov <daniel@lubarov.com>
2024-10-16 21:08:51 -0700

    Refactor, using inner MerkleTreeMmcs

--- merkle-tree/src/hiding_mmcs.rs
+++ merkle-tree/src/hiding_mmcs.rs
@@ -1,7 +1,5 @@
 use alloc::vec::Vec;
 use core::cell::RefCell;
-use core::cmp::Reverse;
-use core::marker::PhantomData;
 
 use itertools::Itertools;
 use p3_commit::Mmcs;
@@ -10,14 +8,12 @@ use p3_matrix::dense::RowMajorMatrix;
 use p3_matrix::stack::HorizontalPair;
 use p3_matrix::{Dimensions, Matrix};
 use p3_symmetric::{CryptographicHasher, Hash, PseudoCompressionFunction};
-use p3_util::log2_ceil_usize;
 use rand::distributions::{Distribution, Standard};
 use rand::Rng;
 use serde::de::DeserializeOwned;
 use serde::{Deserialize, Serialize};
 
-use crate::MerkleTreeError::{RootMismatch, WrongBatchSize, WrongHeight};
-use crate::{MerkleTree, MerkleTreeError};
+use crate::{MerkleTree, MerkleTreeError, MerkleTreeMmcs};
 
 /// A vector commitment scheme backed by a `MerkleTree`.
 ///
@@ -41,21 +37,18 @@ use crate::{MerkleTree, MerkleTreeError};
 #[derive(Clone, Debug)]
 pub struct MerkleTreeHidingMmcs<P, PW, H, C, R, const DIGEST_ELEMS: usize, const SALT_ELEMS: usize>
 {
-    hash: H,
-    compress: C,
+    inner: MerkleTreeMmcs<P, PW, H, C, DIGEST_ELEMS>,
     rng: RefCell<R>,
-    _phantom: PhantomData<(P, PW)>,
 }
 
 impl<P, PW, H, C, R, const DIGEST_ELEMS: usize, const SALT_ELEMS: usize>
     MerkleTreeHidingMmcs<P, PW, H, C, R, DIGEST_ELEMS, SALT_ELEMS>
 {
     pub fn new(hash: H, compress: C, rng: R) -> Self {
+        let inner = MerkleTreeMmcs::new(hash, compress);
         Self {
-            hash,
-            compress,
+            inner,
             rng: rng.into(),
-            _phantom: PhantomData,
         }
     }
 }
@@ -96,44 +89,25 @@ where
                 HorizontalPair::new(mat, salts)
             })
             .collect();
-        let tree = MerkleTree::new::<P, PW, H, C>(&self.hash, &self.compress, salted_inputs);
-        let root = tree.root();
-        (root, tree)
+        self.inner.commit(salted_inputs)
     }
 
     fn open_batch<M: Matrix<P::Value>>(
         &self,
         index: usize,
-        prover_data: &MerkleTree<
-            P::Value,
-            PW::Value,
-            HorizontalPair<M, RowMajorMatrix<P::Value>>,
-            DIGEST_ELEMS,
-        >,
+        prover_data: &Self::ProverData<M>,
     ) -> (
         Vec<Vec<P::Value>>,
         (Vec<Vec<P::Value>>, Vec<[PW::Value; DIGEST_ELEMS]>),
     ) {
-        let max_height = self.get_max_height(prover_data);
-        let log_max_height = log2_ceil_usize(max_height);
-
-        let (openings, salts): (Vec<_>, Vec<_>) = prover_data
-            .leaves
-            .iter()
-            .map(|matrix| {
-                let log2_height = log2_ceil_usize(matrix.height());
-                let bits_reduced = log_max_height - log2_height;
-                let reduced_index = index >> bits_reduced;
-                let opening = matrix.first.row(reduced_index).collect();
-                let salt = matrix.second.row(reduced_index).collect();
-                (opening, salt)
+        let (salted_openings, siblings) = self.inner.open_batch(index, prover_data);
+        let (openings, salts): (Vec<_>, Vec<_>) = salted_openings
+            .into_iter()
+            .map(|row| {
+                let (a, b) = row.split_at(row.len() - SALT_ELEMS);
+                (a.to_vec(), b.to_vec())
             })
             .unzip();
-
-        let siblings: Vec<_> = (0..log_max_height)
-            .map(|i| prover_data.digest_layers[i][(index >> i) ^ 1])
-            .collect();
-
         (openings, (salts, siblings))
     }
 
@@ -148,85 +122,20 @@ where
         &self,
         commit: &Self::Commitment,
         dimensions: &[Dimensions],
-        mut index: usize,
+        index: usize,
         opened_values: &[Vec<P::Value>],
         proof: &Self::Proof,
     ) -> Result<(), Self::Error> {
         let (salts, siblings) = proof;
-        // Check that the openings have the correct shape.
-        if dimensions.len() != opened_values.len() {
-            return Err(WrongBatchSize);
-        }
 
-        // TODO: Disabled for now since TwoAdicFriPcs and CirclePcs currently pass 0 for width.
-        // for (dims, opened_vals) in dimensions.iter().zip(opened_values) {
-        //     if opened_vals.len() != dims.width {
-        //         return Err(WrongWidth);
-        //     }
-        // }
-
-        // TODO: Disabled for now, CirclePcs sometimes passes a height that's off by 1 bit.
-        let max_height = dimensions.iter().map(|dim| dim.height).max().unwrap();
-        let log_max_height = log2_ceil_usize(max_height);
-        if siblings.len() != log_max_height {
-            return Err(WrongHeight {
-                max_height,
-                num_siblings: siblings.len(),
-            });
-        }
-
-        let mut heights_tallest_first = dimensions
+        let opened_salted_values = opened_values
             .iter()
-            .enumerate()
-            .sorted_by_key(|(_, dims)| Reverse(dims.height))
-            .peekable();
+            .zip(salts.iter())
+            .map(|(opened, salt)| opened.iter().chain(salt.iter()).copied().collect_vec())
+            .collect_vec();
 
-        let mut curr_height_padded = heights_tallest_first
-            .peek()
-            .unwrap()
-            .1
-            .height
-            .next_power_of_two();
-
-        let mut root = self.hash.hash_iter_slices(
-            heights_tallest_first
-                .peeking_take_while(|(_, dims)| {
-                    dims.height.next_power_of_two() == curr_height_padded
-                })
-                .flat_map(|(i, _)| [opened_values[i].as_slice(), salts[i].as_slice()]),
-        );
-
-        for &sibling in siblings.iter() {
-            let (left, right) = if index & 1 == 0 {
-                (root, sibling)
-            } else {
-                (sibling, root)
-            };
-
-            root = self.compress.compress([left, right]);
-            index >>= 1;
-            curr_height_padded >>= 1;
-
-            let next_height = heights_tallest_first
-                .peek()
-                .map(|(_, dims)| dims.height)
-                .filter(|h| h.next_power_of_two() == curr_height_padded);
-            if let Some(next_height) = next_height {
-                let next_height_openings_digest = self.hash.hash_iter_slices(
-                    heights_tallest_first
-                        .peeking_take_while(|(_, dims)| dims.height == next_height)
-                        .flat_map(|(i, _)| [opened_values[i].as_slice(), salts[i].as_slice()]),
-                );
-
-                root = self.compress.compress([root, next_height_openings_digest]);
-            }
-        }
-
-        if commit == &root {
-            Ok(())
-        } else {
-            Err(RootMismatch)
-        }
+        self.inner
+            .verify_batch(commit, dimensions, index, &opened_salted_values, siblings)
     }
 }
 
