d2ae7cbdc79085c5fa7f0d4686407c5392ba9a5d
Daniel Lubarov <daniel@lubarov.com>
2024-10-17 13:07:42 -0700

    Modify `Radix2DitParallel` to treat a coset LDE...
    ... as the union of several DFTs onto several smaller cosets.
    
    Also change it to resize the input vector rather than allocating a separate vector for the result. This works well iff the input vector was already allocated with extra capacity.
    
    Also do the first layer of most coset DFTs out-of-place. This is effectively like merging the steps where we copy coefficients onto other cosets into the next layers that use those coefficients.

--- dft/src/butterflies.rs
+++ dft/src/butterflies.rs
@@ -1,3 +1,6 @@
+use core::mem::MaybeUninit;
+
+use itertools::izip;
 use p3_field::{Field, PackedField, PackedValue};
 
 pub trait Butterfly<F: Field>: Copy + Send + Sync {
@@ -12,13 +15,44 @@ pub trait Butterfly<F: Field>: Copy + Send + Sync {
     fn apply_to_rows(&self, row_1: &mut [F], row_2: &mut [F]) {
         let (shorts_1, suffix_1) = F::Packing::pack_slice_with_suffix_mut(row_1);
         let (shorts_2, suffix_2) = F::Packing::pack_slice_with_suffix_mut(row_2);
-        assert_eq!(shorts_1.len(), shorts_2.len());
-        assert_eq!(suffix_1.len(), suffix_2.len());
+        debug_assert_eq!(shorts_1.len(), shorts_2.len());
+        debug_assert_eq!(suffix_1.len(), suffix_2.len());
         for (x_1, x_2) in shorts_1.iter_mut().zip(shorts_2) {
             self.apply_in_place(x_1, x_2);
         }
         for (x_1, x_2) in suffix_1.iter_mut().zip(suffix_2) {
             self.apply_in_place(x_1, x_2);
+        }
+    }
+
+    /// Like `apply_to_rows`, but out-of-place.
+    #[inline]
+    fn apply_to_rows_oop(
+        &self,
+        src_1: &[F],
+        dst_1: &mut [MaybeUninit<F>],
+        src_2: &[F],
+        dst_2: &mut [MaybeUninit<F>],
+    ) {
+        let (src_shorts_1, src_suffix_1) = F::Packing::pack_slice_with_suffix(src_1);
+        let (src_shorts_2, src_suffix_2) = F::Packing::pack_slice_with_suffix(src_2);
+        let (dst_shorts_1, dst_suffix_1) =
+            F::Packing::pack_maybe_uninit_slice_with_suffix_mut(dst_1);
+        let (dst_shorts_2, dst_suffix_2) =
+            F::Packing::pack_maybe_uninit_slice_with_suffix_mut(dst_2);
+        debug_assert_eq!(src_shorts_1.len(), src_shorts_2.len());
+        debug_assert_eq!(src_suffix_1.len(), src_suffix_2.len());
+        debug_assert_eq!(dst_shorts_1.len(), dst_shorts_2.len());
+        debug_assert_eq!(dst_suffix_1.len(), dst_suffix_2.len());
+        for (s_1, s_2, d_1, d_2) in izip!(src_shorts_1, src_shorts_2, dst_shorts_1, dst_shorts_2) {
+            let (res_1, res_2) = self.apply::<F::Packing>(*s_1, *s_2);
+            d_1.write(res_1);
+            d_2.write(res_2);
+        }
+        for (s_1, s_2, d_1, d_2) in izip!(src_suffix_1, src_suffix_2, dst_suffix_1, dst_suffix_2) {
+            let (res_1, res_2) = self.apply::<F>(*s_1, *s_2);
+            d_1.write(res_1);
+            d_2.write(res_2);
         }
     }
 }
