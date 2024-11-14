de79f3815b5620298d59a5e04ba7fdf07e2b766a
BGluth <gluthb@gmail.com>
2024-11-14 13:21:13 -0700

    Fixed changelog gen script not working outside of the project root

--- versioning_tools/changelog_gen.sh
+++ versioning_tools/changelog_gen.sh
@@ -3,15 +3,18 @@
 # Convenience script for prepending to the changelog after a version bump.
 # Also has a few additional checks to prevent double appending a changelog for a version to `CHANGELOG`.
 
+curr_dir=$(realpath "$(dirname "$0")")
+changelog_path="${curr_dir}/../CHANGELOG"
+
 latest_local_release_tag=$(git tag -l | grep -E 'v[0-9]+\.[0-9]+\.[0-9]+' | sort -r | head -n 1)
 latest_local_release_tag_no_v=$(echo "$latest_local_release_tag" | sed -E 's/v(.*)/\1/')
 
 # Check if there already is an entry in the changelog for this version.
-if [ "$(grep -c "\[$latest_local_release_tag_no_v\]" < CHANGELOG)" -gt 0 ]; then
-    echo "Version ${latest_local_release_tag} already has a entry in the changelog. Manually remove it before preprending with this script."
+if [ "$(grep -c "\[$latest_local_release_tag_no_v\]" < "$changelog_path")" -gt 0 ]; then
+    echo "Version ${latest_local_release_tag} already has a entry in the changelog. Manually remove it before prepending to it with this script."
     exit 1
 fi
 
-git cliff -p CHANGELOG --latest
+git cliff -p "$changelog_path" --latest
 
 echo "Changelog successfully updated for ${latest_local_release_tag}."
