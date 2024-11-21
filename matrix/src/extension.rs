512449f970b4060bdcda440f6568c0a1552ea05e
Thomas Coratger <60488569+tcoratger@users.noreply.github.com>
2024-11-21 13:07:23 +0100

    matrix: implement `Deref` for `FlatMatrixView` (#541)
    * matrix: implement Deref for FlatMatrixView
    
    * clean up row_slice

--- matrix/src/extension.rs
+++ matrix/src/extension.rs
@@ -16,7 +16,12 @@ impl<F, EF, Inner> FlatMatrixView<F, EF, Inner> {
     pub fn new(inner: Inner) -> Self {
         Self(inner, PhantomData)
     }
-    pub fn inner_ref(&self) -> &Inner {
+}
+
+impl<F, EF, Inner> Deref for FlatMatrixView<F, EF, Inner> {
+    type Target = Inner;
+
+    fn deref(&self) -> &Self::Target {
         &self.0
     }
 }
@@ -49,14 +54,12 @@ where
     }
 
     fn row_slice(&self, r: usize) -> impl Deref<Target = [F]> {
-        let ef_row: Vec<F> = self
-            .0
+        self.0
             .row_slice(r)
             .iter()
             .flat_map(|val| val.as_base_slice())
             .copied()
-            .collect();
-        ef_row
+            .collect::<Vec<_>>()
     }
 }
 
