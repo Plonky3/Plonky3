be35345d848d88f9ba11cbefd7a59906902fc135
AngusG <angusgruen@gmail.com>
2024-12-05 11:58:57 -0500

    Removing the need for "FastDFTs"

--- uni-stark/examples/prove_koala_bear.rs
+++ uni-stark/examples/prove_koala_bear.rs
@@ -1,18 +1,19 @@
 use std::env;
 use std::fmt::Debug;
 
+use p3_baby_bear::BabyBear;
 use p3_blake3_air::Blake3Air;
-use p3_challenger::{HashChallenger, SerializingChallenger32};
+use p3_challenger::SerializingChallenger32;
 use p3_commit::ExtensionMmcs;
-use p3_dft::Radix2DitParallel;
+use p3_dft::{Radix2DitParallel, TwoAdicSubgroupDft};
 use p3_field::extension::BinomialExtensionField;
+use p3_field::{ExtensionField, PrimeField32, TwoAdicField};
 use p3_fri::{FriConfig, TwoAdicFriPcs};
 use p3_keccak::Keccak256Hash;
 use p3_keccak_air::KeccakAir;
 use p3_koala_bear::KoalaBear;
-use p3_matrix::Matrix;
 use p3_merkle_tree::MerkleTreeMmcs;
-use p3_monty_31::dft::{FastDFTs, RecursiveDft};
+use p3_monty_31::dft::RecursiveDft;
 use p3_symmetric::{CompressionFunctionFromHasher, SerializingHasher32};
 use p3_uni_stark::{prove, verify, StarkConfig};
 use rand::random;
@@ -22,12 +23,15 @@ use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
 use tracing_subscriber::{EnvFilter, Registry};
 
-fn main() -> Result<(), impl Debug> {
-    let args: Vec<String> = env::args().collect();
-    assert_eq!(args.len(), 3, "There should be two arguments passed this functions.
-    \n The first chooses which statement we are proving. Use \"B\" to prove Blake3 hashes and \"K\" to prove Keccak hashes.
-    \n The second gives the choice of fourier transform. Use \"R\" for RecursiveDft and \"P\" for Radix2DitParallel.");
-
+fn prove_hashes<
+    F: PrimeField32 + TwoAdicField,
+    EF: ExtensionField<F> + TwoAdicField,
+    DFT: TwoAdicSubgroupDft<F>,
+>(
+    args: Vec<String>,
+    dft: DFT,
+    num_hashes: usize,
+) -> Result<(), impl Debug> {
     let env_filter = EnvFilter::builder()
         .with_default_directive(LevelFilter::INFO.into())
         .from_env_lossy();
@@ -37,37 +41,35 @@ fn main() -> Result<(), impl Debug> {
         .with(ForestLayer::default())
         .init();
 
-    type Val = KoalaBear;
-    type Challenge = BinomialExtensionField<Val, 4>;
+    // type Val = KoalaBear;
+    // type Challenge = BinomialExtensionField<KoalaBear, 4>;
 
-    type ByteHash = Keccak256Hash;
-    type FieldHash = SerializingHasher32<ByteHash>;
-    let byte_hash = ByteHash {};
-    let field_hash = FieldHash::new(Keccak256Hash {});
+    // type ByteHash = Keccak256Hash;
+    // type FieldHash = SerializingHasher32<ByteHash>;
+    let byte_hash = Keccak256Hash {};
+    let field_hash = SerializingHasher32::new(Keccak256Hash {});
 
-    type MyCompress = CompressionFunctionFromHasher<ByteHash, 2, 32>;
-    let compress = MyCompress::new(byte_hash);
+    // type MyCompress = CompressionFunctionFromHasher<ByteHash, 2, 32>;
+    let compress = CompressionFunctionFromHasher::<_, 2, 32>::new(byte_hash);
 
-    type ValMmcs = MerkleTreeMmcs<Val, u8, FieldHash, MyCompress, 32>;
-    let val_mmcs = ValMmcs::new(field_hash, compress);
+    // type ValMmcs = MerkleTreeMmcs<KoalaBear, u8, FieldHash, MyCompress, 32>;
+    let val_mmcs = MerkleTreeMmcs::<F, u8, _, _, 32>::new(field_hash, compress);
 
-    type ChallengeMmcs = ExtensionMmcs<Val, Challenge, ValMmcs>;
-    let challenge_mmcs = ChallengeMmcs::new(val_mmcs.clone());
+    // type ChallengeMmcs = ExtensionMmcs<KoalaBear, BinomialExtensionField<KoalaBear, 4>, ValMmcs>;
+    let challenge_mmcs = ExtensionMmcs::<F, EF, _>::new(val_mmcs.clone());
 
-    type Challenger = SerializingChallenger32<Val, HashChallenger<u8, ByteHash, 32>>;
+    // type Challenger = SerializingChallenger32<KoalaBear, HashChallenger<u8, ByteHash, 32>>;
 
-    let trace = match args[1].as_ref() {
+    let trace = match args[2].as_ref() {
         "B" | "Blake3" | "BLAKE3" => {
-            const NUM_HASHES: usize = 1 << 13;
-            println!("Proving {NUM_HASHES} Blake3 Hashes");
-            let inputs = (0..NUM_HASHES).map(|_| random()).collect::<Vec<_>>();
-            p3_blake3_air::generate_trace_rows::<Val>(inputs)
+            println!("Proving {num_hashes} Blake3 Hashes");
+            let inputs = (0..num_hashes).map(|_| random()).collect::<Vec<_>>();
+            p3_blake3_air::generate_trace_rows(inputs)
         }
         "K" | "Keccak" | "KECCAK" => {
-            const NUM_HASHES: usize = 1365;
-            println!("Proving {NUM_HASHES} KECCAK Hashes");
-            let inputs = (0..NUM_HASHES).map(|_| random()).collect::<Vec<_>>();
-            p3_keccak_air::generate_trace_rows::<Val>(inputs)
+            println!("Proving {num_hashes} KECCAK Hashes");
+            let inputs = (0..num_hashes).map(|_| random()).collect::<Vec<_>>();
+            p3_keccak_air::generate_trace_rows(inputs)
         }
         _ => {
             panic!("Could not understand the proof goal. Use \"B\" to prove Blake3 Hashes or \"K\" for Keccak hashes.")
@@ -81,37 +83,16 @@ fn main() -> Result<(), impl Debug> {
         mmcs: challenge_mmcs,
     };
 
-    type FastDft = FastDFTs<Val>;
-    let dft = match args[2].as_ref() {
-        "R" | "Recursive" => {
-            println!("Using the RecursiveDft for our DFT");
-            type Dft = RecursiveDft<Val>;
-            let dft = Dft::new(trace.height() << fri_config.log_blowup);
+    // type Pcs = TwoAdicFriPcs<KoalaBear, FastDft, ValMmcs, ChallengeMmcs>;
+    let pcs = TwoAdicFriPcs::new(dft, val_mmcs, fri_config);
 
-            FastDFTs::<Val>::Recursive(dft)
-        }
-        "P" | "Parallel" => {
-            println!("Using the Radix2DitParallel for our DFT");
-            type Dft = Radix2DitParallel<Val>;
-            let dft = Dft::default();
+    // type MyConfig = StarkConfig<Pcs, BinomialExtensionField<KoalaBear, 4>, Challenger>;
+    let config = StarkConfig::new(pcs);
 
-            FastDFTs::<Val>::Parallel(dft)
-        }
-        _ => {
-            panic!("Could not understand the Fourier transform method. Use \"R\" for the Recursive DFT or \"P\" for the Parallel DFT.")
-        }
-    };
+    let mut proof_challenger = SerializingChallenger32::from_hasher(vec![], byte_hash);
+    let mut verif_challenger = SerializingChallenger32::from_hasher(vec![], byte_hash);
 
-    type Pcs = TwoAdicFriPcs<Val, FastDft, ValMmcs, ChallengeMmcs>;
-    let pcs = Pcs::new(dft, val_mmcs, fri_config);
-
-    type MyConfig = StarkConfig<Pcs, Challenge, Challenger>;
-    let config = MyConfig::new(pcs);
-
-    let mut proof_challenger = Challenger::from_hasher(vec![], byte_hash);
-    let mut verif_challenger = Challenger::from_hasher(vec![], byte_hash);
-
-    match args[1].as_ref() {
+    match args[2].as_ref() {
         "B" | "Blake3" | "BLAKE3" => {
             let proof = prove(
                 &config,
@@ -148,6 +129,91 @@ fn main() -> Result<(), impl Debug> {
         }
         _ => {
             panic!("Could not understand the proof goal. Use \"B\" to prove Blake3 Hashes or \"K\" for Keccak hashes.")
+        }
+    }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    assert_eq!(args.len(), 5, "There should be three arguments passed this functions.
+    \n The first chooses the field we are using for the proof. Use \"B\" to use the BabyBear field and \"K\" to use the KoalaBear field.
+    \n The second chooses which statement we are proving. Use \"B\" to prove Blake3 hashes and \"K\" to prove Keccak hashes.
+    \n The third gives the number of Hashes. It should fit in a u8 and will be interpreted as the log base 2 of the trace height. For Blake3 this
+    is equal to the number of hashes but for Keccak this is roughly the number of hashes times 24.
+    \n The fourth gives the choice of fourier transform. Use \"R\" for RecursiveDft and \"P\" for Radix2DitParallel.");
+
+    let log_2_hashes = args[3]
+        .parse::<u8>()
+        .expect("The third command line input should be a u8");
+    let num_hashes = 1 << log_2_hashes;
+
+    match args[1].as_ref() {
+        "K" | "KoalaBear" | "KOALABEAR" => {
+            println!("Proving using the KoalaBear Field.");
+
+            match args[4].as_ref() {
+                "R" | "Recursive" => {
+                    println!("Using the RecursiveDft for our DFT");
+                    let dft = RecursiveDft::new(num_hashes << 1);
+                    let result = prove_hashes::<KoalaBear, BinomialExtensionField<KoalaBear, 4>, _>(
+                        args, dft, num_hashes,
+                    );
+                    if result.is_ok() {
+                        println!("Proof Verified Successfully")
+                    } else {
+                        println!("{:?}", result.unwrap_err())
+                    }
+                }
+                "P" | "Parallel" => {
+                    println!("Using the Radix2DitParallel for our DFT");
+                    let dft = Radix2DitParallel::default();
+                    let result = prove_hashes::<KoalaBear, BinomialExtensionField<KoalaBear, 4>, _>(
+                        args, dft, num_hashes,
+                    );
+                    if result.is_ok() {
+                        println!("Proof Verified Successfully")
+                    } else {
+                        println!("{:?}", result.unwrap_err())
+                    }
+                }
+                _ => {
+                    panic!("Could not understand the Fourier transform method. Use \"R\" for the Recursive DFT or \"P\" for the Parallel DFT.")
+                }
+            };
+        }
+        "B" | "BabyBear" | "BABYBEAR" => {
+            match args[4].as_ref() {
+                "R" | "Recursive" => {
+                    println!("Using the RecursiveDft for our DFT");
+                    let dft = RecursiveDft::new(num_hashes << 1);
+                    let result = prove_hashes::<BabyBear, BinomialExtensionField<BabyBear, 4>, _>(
+                        args, dft, num_hashes,
+                    );
+                    if result.is_ok() {
+                        println!("Proof Verified Successfully")
+                    } else {
+                        println!("{:?}", result.unwrap_err())
+                    }
+                }
+                "P" | "Parallel" => {
+                    println!("Using the Radix2DitParallel for our DFT");
+                    let dft = Radix2DitParallel::default();
+                    let result = prove_hashes::<BabyBear, BinomialExtensionField<BabyBear, 4>, _>(
+                        args, dft, num_hashes,
+                    );
+                    if result.is_ok() {
+                        println!("Proof Verified Successfully")
+                    } else {
+                        println!("{:?}", result.unwrap_err())
+                    }
+                }
+                _ => {
+                    panic!("Could not understand the Fourier transform method. Use \"R\" for the Recursive DFT or \"P\" for the Parallel DFT.")
+                }
+            };
+        }
+        _ => {
+            panic!("Could not understand the choice of field. Use \"B\" to prove using the BabyBear Field or \"K\" to use the Keccak Field.")
         }
     }
 }
