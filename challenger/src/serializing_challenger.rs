01bd38eb5f74af819a2f6eee9c90309f6bf6a497
AngusG <angusgruen@gmail.com>
2024-12-12 12:57:49 -0500

    Adding `impl` method for `SerializingChallenger64` so that the new faster version of Keccak can be used with the goldilocks field.

--- challenger/src/serializing_challenger.rs
+++ challenger/src/serializing_challenger.rs
@@ -186,6 +186,16 @@ impl<F: PrimeField64, const N: usize, Inner: CanObserve<u8>> CanObserve<Hash<F, 
     }
 }
 
+impl<F: PrimeField64, const N: usize, Inner: CanObserve<u8>> CanObserve<Hash<F, u64, N>>
+    for SerializingChallenger64<F, Inner>
+{
+    fn observe(&mut self, values: Hash<F, u64, N>) {
+        for value in values {
+            self.inner.observe_slice(&value.to_le_bytes());
+        }
+    }
+}
+
 impl<F, EF, Inner> CanSample<EF> for SerializingChallenger64<F, Inner>
 where
     F: PrimeField64,
