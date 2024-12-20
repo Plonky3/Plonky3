5805e7a03291d759fcabcc59d4b7dcae17606b2e
carlo <carlo.modica@protonmail.com>
2024-12-20 17:18:55 +0100

    constants and logic and generation made public

--- keccak-air/src/lib.rs
+++ keccak-air/src/lib.rs
@@ -6,16 +6,16 @@ extern crate alloc;
 
 mod air;
 mod columns;
-mod constants;
-mod generation;
-mod logic;
+pub mod constants;
+pub mod generation;
+pub mod logic;
 mod round_flags;
 
 pub use air::*;
 pub use columns::*;
 pub use generation::*;
 
-const NUM_ROUNDS: usize = 24;
+pub const NUM_ROUNDS: usize = 24;
 pub const BITS_PER_LIMB: usize = 8;
 pub const U64_LIMBS: usize = 64 / BITS_PER_LIMB;
 const RATE_BITS: usize = 1088;
