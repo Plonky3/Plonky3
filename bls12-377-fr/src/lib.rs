2843079cd6979ea3a7d6502ccb0ffb9e51a6e440
nazarevsky <nazarevsky.ua@gmail.com>
2025-01-06 14:47:56 +0200

    Add support of bls12-377fr

---
+++ bls12-377-fr/src/lib.rs
@@ -0,0 +1,379 @@
+//! The scalar field of the BLS12-377 curve, defined as `F_r` where `r = 8444461749428370424248824938781546531375899335154063827935233455917409239041`
+mod poseidon2;
+pub mod rc;
+
+pub use ark_bls12_377::Fr as FF_Bls12_377Fr;
+use core::fmt;
+use core::fmt::{Debug, Display, Formatter};
+use core::hash::{Hash, Hasher};
+use core::iter::{Product, Sum};
+use core::ops::{Add, AddAssign, Div, Mul, MulAssign, Neg, Sub, SubAssign};
+use num_bigint::BigUint;
+use p3_field::{Field, FieldAlgebra, Packable, PrimeField, TwoAdicField};
+pub use poseidon2::Poseidon2Bls12337;
+use rand::distributions::{Distribution, Standard};
+use rand::Rng;
+use serde::{Deserialize, Deserializer, Serialize};
+
+use ark_ff::{
+    BigInteger, FftField, Field as ArkField, PrimeField as ArkPrimeField, UniformRand, Zero,
+};
+use ark_serialize::{CanonicalDeserialize, CanonicalSerialize};
+
+/// The BLS12-377 curve scalar field prime, defined as `F_r` where `r = 8444461749428370424248824938781546531375899335154063827935233455917409239041`.
+#[derive(Copy, Clone, Eq)]
+pub struct Bls12_377Fr {
+    pub value: FF_Bls12_377Fr,
+}
+
+impl Bls12_377Fr {
+    pub(crate) const fn new(value: FF_Bls12_377Fr) -> Self {
+        Self { value }
+    }
+}
+
+impl Serialize for Bls12_377Fr {
+    fn serialize<S: serde::Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+        let mut bytes = Vec::new();
+        self.value
+            .serialize_compressed(&mut bytes)
+            .map_err(|err| serde::ser::Error::custom(err.to_string()))?;
+        serializer.serialize_bytes(&bytes)
+    }
+}
+
+impl<'de> Deserialize<'de> for Bls12_377Fr {
+    fn deserialize<D: Deserializer<'de>>(d: D) -> Result<Self, D::Error> {
+        let bytes: Vec<u8> = Deserialize::deserialize(d)?;
+
+        let value = FF_Bls12_377Fr::deserialize_compressed(&bytes[..]);
+
+        value
+            .map(Self::new)
+            .map_err(|_err| serde::de::Error::custom("Invalid field element"))
+    }
+}
+
+impl Default for Bls12_377Fr {
+    fn default() -> Self {
+        Self::new(FF_Bls12_377Fr::default())
+    }
+}
+
+impl PartialEq for Bls12_377Fr {
+    fn eq(&self, other: &Self) -> bool {
+        self.value.eq(&other.value)
+    }
+}
+
+impl Packable for Bls12_377Fr {}
+
+impl Hash for Bls12_377Fr {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        let mut serialized_bytes = Vec::new();
+        self.value
+            .serialize_compressed(&mut serialized_bytes)
+            .unwrap();
+
+        serialized_bytes.hash(state);
+    }
+}
+
+impl Ord for Bls12_377Fr {
+    fn cmp(&self, other: &Self) -> core::cmp::Ordering {
+        self.value.cmp(&other.value)
+    }
+}
+
+impl PartialOrd for Bls12_377Fr {
+    fn partial_cmp(&self, other: &Self) -> Option<core::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Display for Bls12_377Fr {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        <Bls12_377Fr as Debug>::fmt(self, f)
+    }
+}
+
+impl Debug for Bls12_377Fr {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        Debug::fmt(&self.value, f)
+    }
+}
+
+impl FieldAlgebra for Bls12_377Fr {
+    type F = Self;
+
+    const ZERO: Self = Self::new(FF_Bls12_377Fr::ZERO);
+    const ONE: Self = Self::new(FF_Bls12_377Fr::ONE);
+
+    const TWO: Self = Self::new(FF_Bls12_377Fr::new(ark_ff::biginteger::BigInt::new([
+        2_u64, 0, 0, 0,
+    ])));
+
+    // r - 1 = 0x12ab655e9a2ca55660b44d1e5c37b00159aa76fed00000010a11800000000000
+    const NEG_ONE: Self = Self::new(FF_Bls12_377Fr::new(ark_ff::biginteger::BigInt::new([
+        0x0a11800000000000,
+        0x59aa76fed0000001,
+        0x60b44d1e5c37b001,
+        0x12ab655e9a2ca556,
+    ])));
+
+    #[inline]
+    fn from_f(f: Self::F) -> Self {
+        f
+    }
+
+    fn from_bool(b: bool) -> Self {
+        Self::new(FF_Bls12_377Fr::from(b as u64))
+    }
+
+    fn from_canonical_u8(n: u8) -> Self {
+        Self::new(FF_Bls12_377Fr::from(n as u64))
+    }
+
+    fn from_canonical_u16(n: u16) -> Self {
+        Self::new(FF_Bls12_377Fr::from(n as u64))
+    }
+
+    fn from_canonical_u32(n: u32) -> Self {
+        Self::new(FF_Bls12_377Fr::from(n as u64))
+    }
+
+    fn from_canonical_u64(n: u64) -> Self {
+        Self::new(FF_Bls12_377Fr::from(n))
+    }
+
+    fn from_canonical_usize(n: usize) -> Self {
+        Self::new(FF_Bls12_377Fr::from(n as u64))
+    }
+
+    fn from_wrapped_u32(n: u32) -> Self {
+        Self::new(FF_Bls12_377Fr::from(n as u64))
+    }
+
+    fn from_wrapped_u64(n: u64) -> Self {
+        Self::new(FF_Bls12_377Fr::from(n))
+    }
+}
+
+impl Field for Bls12_377Fr {
+    type Packing = Self;
+
+    // generator is 22
+    const GENERATOR: Self = Self::new(FF_Bls12_377Fr::new(ark_ff::biginteger::BigInt::new([
+        22_u64, 0, 0, 0,
+    ])));
+
+    fn is_zero(&self) -> bool {
+        self.value.is_zero()
+    }
+
+    fn try_inverse(&self) -> Option<Self> {
+        self.value.inverse().map(Self::new)
+    }
+
+    /// r = 0x12ab655e9a2ca55660b44d1e5c37b00159aa76fed00000010a11800000000001
+    fn order() -> BigUint {
+        BigUint::new(vec![
+            0x00000001, 0x0a118000, 0xd0000001, 0x59aa76fe, 0x5c37b001, 0x60b44d1e, 0x9a2ca556,
+            0x12ab655e,
+        ])
+    }
+}
+
+impl PrimeField for Bls12_377Fr {
+    fn as_canonical_biguint(&self) -> BigUint {
+        BigUint::from_bytes_le(self.value.into_bigint().to_bytes_le().as_slice())
+    }
+}
+
+impl Add for Bls12_377Fr {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self {
+        Self::new(self.value + rhs.value)
+    }
+}
+
+impl AddAssign for Bls12_377Fr {
+    fn add_assign(&mut self, rhs: Self) {
+        self.value += rhs.value;
+    }
+}
+
+impl Sum for Bls12_377Fr {
+    fn sum<I: Iterator<Item = Self>>(iter: I) -> Self {
+        iter.reduce(|x, y| x + y).unwrap_or(Self::ZERO)
+    }
+}
+
+impl Sub for Bls12_377Fr {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self {
+        Self::new(self.value.sub(rhs.value))
+    }
+}
+
+impl SubAssign for Bls12_377Fr {
+    fn sub_assign(&mut self, rhs: Self) {
+        self.value -= rhs.value;
+    }
+}
+
+impl Neg for Bls12_377Fr {
+    type Output = Self;
+
+    fn neg(self) -> Self::Output {
+        self * Self::NEG_ONE
+    }
+}
+
+impl Mul for Bls12_377Fr {
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self {
+        Self::new(self.value * rhs.value)
+    }
+}
+
+impl MulAssign for Bls12_377Fr {
+    fn mul_assign(&mut self, rhs: Self) {
+        self.value *= rhs.value;
+    }
+}
+
+impl Product for Bls12_377Fr {
+    fn product<I: Iterator<Item = Self>>(iter: I) -> Self {
+        iter.reduce(|x, y| x * y).unwrap_or(Self::ONE)
+    }
+}
+
+impl Div for Bls12_377Fr {
+    type Output = Self;
+
+    #[allow(clippy::suspicious_arithmetic_impl)]
+    fn div(self, rhs: Self) -> Self {
+        self * rhs.inverse()
+    }
+}
+
+impl Distribution<Bls12_377Fr> for Standard {
+    #[inline]
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Bls12_377Fr {
+        Bls12_377Fr::new(FF_Bls12_377Fr::rand(rng))
+    }
+}
+
+impl TwoAdicField for Bls12_377Fr {
+    const TWO_ADICITY: usize = FF_Bls12_377Fr::TWO_ADICITY as usize;
+
+    fn two_adic_generator(bits: usize) -> Self {
+        let mut omega = FF_Bls12_377Fr::TWO_ADIC_ROOT_OF_UNITY;
+        for _ in bits..Self::TWO_ADICITY {
+            omega = omega.square();
+        }
+        Self::new(omega)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use num_traits::One;
+    use p3_field_testing::test_field;
+    use std::str::FromStr;
+
+    use super::*;
+
+    type F = Bls12_377Fr;
+
+    #[test]
+    fn test_bls12_337fr() {
+        let f = F::new(FF_Bls12_377Fr::from(100));
+        assert_eq!(f.as_canonical_biguint(), BigUint::new(vec![100]));
+
+        let f = F::from_canonical_u64(0);
+        assert!(f.is_zero());
+
+        let f = F::new(FF_Bls12_377Fr::from_str(&F::order().to_str_radix(10)).unwrap());
+        assert!(f.is_zero());
+
+        assert_eq!(F::GENERATOR.as_canonical_biguint(), BigUint::new(vec![22]));
+
+        let f_1 = F::new(FF_Bls12_377Fr::from(1_u128));
+        let f_1_copy = F::new(FF_Bls12_377Fr::from(1_u128));
+
+        let expected_result = F::ZERO;
+        assert_eq!(f_1 - f_1_copy, expected_result);
+
+        let expected_result = F::new(FF_Bls12_377Fr::from(2_u128));
+        assert_eq!(f_1 + f_1_copy, expected_result);
+
+        let f_2 = F::new(FF_Bls12_377Fr::from(2_u128));
+        let expected_result = F::new(FF_Bls12_377Fr::from(3_u128));
+        assert_eq!(f_1 + f_1_copy * f_2, expected_result);
+
+        let expected_result = F::new(FF_Bls12_377Fr::from(5_u128));
+        assert_eq!(f_1 + f_2 * f_2, expected_result);
+
+        let f_r_minus_1 = F::new(
+            FF_Bls12_377Fr::from_str(&(F::order() - BigUint::one()).to_str_radix(10)).unwrap(),
+        );
+        let expected_result = F::ZERO;
+        assert_eq!(f_1 + f_r_minus_1, expected_result);
+
+        let f_r_minus_2 = F::new(
+            FF_Bls12_377Fr::from_str(&(F::order() - BigUint::new(vec![2])).to_str_radix(10))
+                .unwrap(),
+        );
+        let expected_result = F::new(
+            FF_Bls12_377Fr::from_str(&(F::order() - BigUint::new(vec![3])).to_str_radix(10))
+                .unwrap(),
+        );
+        assert_eq!(f_r_minus_1 + f_r_minus_2, expected_result);
+
+        let expected_result = F::new(FF_Bls12_377Fr::from(1_u128));
+        assert_eq!(f_r_minus_1 - f_r_minus_2, expected_result);
+
+        let expected_result = f_r_minus_1;
+        assert_eq!(f_r_minus_2 - f_r_minus_1, expected_result);
+
+        let expected_result = f_r_minus_2;
+        assert_eq!(f_r_minus_1 - f_1, expected_result);
+
+        let expected_result = F::new(FF_Bls12_377Fr::from(3_u128));
+        assert_eq!(f_2 * f_2 - f_1, expected_result);
+
+        // Generator check
+        let expected_multiplicative_group_generator = F::new(FF_Bls12_377Fr::from(22_u128));
+        assert_eq!(F::GENERATOR, expected_multiplicative_group_generator);
+
+        let f_serialized = serde_json::to_string(&f).unwrap();
+        let f_deserialized: F = serde_json::from_str(&f_serialized).unwrap();
+        assert_eq!(f, f_deserialized);
+
+        let f_1_serialized = serde_json::to_string(&f_1).unwrap();
+        let f_1_deserialized: F = serde_json::from_str(&f_1_serialized).unwrap();
+        let f_1_serialized_again = serde_json::to_string(&f_1_deserialized).unwrap();
+        let f_1_deserialized_again: F = serde_json::from_str(&f_1_serialized_again).unwrap();
+        assert_eq!(f_1, f_1_deserialized);
+        assert_eq!(f_1, f_1_deserialized_again);
+
+        let f_2_serialized = serde_json::to_string(&f_2).unwrap();
+        let f_2_deserialized: F = serde_json::from_str(&f_2_serialized).unwrap();
+        assert_eq!(f_2, f_2_deserialized);
+
+        let f_r_minus_1_serialized = serde_json::to_string(&f_r_minus_1).unwrap();
+        let f_r_minus_1_deserialized: F = serde_json::from_str(&f_r_minus_1_serialized).unwrap();
+        assert_eq!(f_r_minus_1, f_r_minus_1_deserialized);
+
+        let f_r_minus_2_serialized = serde_json::to_string(&f_r_minus_2).unwrap();
+        let f_r_minus_2_deserialized: F = serde_json::from_str(&f_r_minus_2_serialized).unwrap();
+        assert_eq!(f_r_minus_2, f_r_minus_2_deserialized);
+    }
+
+    test_field!(crate::Bls12_377Fr);
+}
---
+++ bls12-377-fr/src/poseidon2.rs
@@ -0,0 +1,227 @@
+//! Diffusion matrix for Bls12-377
+//!
+//! Even tho the reference is for the other field, we used it for BLS12-377Fr considering the common
+//! field nature.
+//! Reference: https://github.com/HorizenLabs/poseidon2/blob/main/plain_implementations/src/poseidon2/poseidon2_instance_bls12.rs
+use std::sync::OnceLock;
+
+use p3_field::FieldAlgebra;
+use p3_poseidon2::{
+    add_rc_and_sbox_generic, external_initial_permute_state, external_terminal_permute_state,
+    internal_permute_state, matmul_internal, ExternalLayer, ExternalLayerConstants,
+    ExternalLayerConstructor, HLMDSMat4, InternalLayer, InternalLayerConstructor, Poseidon2,
+};
+
+use crate::Bls12_377Fr;
+
+/// Degree of the chosen permutation polynomial for BLS12-377, used as the Poseidon2 S-Box.
+///
+/// As p - 1 is divisible by 2 and 3 the smallest choice for a degree D satisfying gcd(p - 1, D) = 1 is 5.
+const BLS12_337_S_BOX_DEGREE: u64 = 17;
+
+/// An implementation of the Poseidon2 hash function for the [Bls12_377Fr] field.
+///
+/// It acts on arrays of the form `[Bls12_377Fr; WIDTH]`.
+pub type Poseidon2Bls12337<const WIDTH: usize> = Poseidon2<
+    Bls12_377Fr,
+    Poseidon2ExternalLayerBls12337<WIDTH>,
+    Poseidon2InternalLayerBls12337,
+    WIDTH,
+    BLS12_337_S_BOX_DEGREE,
+>;
+
+/// Currently we only support a single width for Poseidon2 Bls12_377Fr.
+const BLS12_377_WIDTH: usize = 3;
+
+#[inline]
+fn get_diffusion_matrix_3() -> &'static [Bls12_377Fr; 3] {
+    static MAT_DIAG3_M_1: OnceLock<[Bls12_377Fr; 3]> = OnceLock::new();
+    MAT_DIAG3_M_1.get_or_init(|| [Bls12_377Fr::ONE, Bls12_377Fr::ONE, Bls12_377Fr::TWO])
+}
+
+#[derive(Debug, Clone, Default)]
+pub struct Poseidon2InternalLayerBls12337 {
+    internal_constants: Vec<Bls12_377Fr>,
+}
+
+impl InternalLayerConstructor<Bls12_377Fr> for Poseidon2InternalLayerBls12337 {
+    fn new_from_constants(internal_constants: Vec<Bls12_377Fr>) -> Self {
+        Self { internal_constants }
+    }
+}
+
+impl InternalLayer<Bls12_377Fr, BLS12_377_WIDTH, BLS12_337_S_BOX_DEGREE>
+    for Poseidon2InternalLayerBls12337
+{
+    /// Perform the internal layers of the Poseidon2 permutation on the given state.
+    fn permute_state(&self, state: &mut [Bls12_377Fr; BLS12_377_WIDTH]) {
+        internal_permute_state::<Bls12_377Fr, BLS12_377_WIDTH, BLS12_337_S_BOX_DEGREE>(
+            state,
+            |x| matmul_internal(x, *get_diffusion_matrix_3()),
+            &self.internal_constants,
+        )
+    }
+}
+
+pub type Poseidon2ExternalLayerBls12337<const WIDTH: usize> =
+    ExternalLayerConstants<Bls12_377Fr, WIDTH>;
+
+impl<const WIDTH: usize> ExternalLayerConstructor<Bls12_377Fr, WIDTH>
+    for Poseidon2ExternalLayerBls12337<WIDTH>
+{
+    fn new_from_constants(external_constants: ExternalLayerConstants<Bls12_377Fr, WIDTH>) -> Self {
+        external_constants
+    }
+}
+
+impl<const WIDTH: usize> ExternalLayer<Bls12_377Fr, WIDTH, BLS12_337_S_BOX_DEGREE>
+    for Poseidon2ExternalLayerBls12337<WIDTH>
+{
+    /// Perform the initial external layers of the Poseidon2 permutation on the given state.
+    fn permute_state_initial(&self, state: &mut [Bls12_377Fr; WIDTH]) {
+        external_initial_permute_state(
+            state,
+            self.get_initial_constants(),
+            add_rc_and_sbox_generic::<_, BLS12_337_S_BOX_DEGREE>,
+            &HLMDSMat4,
+        );
+    }
+
+    /// Perform the terminal external layers of the Poseidon2 permutation on the given state.
+    fn permute_state_terminal(&self, state: &mut [Bls12_377Fr; WIDTH]) {
+        external_terminal_permute_state(
+            state,
+            self.get_terminal_constants(),
+            add_rc_and_sbox_generic::<_, BLS12_337_S_BOX_DEGREE>,
+            &HLMDSMat4,
+        );
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::rc::RC1;
+    use lazy_static::lazy_static;
+    use p3_poseidon2::ExternalLayerConstants;
+    use p3_symmetric::Permutation;
+    use std::str::FromStr;
+
+    use super::*;
+    use crate::FF_Bls12_377Fr;
+
+    fn bls12337_from_str(input: String) -> Bls12_377Fr {
+        Bls12_377Fr {
+            value: FF_Bls12_377Fr::from_str(&input).unwrap(),
+        }
+    }
+
+    struct TestCase {
+        input: [Bls12_377Fr; 3],
+        output: [Bls12_377Fr; 3],
+    }
+
+    lazy_static! {
+        static ref test_cases: Vec<TestCase> = vec![
+            TestCase{
+                input: [
+                    Bls12_377Fr::from_canonical_u64(1),
+                    Bls12_377Fr::from_canonical_u64(1),
+                    Bls12_377Fr::from_canonical_u64(1)
+                ],
+                output: [
+                    Bls12_377Fr{
+                        value: FF_Bls12_377Fr::from_str("860866526687489428989707845699174300428968972136191802628058085350960837665").unwrap()
+                    },
+                    Bls12_377Fr{
+                        value: FF_Bls12_377Fr::from_str("5110597475731104621758769815158117224808582320756097893691034814235891573903").unwrap()
+                    },
+                    Bls12_377Fr{
+                        value: FF_Bls12_377Fr::from_str("7729317803422720288984283127941214463170714974376170076017011151108586598169").unwrap()
+                    }
+                ],
+            },
+            TestCase{
+                input: [
+                    Bls12_377Fr::from_canonical_u64(3457435785349743598),
+                    Bls12_377Fr::from_canonical_u64(6786347127498807634),
+                    Bls12_377Fr::from_canonical_u64(0)
+                ],
+                output: [
+                    Bls12_377Fr{
+                        value: FF_Bls12_377Fr::from_str("675583857530871455730788450062580113099802122446214910490159210203931204829").unwrap()
+                    },
+                    Bls12_377Fr{
+                        value: FF_Bls12_377Fr::from_str("5907675690176137521797568886899101531719052562432127804242792579253736799082").unwrap()
+                    },
+                    Bls12_377Fr{
+                        value: FF_Bls12_377Fr::from_str("3580163914861780974242579068588450174896166405220547377222985096420674645846").unwrap()
+                    }
+                ],
+            },
+            TestCase{
+                input: [
+                    Bls12_377Fr::from_canonical_u64(9379455689548),
+                    Bls12_377Fr::from_canonical_u64(7608439748),
+                    Bls12_377Fr::from_canonical_u64(954338476438754632)
+                ],
+                output: [
+                    Bls12_377Fr{
+                        value: FF_Bls12_377Fr::from_str("3055206294377317359307613780575828874003458762314725501486826284300578105158").unwrap()
+                    },
+                    Bls12_377Fr{
+                        value: FF_Bls12_377Fr::from_str("5797369338536356401300628927961554112193154358645694251738402018195827090483").unwrap()
+                    },
+                    Bls12_377Fr{
+                        value: FF_Bls12_377Fr::from_str("6809256165826541738832209298614382604751407820145487553453583098940097598887").unwrap()
+                    }
+                ],
+            },
+        ];
+    }
+
+    #[test]
+    fn test_poseidon2_bls12337() {
+        const WIDTH: usize = 3;
+        const ROUNDS_F: usize = 8;
+        const ROUNDS_P: usize = 56;
+
+        type F = Bls12_377Fr;
+
+        // Copy over round constants from pre generated values.
+        let round_constants: Vec<[F; WIDTH]> = RC1
+            .iter()
+            .map(|vec| {
+                vec.iter()
+                    .cloned()
+                    .map(bls12337_from_str)
+                    .collect::<Vec<_>>()
+                    .try_into()
+                    .unwrap()
+            })
+            .collect();
+
+        let internal_start = ROUNDS_F / 2;
+        let internal_end = (ROUNDS_F / 2) + ROUNDS_P;
+
+        let internal_round_constants = round_constants
+            .clone()
+            .drain(internal_start..internal_end)
+            .map(|vec| vec[0])
+            .collect::<Vec<_>>();
+
+        let external_round_constants = ExternalLayerConstants::new(
+            round_constants[..(ROUNDS_F / 2)].to_vec(),
+            round_constants[internal_end..].to_vec(),
+        );
+
+        // Our Poseidon2 implementation.
+        let poseidon2 = Poseidon2Bls12337::new(external_round_constants, internal_round_constants);
+
+        test_cases.iter().for_each(|case| {
+            let mut output = case.input;
+            poseidon2.permute_mut(&mut output);
+
+            assert_eq!(output, case.output);
+        });
+    }
+}
