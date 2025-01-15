5c2d6d5568082a3b45d419bbd86c0a8e8b46c5ba
Cesar199999 <cesar.descalzo2@gmail.com>
2025-01-15 17:39:29 +0100

    fix Polynomial tests and add test_compose_with_exponent
    Co-authored-by: Antonio Mejías Gil <anmegi.95@gmail.com>

--- stir/src/polynomial/tests.rs
+++ stir/src/polynomial/tests.rs
@@ -1,21 +1,45 @@
 use itertools::Itertools;
 use p3_baby_bear::BabyBear;
-use p3_field::AbstractField;
+use p3_field::{AbstractField, Field};
+use rand::distributions::Standard;
+use rand::prelude::Distribution;
+use rand::Rng;
 
 use crate::polynomial::Polynomial;
-type F = BabyBear;
+type BB = BabyBear;
+
+// NP TODO move?
+// NP TODO receive Option<random generator>?
+pub fn rand_poly<F: Field>(degree: usize) -> Polynomial<F>
+where
+    Standard: Distribution<F>,
+{
+    let mut rng = rand::thread_rng();
+
+    let mut coeffs: Vec<F> = (0..degree).map(|_| rng.gen()).collect();
+
+    let mut leading_coeff = F::zero();
+
+    while leading_coeff == F::zero() {
+        leading_coeff = rng.gen();
+    }
+
+    coeffs.push(leading_coeff);
+
+    Polynomial::from_coeffs(coeffs)
+}
 
 #[test]
 fn test_evaluate() {
     let a_coeffs = vec![3, 2, 5, 6, 8]
         .into_iter()
-        .map(F::from_canonical_u32)
+        .map(BB::from_canonical_u32)
         .collect_vec();
 
     let a = Polynomial::from_coeffs(a_coeffs);
-    let x = F::from_canonical_u32(1);
+    let x = BB::from_canonical_u32(1);
     let y = a.evaluate(&x);
-    assert_eq!(y, F::from_canonical_u32(24));
+    assert_eq!(y, BB::from_canonical_u32(24));
 }
 
 #[test]
@@ -23,27 +47,27 @@ fn test_sum_sub() {
     // a(x) = 3 + 2x + 5x^2 + 6x^3
     let a_coeffs = vec![3, 2, 5, 6]
         .into_iter()
-        .map(F::from_canonical_u32)
+        .map(BB::from_canonical_u32)
         .collect_vec();
 
     // b(x) = 1 + 2x + 3x^2 + 4x^3 + 5x^4 + 7x^5 + 8x^6
     let b_coeffs = vec![1, 2, 3, 4, 5, 7, 8]
         .into_iter()
-        .map(F::from_canonical_u32)
+        .map(BB::from_canonical_u32)
         .collect_vec();
 
     let expected_coeffs_sum = vec![4, 4, 8, 10, 5, 7, 8]
         .into_iter()
-        .map(F::from_canonical_u32)
+        .map(BB::from_canonical_u32)
         .collect_vec();
 
     let expected_coeffs_sub = vec![2, 0, 2, 2]
         .into_iter()
-        .map(F::from_canonical_u32)
+        .map(BB::from_canonical_u32)
         .chain(vec![
-            -F::from_canonical_u32(5),
-            -F::from_canonical_u32(7),
-            -F::from_canonical_u32(8),
+            -BB::from_canonical_u32(5),
+            -BB::from_canonical_u32(7),
+            -BB::from_canonical_u32(8),
         ])
         .collect_vec();
 
@@ -62,19 +86,19 @@ fn test_mul() {
     // a(x) = 3 + 2x + x^2 + x^3 + x^4
     let a_coeffs = vec![3, 2, 1, 1, 1]
         .into_iter()
-        .map(F::from_canonical_u32)
+        .map(BB::from_canonical_u32)
         .collect_vec();
 
     // b(x) = 8 + 7x + 6x^2
     let b_coeffs = vec![8, 7, 6]
         .into_iter()
-        .map(F::from_canonical_u32)
+        .map(BB::from_canonical_u32)
         .collect_vec();
 
     // c(x) = 24 + 37x + 40x^2 + 27x^3 + 21x^4 + 13x^5 + 6x^6
     let result_coeffs = vec![24, 37, 40, 27, 21, 13, 6]
         .into_iter()
-        .map(F::from_canonical_u32)
+        .map(BB::from_canonical_u32)
         .collect_vec();
 
     let a = Polynomial::from_coeffs(a_coeffs);
@@ -88,24 +112,24 @@ fn test_mul() {
 fn test_divide() {
     // a(x) = 3 - 2x - 5x^2 + 6x^3
     let a_coeffs = vec![
-        F::from_canonical_u32(3),
-        -F::from_canonical_u32(2),
-        -F::from_canonical_u32(5),
-        F::from_canonical_u32(6),
+        BB::from_canonical_u32(3),
+        -BB::from_canonical_u32(2),
+        -BB::from_canonical_u32(5),
+        BB::from_canonical_u32(6),
     ];
 
     // b(x) = -3 + x
-    let b_coeffs = vec![-F::from_canonical_u32(3), F::from_canonical_u32(1)];
+    let b_coeffs = vec![-BB::from_canonical_u32(3), BB::from_canonical_u32(1)];
 
     // q(x) = 37 + 13x + 6x^2
     let q_coeffs = vec![
-        F::from_canonical_u32(37),
-        F::from_canonical_u32(13),
-        F::from_canonical_u32(6),
+        BB::from_canonical_u32(37),
+        BB::from_canonical_u32(13),
+        BB::from_canonical_u32(6),
     ];
 
     // r(x) = 114
-    let r_coeffs = vec![F::from_canonical_u32(114)];
+    let r_coeffs = vec![BB::from_canonical_u32(114)];
 
     let a = Polynomial::from_coeffs(a_coeffs);
     let b = Polynomial::from_coeffs(b_coeffs);
@@ -119,24 +143,46 @@ fn test_divide() {
 #[test]
 fn test_naive_interpolate() {
     // p(x) = 3 + 4x + 2x^2 + 7x^3 + 9x^4 + x^5
-    let polynomial = Polynomial::from_coeffs(
+    let polynomial = Polynomial::<BB>::from_coeffs(
         vec![3, 4, 2, 7, 9, 1]
             .into_iter()
-            .map(F::from_canonical_u32)
+            .map(BB::from_canonical_u32)
             .collect_vec(),
     );
 
     let points = vec![1, 2, 3, 4, 5, 6]
         .into_iter()
-        .map(F::from_canonical_u32)
+        .map(BB::from_canonical_u32)
         .collect_vec();
 
     // p(1) = 26, p(2) = 251, p(3) = 1194, p(4) = 3827, p(5) = 9698, p(6) = 21051
     let evals = vec![26, 251, 1194, 3827, 9698, 21051]
         .into_iter()
-        .map(F::from_canonical_u32)
+        .map(BB::from_canonical_u32)
         .collect_vec();
 
-    let poly = Polynomial::<F>::naive_interpolate(points.into_iter().zip(evals).collect_vec());
-    assert_eq!(poly.coeffs, polynomial.coeffs);
+    let poly = Polynomial::<BB>::naive_interpolate(points.into_iter().zip(evals).collect_vec());
+    assert_eq!(poly.coeffs(), polynomial.coeffs());
+}
+
+#[test]
+fn test_compose_with_exponent() {
+    // p(x) = 3 + 2x + 6x^2 + 7x^3 + 9x^4
+    let polynomial = Polynomial::<BB>::from_coeffs(
+        vec![3, 2, 6, 7, 9]
+            .into_iter()
+            .map(BB::from_canonical_u32)
+            .collect_vec(),
+    );
+
+    let exponent = 3;
+
+    // p(x^3) = 3 + 2x^3 + 6x^6 + 7x^9 + 9x^12
+    assert_eq!(
+        polynomial.compose_with_exponent(exponent).coeffs(),
+        vec![3, 0, 0, 2, 0, 0, 6, 0, 0, 7, 0, 0, 9]
+            .into_iter()
+            .map(BB::from_canonical_u32)
+            .collect_vec()
+    );
 }
