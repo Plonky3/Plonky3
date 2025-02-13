f577ad28b5da2e16ac972e0c80cf95de66e790bb
Thomas Coratger <thomas.coratger@gmail.com>
2025-02-13 15:59:49 +0100

    fix

--- poseidon2/src/round_numbers.rs
+++ poseidon2/src/round_numbers.rs
@@ -30,7 +30,10 @@ use p3_util::gcd_u64;
 /// Given a field, a width and an D return the number of full and partial rounds needed to achieve 128 bit security.
 pub const fn poseidon2_round_numbers_128<F: PrimeField64>(width: usize, d: u64) -> (usize, usize) {
     // Start by checking that d is a valid permutation.
-    assert!(gcd_u64(d, F::ORDER_U64 - 1) == 1, "Invalid permutation: gcd(d, F::ORDER_U64 - 1) must be 1")
+    assert!(
+        gcd_u64(d, F::ORDER_U64 - 1) == 1,
+        "Invalid permutation: gcd(d, F::ORDER_U64 - 1) must be 1"
+    );
 
     // Next compute the number of bits in p.
     let prime_bit_number = F::ORDER_U64.ilog2() + 1;
