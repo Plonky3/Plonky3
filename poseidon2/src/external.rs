09c73db280fb8869dec011b048c635d6236c7989
Thomas Coratger <60488569+tcoratger@users.noreply.github.com>
2025-02-13 15:46:49 +0100

    poseidon: small touchups (#664)

--- poseidon2/src/external.rs
+++ poseidon2/src/external.rs
@@ -48,7 +48,7 @@ where
     let t23 = x[2].clone() + x[3].clone();
     let t0123 = t01.clone() + t23.clone();
     let t01123 = t0123.clone() + x[1].clone();
-    let t01233 = t0123.clone() + x[3].clone();
+    let t01233 = t0123 + x[3].clone();
     // The order here is important. Need to overwrite x[0] and x[2] after x[1] and x[3].
     x[3] = t01233.clone() + x[0].double(); // 3*x[0] + x[1] + x[2] + 2*x[3]
     x[1] = t01123.clone() + x[2].double(); // x[0] + 2*x[1] + 3*x[2] + x[3]
@@ -187,11 +187,11 @@ impl<T, const WIDTH: usize> ExternalLayerConstants<T, WIDTH> {
         Self::new(initial_consts, terminal_consts)
     }
 
-    pub fn get_initial_constants(&self) -> &Vec<[T; WIDTH]> {
+    pub const fn get_initial_constants(&self) -> &Vec<[T; WIDTH]> {
         &self.initial
     }
 
-    pub fn get_terminal_constants(&self) -> &Vec<[T; WIDTH]> {
+    pub const fn get_terminal_constants(&self) -> &Vec<[T; WIDTH]> {
         &self.terminal
     }
 }
