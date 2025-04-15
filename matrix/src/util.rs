b0ab9111938a71f9e5cc00e56d4e7d21454b48f0
Thomas Coratger <thomas.coratger@gmail.com>
2025-04-16 00:53:47 +0200

    small touchup

--- matrix/src/util.rs
+++ matrix/src/util.rs
@@ -41,13 +41,12 @@ where
     let w = mat.width();
     let h = mat.height();
     let log_h = log2_strict_usize(h);
-    let values = mat.values.borrow_mut().as_mut_ptr() as usize;
+    let ptr = mat.values.borrow_mut().as_mut_ptr();
 
     (0..h).into_par_iter().for_each(|i| {
-        let values = values as *mut F;
         let j = reverse_bits_len(i, log_h);
         if i < j {
-            unsafe { swap_rows_raw(values, w, i, j) };
+            unsafe { swap_rows_raw(ptr, w, i, j) };
         }
     });
 }
