e6194cf8969f1def5024a911b397624083d0077a
Thomas Coratger <thomas.coratger@gmail.com>
2025-04-22 18:54:27 +0200

    fix comment

--- field/src/field.rs
+++ field/src/field.rs
@@ -7,6 +7,7 @@ use core::ops::{Add, AddAssign, Div, Mul, MulAssign, Neg, Sub, SubAssign};
 use core::{array, slice};
 
 use num_bigint::BigUint;
+use p3_maybe_rayon::prelude::{ParallelIterator, ParallelSlice};
 use p3_util::iter_array_chunks_padded;
 use serde::Serialize;
 use serde::de::DeserializeOwned;
@@ -506,8 +507,6 @@ pub trait BasedVectorSpace<F: PrimeCharacteristicRing>: Sized {
         F: Sync,
         Self: Send,
     {
-        use p3_maybe_rayon::prelude::{ParallelIterator, ParallelSlice};
-
         assert_eq!(vec.len() % Self::DIMENSION, 0);
 
         vec.par_chunks_exact(Self::DIMENSION)
