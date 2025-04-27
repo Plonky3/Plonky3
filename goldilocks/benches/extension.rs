4efe265a12775ecf6c97986500763ffb6ef65627
AngusG <angusgruen@gmail.com>
2025-04-27 18:56:41 -0400

    fmt and toml

--- goldilocks/benches/extension.rs
+++ goldilocks/benches/extension.rs
@@ -1,11 +1,9 @@
 use criterion::{Criterion, criterion_group, criterion_main};
 use p3_field::extension::BinomialExtensionField;
-use p3_field_testing::{
-    bench_func::{
-        benchmark_inv, benchmark_mul_latency, benchmark_mul_throughput, benchmark_square,
-    },
-    benchmark_mul,
+use p3_field_testing::bench_func::{
+    benchmark_inv, benchmark_mul_latency, benchmark_mul_throughput, benchmark_square,
 };
+use p3_field_testing::benchmark_mul;
 use p3_goldilocks::Goldilocks;
 
 type EF2 = BinomialExtensionField<Goldilocks, 2>;
