e938782a659228559ae7919d1d59c2f33facdf38
AngusG <agruen@polygon.technology>
2025-05-07 12:23:52 -0400

    Move split_evals and split_domains into commit_quotient and add comments (#859)
    * Move split_evals and split_domains into commit_quotient.
    
    * Comment fix
    
    * Update commit/src/domain.rs
    
    Co-authored-by: Zach Langley <zach@axiom.xyz>
    
    * Update uni-stark/src/prover.rs
    
    Co-authored-by: Zach Langley <zach@axiom.xyz>
    
    * Comment updates
    
    * Comment fixes
    
    * Minor word fixes
    
    ---------
    
    Co-authored-by: Zach Langley <zach@axiom.xyz>

--- uni-stark/src/prover.rs
+++ uni-stark/src/prover.rs
@@ -36,26 +36,80 @@ where
     #[cfg(debug_assertions)]
     crate::check_constraints::check_constraints(air, &trace, public_values);
 
-    let pcs = config.pcs();
-
+    // Compute the height `N = 2^n` and `log_2(height)`, `n`, of the trace.
     let degree = trace.height();
     let log_degree = log2_strict_usize(degree);
     let log_ext_degree = log_degree + config.is_zk();
 
+    // Compute the constraint polynomials as vectors of symbolic expressions.
     let symbolic_constraints = get_symbolic_constraints::<Val<SC>, A>(air, 0, public_values.len());
+
+    // Count the number of constraints that we have.
     let constraint_count = symbolic_constraints.len();
+
+    // Each constraint polynomial looks like `C_j(X_1, ..., X_w, Y_1, ..., Y_w, Z_1, ..., Z_j)`.
+    // When evaluated on a given row, the X_i's will be the `i`'th element of the that row, the
+    // Y_i's will be the `i`'th element of the next row and the Z_i's will be evaluations of
+    // selector polynomials on the given row index.
+    //
+    // When we convert to working with polynomials, the `X_i`'s and `Y_i`'s will be replaced by the
+    // degree `N - 1` polynomials `T_i(x)` and `T_i(hx)` respectively. The selector polynomials are
+    //  a little more complicated however.
+    //
+    // In our our case, the selector polynomials are `S_1(x) = is_first_row`, `S_2(x) = is_last_row`
+    // and `S_3(x) = is_transition`. Both `S_1(x)` and `S_2(x)` are polynomials of degree `N - 1`
+    // as they must be non zero only at a single location in the initial domain. However, `is_transition`
+    // is a polynomial of degree `1` as it simply need to be `0` on the last row.
+    //
+    // The constraint degree (`deg(C)`) is the linear factor of `N` in the constraint polynomial. In other
+    // words, it is roughly the total degree of `C` however, we treat `Z_3` as a constant term which does
+    // not contribute to the degree.
+    //
+    // E.g. `C_j = Z_1 * (X_1^3 - X_2 * X_3 * X_4)` would have degree `4`.
+    //      `C_j = Z_3 * (X_1^3 - X_2 * X_3 * X_4)` would have degree `3`.
+    //
+    // The point of all this is that, defining:
+    //          C(x) = C(T_1(x), ..., T_w(x), T_1(hx), ... T_w(hx), S_1(x), S_2(x), S_3(x))
+    // We get the constraint bound:
+    //          deg(C(x)) <= deg(C) * (N - 1) + 1
+    // The `+1` is due to the `is_transition` selector which is not accounted for in `deg(C)`. Note
+    // that S_i^2 should never appear in a constraint as it should just be replaced by `S_i`.
+    //
+    // For now in comments we assume that `deg(C) = 3` meaning `deg(C(x)) <= 3N - 2`
     let constraint_degree = symbolic_constraints
         .iter()
         .map(SymbolicExpression::degree_multiple)
         .max()
         .unwrap_or(0);
+
+    // From the degree of the constraint polynomial, compute the number
+    // of quotient polynomials we will split Q(x) into. This is chosen to
+    // always be a power of 2.
     let log_quotient_degree = log2_ceil_usize(constraint_degree - 1 + config.is_zk());
     let quotient_degree = 1 << (log_quotient_degree + config.is_zk());
 
+    // Initialize the PCS and the Challenger.
+    let pcs = config.pcs();
     let mut challenger = config.initialise_challenger();
+
+    // Get the subgroup `H` of size `N`. We treat each column `T_i` of
+    // the trace as an evaluation vector of polynomials `T_i(x)` over `H`.
+    // (In the Circle STARK case `H` is instead a standard position twin coset of size `N`)
     let trace_domain = pcs.natural_domain_for_degree(degree);
+
+    // When ZK is enabled, we need to use an extended domain of size `2N` as we will
+    // add random values to the trace.
     let ext_trace_domain = pcs.natural_domain_for_degree(degree * (config.is_zk() + 1));
 
+    // Let `g` denote a generator of the multiplicative group of `F` and `H'` the unique
+    // subgroup of `F` of size `N << (pcs.config.log_blowup + config.is_zk())`.
+    // If `zk` is enabled, we double the trace length by adding random values.
+    //
+    // For each trace column `T_i`, we compute the evaluation vector of `T_i(x)` over `H'`. This
+    // new extended trace `ET` is hashed into a Merkle tree with its rows bit-reversed.
+    //      trace_commit contains the root of the tree
+    //      trace_data contains the entire tree.
+    //          - trace_data.leaves is the matrix containing `ET`.
     let (trace_commit, trace_data) =
         info_span!("commit to trace data").in_scope(|| pcs.commit([(ext_trace_domain, trace)]));
 
@@ -65,7 +119,10 @@ where
     challenger.observe(Val::<SC>::from_u8(log_degree as u8));
     // TODO: Might be best practice to include other instance data here; see verifier comment.
 
+    // Observe the Merkle root of the trace commitment.
     challenger.observe(trace_commit.clone());
+
+    // Observe the public input values.
     challenger.observe_slice(public_values);
 
     // Get the first Fiat Shamir challenge which will be used to combine all constraint polynomials
@@ -90,11 +147,23 @@ where
     // a soundness issue.
     let alpha: SC::Challenge = challenger.sample_algebra_element();
 
+    // A domain large enough to uniquely identify the quotient polynomial.
+    // This domain must be contained in the domain over which `trace_data` is defined.
+    // Explicitly it should be equal to `gK` for some subgroup `K` contained in `H'`.
     let quotient_domain =
         ext_trace_domain.create_disjoint_domain(1 << (log_ext_degree + log_quotient_degree));
 
+    // Return a the subset of the extended trace `ET` corresponding to the rows giving evaluations
+    // over the quotient domain.
+    //
+    // This only works if the trace domain is `gH'` and the quotient domain is `gK` for some subgroup `K` contained in `H'`.
+    // TODO: Make this explicit in `get_evaluations_on_domain` or otherwise fix this.
     let trace_on_quotient_domain = pcs.get_evaluations_on_domain(&trace_data, 0, quotient_domain);
 
+    // Compute the quotient polynomial `Q(x)` by evaluating
+    //          `C(T_1(x), ..., T_w(x), T_1(hx), ..., T_w(hx), selectors(x)) / Z_H(x)`
+    // at every point in the quotient domain. The degree of `Q(x)` is `<= deg(C(x)) - N = 2N - 2` in the case
+    // where `deg(C) = 3`. (See the discussion above constraint_degree for more details.)
     let quotient_values = quotient_values(
         air,
         public_values,
@@ -105,12 +174,35 @@ where
         constraint_count,
     );
 
+    // Due to `alpha`, evaluations of `Q` all lie in the extension field `E`.
+    // We flatten this into a matrix of `F` values by treating `E` as an `F`
+    // vector space and so separating each element of `E` into `e + 1 = [E: F]` elements of `F`.
+    //
+    // This is valid to do because our domain lies in the base field `F`. Hence we can split
+    // `Q(x)` into `e + 1` polynomials `Q_0(x), ... , Q_e(x)` each contained in `F`.
+    // such that `Q(x) = [Q_0(x), ... ,Q_e(x)]` holds for all `x` in `F`.
     let quotient_flat = RowMajorMatrix::new_col(quotient_values).flatten_to_base();
-    let quotient_chunks = quotient_domain.split_evals(quotient_degree, quotient_flat);
-    let qc_domains = quotient_domain.split_domains(quotient_degree);
 
+    // Currently each polynomial `Q_i(x)` is of degree `<= 2(N - 1)` and
+    // we have it's evaluations over a the coset `gK of size `2N`. Let `k` be the chosen
+    // generator of `K` which satisfies `k^2 = h`.
+    //
+    // We can split this coset into the sub-cosets `gH` and `gkH` each of size `N`.
+    // Define:  L_g(x)    = (x^N - (gk)^N)/(g^N - (gk)^N) = (x^N + g^N)/2g^N
+    //          L_{gk}(x) = (x^N - g^N)/(g^N - (gk)^N)    = -(x^N - g^N)/2g^N.
+    // Then `L_g` is equal to `1` on `gH` and `0` on `gkH` and `L_{gk}` is equal to `1` on `gkH` and `0` on `gH`.
+    //
+    // Thus we can decompose `Q_i(x) = L_{g}(x)q_{i0}(x) + L_{gk}(x)q_{i1}(x)` (Or an randomized version of this in the zk case)
+    // where `q_{i0}(x)` and `q_{i1}(x)` are polynomials of degree `<= N - 1`.
+    // Moreover the evaluations of `q_{i0}(x), q_{i1}(x)` on `gH` and `gkH` respectively are
+    // exactly the evaluations of `Q_i(x)` on `gH` and `gkH`.
+    // For each polynomial `q_{ij}`, compute the evaluation vector of `q_{ij}(x)` over `gH'`. We bit
+    // reverse the rows and hash the resulting matrix into a merkle tree.
+    //      quotient_commit contains the root of the tree
+    //      quotient_data contains the entire tree.
+    //          - quotient_data.leaves is a pair of matrices containing the `q_i0(x)` and `q_i1(x)`.
     let (quotient_commit, quotient_data) = info_span!("commit to quotient poly chunks")
-        .in_scope(|| pcs.commit_quotient(qc_domains, quotient_chunks));
+        .in_scope(|| pcs.commit_quotient(quotient_domain, quotient_flat, quotient_degree));
     challenger.observe(quotient_commit.clone());
 
     // If zk is enabled, we generate random extension field values of the size of the randomized trace. If `n` is the degree of the initial trace,
@@ -130,6 +222,8 @@ where
         (None, None)
     };
 
+    // Combine our commitments to the trace and quotient polynomials into a single object which
+    // will be passed to the verifier.
     let commitments = Commitments {
         trace: trace_commit,
         quotient_chunks: quotient_commit,
