30e419b12275a8b085728a66f0b9d04f748a994c
Thomas Coratger <60488569+tcoratger@users.noreply.github.com>
2025-05-20 22:46:02 +0200

    Update util/src/transpose.rs
    Co-authored-by: AngusG <angusgruen@gmail.com>

--- util/src/transpose.rs
+++ util/src/transpose.rs
@@ -34,7 +34,7 @@ unsafe fn transpose_in_place_square_small<T>(
 ) {
     unsafe {
         // Loop over upper triangle (excluding diagonal)
-        for i in x + 1..x + (1 << lb_size) {
+        for i in (x + 1)..(x + (1 << lb_size)) {
             for j in x..i {
                 // Compute memory offsets and swap M[i, j] <-> M[j, i]
                 swap(
