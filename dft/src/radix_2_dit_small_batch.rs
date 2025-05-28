53e2fbbabe11597a41e5aa19f2b87e5b44f91424
AngusG <angusgruen@gmail.com>
2025-05-28 15:36:04 -0400

    Code for idft

--- dft/src/radix_2_dit_small_batch.rs
+++ dft/src/radix_2_dit_small_batch.rs
@@ -1,5 +1,7 @@
 use alloc::vec::Vec;
 use core::cell::RefCell;
+use core::iter;
+use p3_matrix::util::reverse_matrix_index_bits;
 
 use p3_field::{Field, TwoAdicField};
 use p3_matrix::Matrix;
@@ -8,7 +10,10 @@ use p3_matrix::dense::{RowMajorMatrix, RowMajorMatrixViewMut};
 use p3_maybe_rayon::prelude::*;
 use p3_util::{log2_strict_usize, reverse_slice_index_bits};
 
-use crate::{Butterfly, DitButterfly, TwiddleFreeButterfly, TwoAdicSubgroupDft};
+use crate::{
+    Butterfly, DifButterfly, DitButterfly, TwiddleFreeButterfly, TwoAdicSubgroupDft,
+    divide_by_height,
+};
 
 /// Estimates the optimal workload size for `T` to fit in L1 cache.
 ///
@@ -43,6 +48,8 @@ pub struct Radix2DitSmallBatch<F: Field> {
     /// threads; consider using RwLock or finding a better design
     /// instead.
     twiddles: RefCell<Vec<Vec<F>>>,
+
+    inv_twiddles: RefCell<Vec<Vec<F>>>,
 }
 
 impl<F: TwoAdicField> Radix2DitSmallBatch<F> {
@@ -52,6 +59,7 @@ impl<F: TwoAdicField> Radix2DitSmallBatch<F> {
     pub fn new(n: usize) -> Self {
         let res = Self {
             twiddles: RefCell::default(),
+            inv_twiddles: RefCell::default(),
         };
         res.update_twiddles(n);
         res
@@ -72,11 +80,7 @@ impl<F: TwoAdicField> Radix2DitSmallBatch<F> {
         let nth_roots: Vec<_> = generator.powers().take(half_n).collect();
 
         (0..lg_n)
-            .map(|i| {
-                let mut twiddles: Vec<F> = nth_roots.iter().step_by(1 << i).copied().collect();
-                reverse_slice_index_bits(&mut twiddles);
-                twiddles
-            })
+            .map(|i| nth_roots.iter().step_by(1 << i).copied().collect())
             .collect()
     }
 
@@ -88,8 +92,27 @@ impl<F: TwoAdicField> Radix2DitSmallBatch<F> {
         // roots_of_unity_table(fft_len) returns a vector of twiddles of length log_2(fft_len).
         let curr_max_fft_len = 1 << self.twiddles.borrow().len();
         if fft_len > curr_max_fft_len {
-            let new_twiddles = self.roots_of_unity_table(fft_len);
+            let mut new_twiddles = self.roots_of_unity_table(fft_len);
+            let mut new_inv_twiddles: Vec<Vec<F>> = new_twiddles
+                .iter()
+                .map(|ts| {
+                    // The first twiddle is still one, instead of inverting, we can
+                    // just reverse and negate.
+                    iter::once(F::ONE)
+                        .chain(ts[1..].iter().rev().map(|&f| -f))
+                        .collect()
+                })
+                .collect();
+
+            new_twiddles.iter_mut().for_each(|ts| {
+                reverse_slice_index_bits(ts);
+            });
+            new_inv_twiddles.iter_mut().for_each(|ts| {
+                reverse_slice_index_bits(ts);
+            });
+
             self.twiddles.replace(new_twiddles);
+            self.inv_twiddles.replace(new_inv_twiddles);
         }
     }
 }
@@ -133,6 +156,54 @@ where
         // Finally we bit-reverse the matrix to ensure the output is in the correct order.
         mat.bit_reverse_rows()
     }
+
+    fn idft_batch(&self, mut mat: RowMajorMatrix<F>) -> RowMajorMatrix<F> {
+        let h = mat.height();
+        let w = mat.width();
+        let log_h = log2_strict_usize(h);
+
+        self.update_twiddles(h);
+        let root_table = self.inv_twiddles.borrow();
+        let len = root_table.len();
+        let root_table = &root_table[len - log_h..];
+
+        // Find the number of rows which can roughly fit in L1 cache.
+        // The strategy will be to do a standard round-by-round parallelization
+        // until the chunk size is smaller than `num_par_rows * mat.width()` after which we
+        // send `num_par_rows` chunks to each thread and do the remainder of the
+        // fft without transferring any more data between threads.
+        let num_par_rows = (workload_size::<F>() / w).next_power_of_two().min(h); // Ensure we don't exceed the height of the matrix.
+        let log_num_par_rows = log2_strict_usize(num_par_rows);
+        let chunk_size = num_par_rows * w;
+
+        reverse_matrix_index_bits(&mut mat);
+
+        // Once the blocks are small enough, we can split the matrix
+        // into chunks of size `chunk_size` and process them in parallel.
+        // This avoids passing data between threads, which can be expensive.
+        par_initial_layers(&mut mat.values, chunk_size, root_table, log_num_par_rows);
+
+        // For the layers involving blocks larger than `num_par_rows`, we will
+        // parallelize across the blocks.
+        for twiddles in root_table[log_num_par_rows..].iter() {
+            dif_layer_par(&mut mat.as_view_mut(), twiddles);
+        }
+
+        divide_by_height(&mut mat);
+
+        // Finally we bit-reverse the matrix to ensure the output is in the correct order.
+        mat
+    }
+
+    // fn coset_lde_batch(
+    //     &self,
+    //     mut mat: RowMajorMatrix<F>,
+    //     added_bits: usize,
+    //     shift: F,
+    // ) -> Self::Evaluations {
+    //     reverse_matrix_index_bits(&mut mat);
+    //     todo!();
+    // }
 }
 
 /// Applies one layer of the Radix-2 DIT FFT butterfly network making use of parallelization.
@@ -189,6 +260,60 @@ fn dit_layer_par<F: Field>(mat: &mut RowMajorMatrixViewMut<F>, twiddles: &[F]) {
         });
 }
 
+/// Applies one layer of the Radix-2 DIT FFT butterfly network making use of parallelization.
+///
+/// Splits the matrix into blocks of rows and performs in-place butterfly operations
+/// on each block. Uses a `TwiddleFreeButterfly` for the first pair and `DitButterfly`
+/// with precomputed twiddles for the rest.
+///
+/// Each block is processed in parallel, if the blocks are large enough they themselves
+/// are split into parallel sub-blocks.
+///
+/// # Arguments
+/// - `mat`: Mutable matrix whose height is a power of two.
+/// - `twiddles`: Precomputed twiddle factors for this layer.
+#[inline]
+fn dif_layer_par<F: Field>(mat: &mut RowMajorMatrixViewMut<F>, twiddles: &[F]) {
+    debug_assert!(
+        mat.height() % twiddles.len() == 0,
+        "Matrix height must be divisible by the number of twiddles"
+    );
+    let size = mat.values.len();
+    let num_blocks = twiddles.len();
+
+    let outer_block_size = size / num_blocks;
+    let half_outer_block_size = outer_block_size / 2;
+
+    mat.values
+        .par_chunks_exact_mut(outer_block_size)
+        .enumerate()
+        .for_each(|(ind, block)| {
+            // Split each block vertically into top (hi) and bottom (lo) halves
+            let (hi_chunk, lo_chunk) = block.split_at_mut(half_outer_block_size);
+
+            // If num_blocks is small, we probably are not using all available threads.
+            // This is an estimate of the optimal block size to make use of all threads.
+            // That being said, this is a bit of a magic number which I determined through
+            // experimentation. It is likely that different hardware might work better with
+            // a slightly different value.
+            const NUM_THREADS: usize = 32;
+            let inner_block_size = size / (2 * num_blocks).max(NUM_THREADS);
+
+            hi_chunk
+                .par_chunks_exact_mut(inner_block_size)
+                .zip(lo_chunk.par_chunks_exact_mut(inner_block_size))
+                .for_each(|(hi_chunk, lo_chunk)| {
+                    if ind == 0 {
+                        // The first pair doesn't require a twiddle factor
+                        TwiddleFreeButterfly.apply_to_rows(hi_chunk, lo_chunk);
+                    } else {
+                        // Apply DIT butterfly using the twiddle factor at index `ind - 1`
+                        DifButterfly(twiddles[ind]).apply_to_rows(hi_chunk, lo_chunk);
+                    }
+                });
+        });
+}
+
 /// Splits the matrix into chunks of size `chunk_size` and performs
 /// the remaining layers of the FFT in parallel on each chunk.
 ///
@@ -208,6 +333,29 @@ fn par_remaining_layers<F: Field>(
                 let start = index * num_twiddles_per_block;
                 let twiddle_range = start..(start + num_twiddles_per_block);
                 dit_layer(chunk, &(twiddles[twiddle_range]));
+            }
+        });
+}
+
+/// Splits the matrix into chunks of size `chunk_size` and performs
+/// the remaining layers of the FFT in parallel on each chunk.
+///
+/// This avoids passing data between threads, which can be expensive.
+#[inline]
+fn par_initial_layers<F: Field>(
+    mat: &mut [F],
+    chunk_size: usize,
+    root_table: &[Vec<F>],
+    log_num_par_rows: usize,
+) {
+    mat.par_chunks_exact_mut(chunk_size)
+        .enumerate()
+        .for_each(|(index, chunk)| {
+            for (layer, twiddles) in root_table[..log_num_par_rows].iter().enumerate() {
+                let num_twiddles_per_block = 1 << (log_num_par_rows - layer - 1);
+                let start = index * num_twiddles_per_block;
+                let twiddle_range = start..(start + num_twiddles_per_block);
+                dif_layer(chunk, &(twiddles[twiddle_range]));
             }
         });
 }
@@ -241,5 +389,37 @@ fn dit_layer<F: Field>(vec: &mut [F], twiddles: &[F]) {
 
             // Apply DIT butterfly using the twiddle factor at index `ind - 1`
             DitButterfly(twiddle).apply_to_rows(hi_chunk, lo_chunk);
+        });
+}
+
+/// Applies one layer of the Radix-2 DIT FFT butterfly network on a single core.
+///
+/// Splits the matrix into blocks of rows and performs in-place butterfly operations
+/// on each block.
+///
+/// # Arguments
+/// - `vec`: Mutable vector whose height is a power of two.
+/// - `twiddles`: Precomputed twiddle factors for this layer.
+#[inline]
+fn dif_layer<F: Field>(vec: &mut [F], twiddles: &[F]) {
+    debug_assert_eq!(
+        vec.len() % twiddles.len(),
+        0,
+        "Vector length must be divisible by the number of twiddles"
+    );
+    let size = vec.len();
+    let num_blocks = twiddles.len();
+
+    let block_size = size / num_blocks;
+    let half_block_size = block_size / 2;
+
+    vec.chunks_exact_mut(block_size)
+        .zip(twiddles)
+        .for_each(|(block, &twiddle)| {
+            // Split each block vertically into top (hi) and bottom (lo) halves
+            let (hi_chunk, lo_chunk) = block.split_at_mut(half_block_size);
+
+            // Apply DIT butterfly using the twiddle factor at index `ind - 1`
+            DifButterfly(twiddle).apply_to_rows(hi_chunk, lo_chunk);
         });
 }
