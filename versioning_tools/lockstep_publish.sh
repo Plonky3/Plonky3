b91644a80df167c286c89387df84f5c7fd340305
BGluth <gluthb@gmail.com>
2025-06-03 18:18:04 -0600

    Bump and release automation (#546)
    * Lockstep bumped all crates to `0.3.0`
    
    - Note that `0.2.0` was already published to `crates.io` but the crate
      ver`sion bump was not merged back to `main`.
    
    * All sub-crates now share workplace config
    
    - I'm also going to try setting `version` with `version.workspace` since
      we're planning on using lockstep versioning. May back away from this
      down the road if it ends up not working out.
    
    * Progress on sub-crate publish script
    
    * Put all local crates back to `0.2.0`
    
    - It's a bit weird. Currently all deps on crates.io are at `0.2.0` even
      though there doesn't exist any commit upstream that `0.2.0` was
      created from.
    - The idea here is we're going to create a "fake" `0.2.0` just so the
      publishing script works. However, this commit containing `0.2.0` will
      never exist in the published commit history (because of squashing),
      and this commit only serves to get the publish script working.
    
    * More work on publishing script
    
    * Renamed publish script
    
    - Realized that we actually probably want two seperate scripts after
      thinking about the workflow a bit more.
    - One script to bump versions (then we create a PR from this).
    - Another script to publish to `crates.io` once the PR eventually gets
      merged.
    
    * Split bump and versioning logic into two scripts
    
    - Fits the intended workflow a lot better this way.
    
    * Fixed bug in major bump surpressed detection logic
    
    - Was always triggering regardless.
    
    * Removed a debug line
    
    * Added a config for git-cliff
    
    - Looks for merge commits and takes their first lines for the changelog.
    
    * Added a script for prepending to the changelog
    
    - This is just a wrapper with some simple checks for invoking
      `git-cliff` for changelog generation.
    
    * Moved versioning scripts to their own directory
    
    * Fixed changelog gen script not working outside of the project root
    
    * Changelog gen now lists all authors that contributed
    
    * Bit of cleanup and fixes in the versioning scripts
    
    * Added missing flags for workspace version bump
    
    * Few more cleanup and fixes in versioning scripts
    
    * A bit more refactoring of versioning scripts
    
    * Added actual dangerious publishing logic
    
    * Now creates the verion bump branch on remote if missing
    
    - Rational is that a lot of the time the user will create a version bump
      PR just from running the script, so it's not expected that they will
      have pushed their new branch to remote manually.
    
    * Added a first draft of a readme for the versioning tools
    
    - Will fill in the script specific stuff later.
    
    * Removed contents in `CHANGELOG`
    
    - Did not mean to commit this...
    
    * Renamed `CHANGELOG` --> `CHANGELOG.md`
    
    * Fixed incorrect sub-crate versions being used after rebase

---
+++ versioning_tools/lockstep_publish.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+# Publish all subcrates to `crates.io` once we merged a version bump PR back to main.
+
+# Note that the intended workflow of this script is:
+# - Have a PR of a successful run of `lockstep_version_bump.sh` merged back into `main`.
+# - Run this script on the commit that got merged back into `main`.
+
+# 1 --> Prompt string
+# 2 --> Yes string
+get_yes_no_input_from_user_and_exit_on_no() {
+    echo "${1} (y/n)"
+
+    read -r input
+    if [ "$input" = "y" ]; then
+        echo "$2"
+    else
+        exit 0
+    fi
+}
+
+# If a version bump occurred on this commit (highest version tag is present on this commit), then this will report that nothing has changed since the last bump (because we just bumped on this commit).
+changed_res=$(cargo workspaces changed --error-on-empty)
+
+if [ ! $? ]; then
+    # The number of changes since the latest release tag should be zero if we are on the corresponding commit.
+    num_changed=$(echo "$changed_res" | wc -l)
+
+    if [ "$num_changed" -gt 0 ]; then
+        latest_local_release_tag=$(git tag -l | grep -E 'v[0-9]+\.[0-9]+\.[0-9]+' | sort -r | head -n 1)
+        
+        echo "Changes detected since the latest version release tag (${latest_local_release_tag})!"
+        echo "Make sure to run \`lockstep_version_bump.sh\` before running this script."
+        exit 1
+    fi
+
+    get_yes_no_input_from_user_and_exit_on_no "Do you want to publish a release now?" "Publishing to crates.io..."
+else
+    echo "The latest version tag is not on this commit. Run \`lockstep_version_bump.sh\` to create a commit for publishing."
+    exit 1
+fi
+
+# User green-lighted publishing.
+# Because a failure during publishing could result in a desync between local and remote (and thus a big headache), we're going to do a dry run first in order to detect any errors during publishing.
+if ! cargo workspaces publish --dry-run --no-git-push --allow-branch main; then
+    echo "crates.io publishing dry run failed."
+    exit 1
+fi
+
+# Publishing dry run succeeded. Do a real publish now.
+
+get_yes_no_input_from_user_and_exit_on_no "Publishing dry run succeeded! Perform a real publish now? (Be careful!!! This is non-reversible!)" "Publishing to crates.io..."
+
+# Perform a real publish. Hopefully nothing with break at this point.
+if ! cargo workspaces publish --allow-branch main; then
+    echo "crates.io publishing failed. This is probably not good..."
+    exit 1
+fi
