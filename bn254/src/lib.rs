6d0facc2623422613492e42af42e6d1e4011afbd
AngusG <angusgruen@gmail.com>
2025-06-30 18:54:48 -0400

    oops, left my comment out of #![no_std] in.

--- bn254/src/lib.rs
+++ bn254/src/lib.rs
@@ -1,5 +1,5 @@
 //! The scalar field of the BN254 curve, defined as `F_P` where `P = 21888242871839275222246405745257275088548364400416034343698204186575808495617`.
-// #![no_std]
+#![no_std]
 
 extern crate alloc;
 
