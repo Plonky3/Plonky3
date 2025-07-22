f4ff7576fcbd8e9e64ede4efd1bd44944688aa6a
Avory <avorycorelli@gmail.com>
2025-07-22 18:12:14 +0300

    Update util.rs

--- rescue/src/util.rs
+++ rescue/src/util.rs
@@ -1,20 +1,4 @@
-use alloc::vec;
-use alloc::vec::Vec;
-
 use p3_util::log2_ceil_u64;
-use sha3::Shake256;
-use sha3::digest::{ExtendableOutput, Update, XofReader};
-
-/// Compute the SHAKE256 variant of SHA-3.
-/// This is used to generate the round constants from a seed string.
-pub(crate) fn shake256_hash(seed_bytes: &[u8], num_bytes: usize) -> Vec<u8> {
-    let mut hasher = Shake256::default();
-    hasher.update(seed_bytes);
-    let mut reader = hasher.finalize_xof();
-    let mut result = vec![0u8; num_bytes];
-    reader.read(&mut result);
-    result
-}
 
 /// Return y such that |y - 2^x| < tol for x an f32.
 ///
