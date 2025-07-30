88b6e2d74c38823ecdfd1e1c095428df211f2fd8
AngusG <angusgruen@gmail.com>
2025-07-29 21:39:40 -0400

    Joining packed_add's together

--- monty-31/src/x86_64_avx2/packing.rs
+++ monty-31/src/x86_64_avx2/packing.rs
@@ -1245,72 +1245,44 @@ where
     let mut res = [MontyField31::ZERO; WIDTH];
     match WIDTH {
         1 => res[0] = a[0] + b[0],
-        4 => quartic_add_packed(a, b, &mut res),
-        5 => quintic_add_packed(a, b, &mut res),
-        8 => octic_add_packed(a, b, &mut res),
-        _ => panic!("Unsupported width for packed addition"),
+        4 => {
+            // Perfectly fits into a m128i vector. The compiler is good at
+            // optimising this into AVX2 instructions in cases where we need to
+            // do multiple additions.
+            let out: [MontyField31<FP>; 4] = unsafe {
+                let a: __m128i = transmute([a[0], a[1], a[2], a[3]]);
+                let b: __m128i = transmute([b[0], b[1], b[2], b[3]]);
+                let p: __m128i = x86_64::_mm_set1_epi32(FP::PRIME as i32);
+                transmute(mm128_mod_add(a, b, p))
+            };
+
+            res.copy_from_slice(&out);
+        }
+        5 => {
+            // We fit what we can into a m128i vector. The final add on
+            // is done using a scalar addition. This seems to be faster than
+            // trying to fit everything into an AVX2 vector and makes it much
+            // easier for the compiler to optimise in cases where it needs to
+            // do multiple additions.
+            let out: [MontyField31<FP>; 4] = unsafe {
+                let a: __m128i = transmute([a[0], a[1], a[2], a[3]]);
+                let b: __m128i = transmute([b[0], b[1], b[2], b[3]]);
+                let p: __m128i = x86_64::_mm_set1_epi32(FP::PRIME as i32);
+                transmute(mm128_mod_add(a, b, p))
+            };
+            res[4] = a[4] + b[4];
+
+            res[..4].copy_from_slice(&out);
+        }
+        8 => {
+            // This perfectly fits into a single AVX2 vector.
+            let packed_a = PackedMontyField31AVX2([a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7]]);
+            let packed_b = PackedMontyField31AVX2([b[0], b[1], b[2], b[3], b[4], b[5], b[6], b[7]]);
+            let out = packed_a + packed_b;
+
+            res.copy_from_slice(&out.0);
+        }
+        _ => panic!("Currently unsupported width for packed addition"),
     }
     res
-}
-
-/// Addition in a quartic binomial extension field.
-#[inline(always)]
-fn quartic_add_packed<FP, const WIDTH: usize>(
-    a: &[MontyField31<FP>; WIDTH],
-    b: &[MontyField31<FP>; WIDTH],
-    res: &mut [MontyField31<FP>; WIDTH],
-) where
-    FP: FieldParameters + BinomialExtensionData<WIDTH>,
-{
-    assert_eq!(WIDTH, 4);
-
-    let out: [MontyField31<FP>; 4] = unsafe {
-        let a: __m128i = transmute([a[0], a[1], a[2], a[3]]);
-        let b: __m128i = transmute([b[0], b[1], b[2], b[3]]);
-        let p: __m128i = x86_64::_mm_set1_epi32(FP::PRIME as i32);
-        transmute(mm128_mod_add(a, b, p))
-    };
-
-    res.copy_from_slice(&out);
-}
-
-/// Addition in a quintic binomial extension field.
-#[inline(always)]
-fn quintic_add_packed<FP, const WIDTH: usize>(
-    a: &[MontyField31<FP>; WIDTH],
-    b: &[MontyField31<FP>; WIDTH],
-    res: &mut [MontyField31<FP>; WIDTH],
-) where
-    FP: FieldParameters + BinomialExtensionData<WIDTH>,
-{
-    assert_eq!(WIDTH, 5);
-
-    let out: [MontyField31<FP>; 4] = unsafe {
-        let a: __m128i = transmute([a[0], a[1], a[2], a[3]]);
-        let b: __m128i = transmute([b[0], b[1], b[2], b[3]]);
-        let p: __m128i = x86_64::_mm_set1_epi32(FP::PRIME as i32);
-        transmute(mm128_mod_add(a, b, p))
-    };
-    res[4] = a[4] + b[4];
-
-    res[..4].copy_from_slice(&out);
-}
-
-/// Addition in an octic binomial extension field.
-#[inline(always)]
-fn octic_add_packed<FP, const WIDTH: usize>(
-    a: &[MontyField31<FP>; WIDTH],
-    b: &[MontyField31<FP>; WIDTH],
-    res: &mut [MontyField31<FP>; WIDTH],
-) where
-    FP: FieldParameters + BinomialExtensionData<WIDTH>,
-{
-    assert_eq!(WIDTH, 8);
-
-    // We only care about the first four entries.
-    let packed_a = PackedMontyField31AVX2([a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7]]);
-    let packed_b = PackedMontyField31AVX2([b[0], b[1], b[2], b[3], b[4], b[5], b[6], b[7]]);
-    let out = packed_a + packed_b;
-
-    res.copy_from_slice(&out.0[..]);
 }
