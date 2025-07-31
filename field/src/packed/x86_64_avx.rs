92d06cdee67f3af36285eaa675c129f8a39924a2
AngusG <angusgruen@gmail.com>
2025-07-31 14:36:42 -0400

    rm __m128i support.
    Don't need that until the next pr

--- field/src/packed/x86_64_avx.rs
+++ field/src/packed/x86_64_avx.rs
@@ -2,7 +2,7 @@
 
 #[cfg(all(feature = "nightly-features", target_feature = "avx512f"))]
 use core::arch::x86_64::__m512i;
-use core::arch::x86_64::{self, __m128i, __m256i};
+use core::arch::x86_64::{self, __m256i};
 
 // Goal: Compute r = lhs + rhs mod P for lhs, rhs <= P < 2^31
 // Output should lie in [0, P).
@@ -15,31 +15,6 @@ use core::arch::x86_64::{self, __m128i, __m256i};
 //      - If t is in [P, 2 P], then u is in [0, P] and r = u lies in the correct range.
 //   As both t and u are both equal to lhs + rhs mod P, we conclude that
 //   r = (lhs + rhs) mod P and lies in the correct range.
-
-/// Add the two packed vectors `a` and `b` modulo `p`.
-///
-/// This allows us to add elements at once.
-///
-/// Assumes that `p` is less than `2^31` and `a + b <= 2P`.
-/// If the inputs are not in this range, the result may be incorrect.
-/// The result will be in the range `[0, P]` and equal to `(a + b) mod p`.
-/// It will be equal to `P` if and only if `a + b = 2P`.
-#[inline(always)]
-#[must_use]
-pub fn mm128_mod_add(a: __m128i, b: __m128i, p: __m128i) -> __m128i {
-    // We want this to compile to:
-    //      paddd   t, lhs, rhs
-    //      psubd   u, t, P
-    //      pminud  res, t, u
-    // throughput: 1 cyc/vec (8 els/cyc)
-    // latency: 3 cyc
-
-    unsafe {
-        let t = x86_64::_mm_add_epi32(a, b);
-        let u = x86_64::_mm_sub_epi32(t, p);
-        x86_64::_mm_min_epu32(t, u)
-    }
-}
 
 /// Add the two packed vectors `a` and `b` modulo `p`.
 ///
