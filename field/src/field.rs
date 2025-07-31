0c720928e1389cdbd9ab1ca966d147000b32dfc2
AngusG <angusgruen@gmail.com>
2025-07-31 10:44:09 -0400

    Adding TODO comments for the Neon implementations.

--- field/src/field.rs
+++ field/src/field.rs
@@ -142,16 +142,13 @@ pub trait PrimeCharacteristicRing:
     /// # Panics
     /// The function will panic if the field has characteristic 2.
     #[must_use]
-    fn halve(&self) -> Self;
-    //  {
-    //     // This should be overwritten by most field implementations.
-    //     let half = Self::from_prime_subfield(
-    //         Self::PrimeSubfield::TWO
-    //             .try_inverse()
-    //             .expect("Cannot divide by 2 in fields with characteristic 2"),
-    //     );
-    //     self.clone() * half
-    // }
+    fn halve(&self) -> Self {
+        // This must be overwritten by most field implementations as this definition
+        // is circular when PrimeSubfield = Self. It should also be overwritten by
+        // most rings to avoid the multiplication.
+        let half = Self::from_prime_subfield(Self::PrimeSubfield::ONE.halve());
+        self.clone() * half
+    }
 
     /// The elementary function `square(a) = a^2`.
     ///
