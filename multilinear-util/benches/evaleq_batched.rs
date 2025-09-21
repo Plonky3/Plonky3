a386138e2c90680abfd91a74566f9d7beba3c8da
Thomas Coratger <thomas.coratger@gmail.com>
2025-09-22 00:06:55 +0200

    bench

--- multilinear-util/benches/evaleq_batched.rs
+++ multilinear-util/benches/evaleq_batched.rs
@@ -245,8 +245,8 @@ fn bench_batch_vs_single_base(c: &mut Criterion) {
 
 criterion_group!(
     benches,
-    // bench_eval_eq_batch,
-    // bench_eval_eq_base_batch,
+    bench_eval_eq_batch,
+    bench_eval_eq_base_batch,
     bench_batch_vs_single_extension,
     bench_batch_vs_single_base
 );
