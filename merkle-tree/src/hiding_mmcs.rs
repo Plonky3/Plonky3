2d9dcc82e9670170077ed1a472c2e20603090bfc
Thomas Coratger <60488569+tcoratger@users.noreply.github.com>
2025-09-29 19:47:23 +0200

    doc: add better doc in air and fix TODO (#1061)
    * doc: add better doc in air and fix TODO
    
    * Update air/src/virtual_column.rs
    
    Co-authored-by: AngusG <angusgruen@gmail.com>
    
    * Update air/src/virtual_column.rs
    
    Co-authored-by: AngusG <angusgruen@gmail.com>
    
    * Update air/src/virtual_column.rs
    
    Co-authored-by: AngusG <angusgruen@gmail.com>
    
    * Update air/src/virtual_column.rs
    
    Co-authored-by: AngusG <angusgruen@gmail.com>
    
    * Update air/src/virtual_column.rs
    
    Co-authored-by: AngusG <angusgruen@gmail.com>
    
    * Update air/src/virtual_column.rs
    
    Co-authored-by: AngusG <angusgruen@gmail.com>
    
    * Update air/src/virtual_column.rs
    
    Co-authored-by: AngusG <angusgruen@gmail.com>
    
    * Update air/src/virtual_column.rs
    
    Co-authored-by: AngusG <angusgruen@gmail.com>
    
    * Update air/src/virtual_column.rs
    
    Co-authored-by: AngusG <angusgruen@gmail.com>
    
    * Update air/src/virtual_column.rs
    
    Co-authored-by: AngusG <angusgruen@gmail.com>
    
    * Update air/src/virtual_column.rs
    
    Co-authored-by: AngusG <angusgruen@gmail.com>
    
    * Update air/src/virtual_column.rs
    
    Co-authored-by: AngusG <angusgruen@gmail.com>
    
    * Update air/src/virtual_column.rs
    
    Co-authored-by: AngusG <angusgruen@gmail.com>
    
    ---------
    
    Co-authored-by: AngusG <angusgruen@gmail.com>

--- merkle-tree/src/hiding_mmcs.rs
+++ merkle-tree/src/hiding_mmcs.rs
@@ -99,7 +99,7 @@ where
         prover_data: &Self::ProverData<M>,
     ) -> BatchOpening<P::Value, Self> {
         let (salted_openings, siblings) = self.inner.open_batch(index, prover_data).unpack();
-        let (openings, salts): (Vec<_>, Vec<_>) = salted_openings
+        let (openings, salts) = salted_openings
             .into_iter()
             .map(|row| {
                 let (a, b) = row.split_at(row.len() - SALT_ELEMS);
