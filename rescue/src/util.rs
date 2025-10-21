675aaf46ccf3f44101474f5b231ef594bca4b524
AJoX <shuraev2707@gmail.com>
2025-10-21 20:00:39 +0200

    Update util.rs

--- rescue/src/util.rs
+++ rescue/src/util.rs
@@ -7,7 +7,7 @@ use sha3::digest::{ExtendableOutput, Update, XofReader};
 
 /// Compute the SHAKE256 variant of SHA-3.
 /// This is used to generate the round constants from a seed string.
-pub(crate) fn shake256_hash(seed_bytes: &[u8], num_bytes: usize) -> Vec<u8> {
+pub fn shake256_hash(seed_bytes: &[u8], num_bytes: usize) -> Vec<u8> {
     let mut hasher = Shake256::default();
     hasher.update(seed_bytes);
     let mut reader = hasher.finalize_xof();
