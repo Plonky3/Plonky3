af985084b6fea39736242f5550110e30bd94b84b
Thomas Coratger <thomas.coratger@gmail.com>
2025-10-23 12:07:15 +0200

    fix typo

--- circle/src/deep_quotient.rs
+++ circle/src/deep_quotient.rs
@@ -139,7 +139,7 @@ impl<F: ComplexExtendable, M: Matrix<F>> CircleEvaluations<F, M> {
             .map(|x| deep_quotient_vanishing_part(x, zeta, alpha_pow_width))
             .unzip();
 
-        // Invert the deominators.
+        // Invert the denominators.
         let vp_denom_invs = batch_multiplicative_inverse(&vp_denoms);
 
         // Precompute powers of alpha for constraint part computation
