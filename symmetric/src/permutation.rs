88d42bb81ea152f6e8528d9026df4c3289eb1f24
AngusG <angusgruen@gmail.com>
2025-12-01 20:07:46 -0500

    Add comment to Permutation trait method indicating the circular nature of the trait definitions.

--- symmetric/src/permutation.rs
+++ symmetric/src/permutation.rs
@@ -1,5 +1,10 @@
 /// A permutation in the mathematical sense.
 pub trait Permutation<T: Clone>: Clone + Sync {
+    // The methods permute, permute_mut are defined in a circular manner
+    // so you only need to implement one of them and will get the other
+    // for free. If you implement neither, this will cause a run time
+    // error.
+
     #[inline(always)]
     fn permute(&self, mut input: T) -> T {
         self.permute_mut(&mut input);
