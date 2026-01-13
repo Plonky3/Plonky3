cc42887c59658b3c907ef33d4cd357a406dc137c
sashaodessa <140454972+sashaodessa@users.noreply.github.com>
2026-01-13 23:03:02 +0100

    Update lib.rs

--- util/src/lib.rs
+++ util/src/lib.rs
@@ -50,13 +50,13 @@ pub const fn log2_strict_usize(n: usize) -> usize {
 /// Returns `[0, ..., N - 1]`.
 #[must_use]
 pub const fn indices_arr<const N: usize>() -> [usize; N] {
-    let mut arr = [0; N];
+    let mut indices_arr = [0; N];
     let mut i = 0;
     while i < N {
-        arr[i] = i;
+        indices_arr[i] = i;
         i += 1;
     }
-    arr
+    indices_arr
 }
 
 #[inline]
