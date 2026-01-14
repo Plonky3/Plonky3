0e7ca3271580181ad220dad2dfa55f7aa6c89763
sashaodessa <140454972+sashaodessa@users.noreply.github.com>
2026-01-14 09:57:11 +0100

    Update lib.rs

--- maybe-rayon/src/lib.rs
+++ maybe-rayon/src/lib.rs
@@ -1,50 +1,21 @@
 #![no_std]
 
-#[cfg(feature = "parallel")]
-pub mod prelude {
-    use core::marker::{Send, Sync};
-
-    pub use rayon::prelude::*;
-    pub use rayon::{current_num_threads, join};
-
-    pub trait SharedExt: ParallelIterator {
-        fn par_fold_reduce<Acc, Id, F, R>(self, identity: Id, fold_op: F, reduce_op: R) -> Acc
-        where
-            Acc: Send,
-            Id: Fn() -> Acc + Sync + Send,
-            F: Fn(Acc, Self::Item) -> Acc + Sync + Send,
-            R: Fn(Acc, Acc) -> Acc + Sync + Send;
-    }
-
-    impl<I: ParallelIterator> SharedExt for I {
-        #[inline]
-        fn par_fold_reduce<Acc, Id, F, R>(self, identity: Id, fold_op: F, reduce_op: R) -> Acc
-        where
-            Acc: Send,
-            Id: Fn() -> Acc + Sync + Send,
-            F: Fn(Acc, Self::Item) -> Acc + Sync + Send,
-            R: Fn(Acc, Acc) -> Acc + Sync + Send,
-        {
-            self.fold(&identity, fold_op).reduce(&identity, reduce_op)
-        }
-    }
-}
-
-#[cfg(feature = "parallel")]
-pub mod iter {
-    pub use rayon::iter::{repeat, repeat_n};
-}
-
 #[cfg(not(feature = "parallel"))]
 mod serial;
 
-#[cfg(not(feature = "parallel"))]
 pub mod prelude {
+    use core::marker::{Send, Sync};
+
+    #[cfg(feature = "parallel")]
+    pub use rayon::prelude::*;
+    #[cfg(feature = "parallel")]
+    pub use rayon::{current_num_threads, join};
+
+    #[cfg(not(feature = "parallel"))]
     pub use core::iter::{
         ExactSizeIterator as IndexedParallelIterator, Iterator as ParallelIterator,
     };
-    use core::marker::{Send, Sync};
-
+    #[cfg(not(feature = "parallel"))]
     pub use super::serial::*;
 
     pub trait SharedExt: ParallelIterator {
@@ -58,19 +29,31 @@ pub mod prelude {
 
     impl<I: ParallelIterator> SharedExt for I {
         #[inline]
-        fn par_fold_reduce<Acc, Id, F, R>(self, identity: Id, fold_op: F, _reduce_op: R) -> Acc
+        fn par_fold_reduce<Acc, Id, F, R>(self, identity: Id, fold_op: F, reduce_op: R) -> Acc
         where
             Acc: Send,
             Id: Fn() -> Acc + Sync + Send,
             F: Fn(Acc, Self::Item) -> Acc + Sync + Send,
             R: Fn(Acc, Acc) -> Acc + Sync + Send,
         {
-            self.fold(identity(), fold_op)
+            #[cfg(feature = "parallel")]
+            {
+                self.fold(&identity, fold_op).reduce(&identity, reduce_op)
+            }
+
+            #[cfg(not(feature = "parallel"))]
+            {
+                let _ = reduce_op;
+                self.fold(identity(), fold_op)
+            }
         }
     }
 }
 
-#[cfg(not(feature = "parallel"))]
 pub mod iter {
+    #[cfg(feature = "parallel")]
+    pub use rayon::iter::{repeat, repeat_n};
+
+    #[cfg(not(feature = "parallel"))]
     pub use core::iter::{repeat, repeat_n};
 }
