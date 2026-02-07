12eb1a02f05a35aa09d10862503e842123c07bd9
Tom Wambsgans <101908066+TomWambsgans@users.noreply.github.com>
2026-02-07 21:37:36 +0400

    Update monty-31/src/aarch64_neon/packing.rs
    Co-authored-by: Thomas Coratger <60488569+tcoratger@users.noreply.github.com>

--- monty-31/src/aarch64_neon/packing.rs
+++ monty-31/src/aarch64_neon/packing.rs
@@ -955,7 +955,6 @@ pub(crate) fn quintic_mul_packed_trinomial<FP: FieldParameters>(
     b: &[MontyField31<FP>; 5],
     res: &mut [MontyField31<FP>; 5],
 ) {
-    use p3_field::PrimeCharacteristicRing;
 
     let b0_minus_b3 = b[0] - b[3];
     let b1_minus_b4 = b[1] - b[4];
