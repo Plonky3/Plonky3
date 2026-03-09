107d48c8cb3dc52f359da0a60f8719677438199e
Thomas Coratger <thomas.coratger@gmail.com>
2026-03-09 13:34:03 +0100

    rm HL

--- goldilocks/src/poseidon2.rs
+++ goldilocks/src/poseidon2.rs
@@ -5,9 +5,9 @@ use alloc::vec::Vec;
 use p3_field::{Algebra, InjectiveMonomial, PrimeCharacteristicRing};
 use p3_poseidon2::{
     ExternalLayer, ExternalLayerConstants, ExternalLayerConstructor, GenericPoseidon2LinearLayers,
-    HLMDSMat4, InternalLayer, InternalLayerConstructor, MDSMat4, Poseidon2,
-    add_rc_and_sbox_generic, external_initial_permute_state, external_terminal_permute_state,
-    internal_permute_state, matmul_internal,
+    InternalLayer, InternalLayerConstructor, MDSMat4, Poseidon2, add_rc_and_sbox_generic,
+    external_initial_permute_state, external_terminal_permute_state, internal_permute_state,
+    matmul_internal,
 };
 
 use crate::Goldilocks;
@@ -30,19 +30,6 @@ pub type Poseidon2Goldilocks<const WIDTH: usize> = crate::Poseidon2GoldilocksFus
 pub type Poseidon2Goldilocks<const WIDTH: usize> = Poseidon2<
     Goldilocks,
     Poseidon2ExternalLayerGoldilocks<WIDTH>,
-    Poseidon2InternalLayerGoldilocks,
-    WIDTH,
-    GOLDILOCKS_S_BOX_DEGREE,
->;
-
-/// An alternative Poseidon2 implementation for the Goldilocks field using the HL external matrix.
-///
-/// It acts on arrays of the form `[Goldilocks; WIDTH]`.
-/// This implementation is slightly slower than `Poseidon2Goldilocks` as it uses a slower matrix
-/// for the external rounds.
-pub type Poseidon2GoldilocksHL<const WIDTH: usize> = Poseidon2<
-    Goldilocks,
-    Poseidon2ExternalLayerGoldilocksHL<WIDTH>,
     Poseidon2InternalLayerGoldilocks,
     WIDTH,
     GOLDILOCKS_S_BOX_DEGREE,
@@ -459,252 +446,13 @@ impl GenericPoseidon2LinearLayers<20> for GenericPoseidon2LinearLayersGoldilocks
     }
 }
 
-/// The external layers of the Poseidon2 permutation using the HL external matrix.
-#[derive(Clone)]
-pub struct Poseidon2ExternalLayerGoldilocksHL<const WIDTH: usize> {
-    pub(crate) external_constants: ExternalLayerConstants<Goldilocks, WIDTH>,
-}
-
-impl<const WIDTH: usize> ExternalLayerConstructor<Goldilocks, WIDTH>
-    for Poseidon2ExternalLayerGoldilocksHL<WIDTH>
-{
-    fn new_from_constants(external_constants: ExternalLayerConstants<Goldilocks, WIDTH>) -> Self {
-        Self { external_constants }
-    }
-}
-
-impl<A: Algebra<Goldilocks> + InjectiveMonomial<GOLDILOCKS_S_BOX_DEGREE>, const WIDTH: usize>
-    ExternalLayer<A, WIDTH, GOLDILOCKS_S_BOX_DEGREE> for Poseidon2ExternalLayerGoldilocksHL<WIDTH>
-{
-    /// Perform the initial external layers of the Poseidon2 permutation on the given state.
-    fn permute_state_initial(&self, state: &mut [A; WIDTH]) {
-        external_initial_permute_state(
-            state,
-            self.external_constants.get_initial_constants(),
-            add_rc_and_sbox_generic,
-            &HLMDSMat4,
-        );
-    }
-
-    /// Perform the terminal external layers of the Poseidon2 permutation on the given state.
-    fn permute_state_terminal(&self, state: &mut [A; WIDTH]) {
-        external_terminal_permute_state(
-            state,
-            self.external_constants.get_terminal_constants(),
-            add_rc_and_sbox_generic,
-            &HLMDSMat4,
-        );
-    }
-}
-
-pub const HL_GOLDILOCKS_8_EXTERNAL_ROUND_CONSTANTS: [[[u64; 8]; 4]; 2] = [
-    [
-        [
-            0xdd5743e7f2a5a5d9,
-            0xcb3a864e58ada44b,
-            0xffa2449ed32f8cdc,
-            0x42025f65d6bd13ee,
-            0x7889175e25506323,
-            0x34b98bb03d24b737,
-            0xbdcc535ecc4faa2a,
-            0x5b20ad869fc0d033,
-        ],
-        [
-            0xf1dda5b9259dfcb4,
-            0x27515210be112d59,
-            0x4227d1718c766c3f,
-            0x26d333161a5bd794,
-            0x49b938957bf4b026,
-            0x4a56b5938b213669,
-            0x1120426b48c8353d,
-            0x6b323c3f10a56cad,
-        ],
-        [
-            0xce57d6245ddca6b2,
-            0xb1fc8d402bba1eb1,
-            0xb5c5096ca959bd04,
-            0x6db55cd306d31f7f,
-            0xc49d293a81cb9641,
-            0x1ce55a4fe979719f,
-            0xa92e60a9d178a4d1,
-            0x002cc64973bcfd8c,
-        ],
-        [
-            0xcea721cce82fb11b,
-            0xe5b55eb8098ece81,
-            0x4e30525c6f1ddd66,
-            0x43c6702827070987,
-            0xaca68430a7b5762a,
-            0x3674238634df9c93,
-            0x88cee1c825e33433,
-            0xde99ae8d74b57176,
-        ],
-    ],
-    [
-        [
-            0x014ef1197d341346,
-            0x9725e20825d07394,
-            0xfdb25aef2c5bae3b,
-            0xbe5402dc598c971e,
-            0x93a5711f04cdca3d,
-            0xc45a9a5b2f8fb97b,
-            0xfe8946a924933545,
-            0x2af997a27369091c,
-        ],
-        [
-            0xaa62c88e0b294011,
-            0x058eb9d810ce9f74,
-            0xb3cb23eced349ae4,
-            0xa3648177a77b4a84,
-            0x43153d905992d95d,
-            0xf4e2a97cda44aa4b,
-            0x5baa2702b908682f,
-            0x082923bdf4f750d1,
-        ],
-        [
-            0x98ae09a325893803,
-            0xf8a6475077968838,
-            0xceb0735bf00b2c5f,
-            0x0a1a5d953888e072,
-            0x2fcb190489f94475,
-            0xb5be06270dec69fc,
-            0x739cb934b09acf8b,
-            0x537750b75ec7f25b,
-        ],
-        [
-            0xe9dd318bae1f3961,
-            0xf7462137299efe1a,
-            0xb1f6b8eee9adb940,
-            0xbdebcc8a809dfe6b,
-            0x40fc1f791b178113,
-            0x3ac1c3362d014864,
-            0x9a016184bdb8aeba,
-            0x95f2394459fbc25e,
-        ],
-    ],
-];
-pub const HL_GOLDILOCKS_8_INTERNAL_ROUND_CONSTANTS: [u64; 22] = [
-    0x488897d85ff51f56,
-    0x1140737ccb162218,
-    0xa7eeb9215866ed35,
-    0x9bd2976fee49fcc9,
-    0xc0c8f0de580a3fcc,
-    0x4fb2dae6ee8fc793,
-    0x343a89f35f37395b,
-    0x223b525a77ca72c8,
-    0x56ccb62574aaa918,
-    0xc4d507d8027af9ed,
-    0xa080673cf0b7e95c,
-    0xf0184884eb70dcf8,
-    0x044f10b0cb3d5c69,
-    0xe9e3f7993938f186,
-    0x1b761c80e772f459,
-    0x606cec607a1b5fac,
-    0x14a0c2e1d45f03cd,
-    0x4eace8855398574f,
-    0xf905ca7103eff3e6,
-    0xf8c8f8d20862c059,
-    0xb524fe8bdd678e5a,
-    0xfbb7865901a1ec41,
-];
-
 #[cfg(test)]
 mod tests {
-    use core::array;
-
     use p3_field::PrimeCharacteristicRing;
-    use p3_poseidon2::Poseidon2;
-    use p3_symmetric::Permutation;
 
     use super::*;
 
     type F = Goldilocks;
-
-    // A function which recreates the HL Poseidon2 implementation for Goldilocks width 8.
-    fn hl_poseidon2_goldilocks_width_8(input: &mut [F; 8]) {
-        const WIDTH: usize = 8;
-
-        // Our Poseidon2 implementation.
-        let poseidon2: Poseidon2GoldilocksHL<WIDTH> = Poseidon2::new(
-            ExternalLayerConstants::<Goldilocks, WIDTH>::new_from_saved_array(
-                HL_GOLDILOCKS_8_EXTERNAL_ROUND_CONSTANTS,
-                Goldilocks::new_array,
-            ),
-            Goldilocks::new_array(HL_GOLDILOCKS_8_INTERNAL_ROUND_CONSTANTS).to_vec(),
-        );
-
-        poseidon2.permute_mut(input);
-    }
-
-    /// Test on the constant 0 input.
-    #[test]
-    fn test_poseidon2_width_8_zeros() {
-        let mut input: [F; 8] = [Goldilocks::ZERO; 8];
-
-        let expected: [F; 8] = Goldilocks::new_array([
-            18411014882916974180,
-            11853243659833051879,
-            2553980965289355629,
-            5435536888074291950,
-            11414233414141119281,
-            15612551474745760831,
-            15745650375519692590,
-            4546169000627739578,
-        ]);
-        hl_poseidon2_goldilocks_width_8(&mut input);
-        assert_eq!(input, expected);
-    }
-
-    /// Test on the input 0..16.
-    #[test]
-    fn test_poseidon2_width_8_range() {
-        let mut input: [F; 8] = array::from_fn(|i| F::from_u64(i as u64));
-
-        let expected: [F; 8] = Goldilocks::new_array([
-            14758079437403499858,
-            4768220715988658038,
-            9988209636190012306,
-            8808631253505580005,
-            17526572370116009359,
-            1590367810676479047,
-            13027328087430412699,
-            13357513690486523336,
-        ]);
-        hl_poseidon2_goldilocks_width_8(&mut input);
-        assert_eq!(input, expected);
-    }
-
-    /// Test on a roughly random input.
-    /// This random input is generated by the following sage code:
-    /// set_random_seed(2468)
-    /// vector([ZZ.random_element(2**31) for t in range(16)])
-    #[test]
-    fn test_poseidon2_width_8_random() {
-        let mut input: [F; 8] = Goldilocks::new_array([
-            5116996373749832116,
-            8931548647907683339,
-            17132360229780760684,
-            11280040044015983889,
-            11957737519043010992,
-            15695650327991256125,
-            17604752143022812942,
-            543194415197607509,
-        ]);
-
-        let expected: [F; 8] = Goldilocks::new_array([
-            9817406215841052104,
-            16787690088272864961,
-            16566820001699848722,
-            7208405131694630795,
-            16315106302112132474,
-            15663526335160302273,
-            8171740919697725040,
-            7324539319521186184,
-        ]);
-
-        hl_poseidon2_goldilocks_width_8(&mut input);
-        assert_eq!(input, expected);
-    }
 
     #[test]
     fn test_generic_internal_linear_layer_8_matches_matmul_internal() {
