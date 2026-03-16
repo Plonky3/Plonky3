e70f23c73a6a378df1446b306811c6f808fc63e0
Thomas Coratger <60488569+tcoratger@users.noreply.github.com>
2026-03-16 22:59:32 +0100

    Update monty-31/src/x86_64_avx2/poseidon1.rs
    Co-authored-by: AngusG <angusgruen@gmail.com>

--- monty-31/src/x86_64_avx2/poseidon1.rs
+++ monty-31/src/x86_64_avx2/poseidon1.rs
@@ -164,9 +164,9 @@ where
             // PATH B (can overlap with S-box): partial dot product on s_hi.
             let s_hi: &[PackedMontyField31AVX2<FP>; 15] = unsafe { transmute(&split.s_hi) };
             let first_row = &self.packed_sparse_first_row[r];
-            let first_row_hi: [PackedMontyField31AVX2<FP>; 15] =
-                core::array::from_fn(|i| first_row[i + 1]);
-            let partial_dot = PackedMontyField31AVX2::<FP>::dot_product(s_hi, &first_row_hi);
+            let first_row_hi: &[PackedMontyField31AVX2<FP>; 15] =
+                &first_row[1..].try_into().unwrap();
+            let partial_dot = PackedMontyField31AVX2::<FP>::dot_product(s_hi, first_row_hi);
 
             // SERIAL: complete s0 and rank-1 update.
             let s0_val = split.s0;
