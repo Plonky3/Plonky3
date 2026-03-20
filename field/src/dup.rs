0000000081e476a21b86e9b3643c2dba0c61ec6f
Robin Salen <salenrobin@gmail.com>
2026-03-20 12:15:16 +0900

    doc: add a bit more for downstream users

--- field/src/dup.rs
+++ field/src/dup.rs
@@ -4,6 +4,11 @@
 ///  - non-[`Copy`] rings (e.g. symbolic expressions) should implement this with [`Clone::clone`]
 ///
 /// It defaults to a trivial copy for types that are both [`Copy`] and [`Clone`].
+/// 
+/// # Usage
+/// 
+/// It is recommended to rely on the [`Dup`] trait in downstream implementations for any type
+/// that is used in hot paths, such as trace cells in constraint evaluation for instance.
 pub trait Dup: Clone {
     fn dup(&self) -> Self;
 }
