d53f56cd0cae5e7b4ab7bb529c213dfd8ff4b7ba
Robin Salen <salenrobin@gmail.com>
2026-04-02 01:02:15 +0900

    chore: typo

--- goldilocks/src/goldilocks.rs
+++ goldilocks/src/goldilocks.rs
@@ -326,8 +326,8 @@ impl PrimeCharacteristicRing for Goldilocks {
     fn zero_vec(len: usize) -> Vec<Self> {
         // SAFETY:
         // Due to `#[repr(transparent)]`, Goldilocks and u64 have the same size, alignment
-        // and memory layout making `flatten_to_base` safe. This this will create
-        // a vector Goldilocks elements with value set to 0.
+        // and memory layout making `flatten_to_base` safe. This will create
+        // a vector of Goldilocks elements with value set to 0.
         unsafe { flatten_to_base(vec![0u64; len]) }
     }
 }
