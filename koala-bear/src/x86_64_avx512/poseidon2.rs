b9a9069a4997fced97b0945a538898c4727a294d
Irshad Kohl <irshadkohlirko@gmail.com>
2026-04-13 20:59:37 +0530

    Add property tests for x86 Poseidon2 width-32 (#1550)

--- koala-bear/src/x86_64_avx512/poseidon2.rs
+++ koala-bear/src/x86_64_avx512/poseidon2.rs
@@ -240,7 +240,9 @@ impl InternalLayerParametersAVX512<KoalaBearParameters, 32> for KoalaBearInterna
 
 #[cfg(test)]
 mod tests {
+    use p3_field::PrimeCharacteristicRing;
     use p3_symmetric::Permutation;
+    use proptest::prelude::*;
     use rand::rngs::SmallRng;
     use rand::{RngExt, SeedableRng};
 
@@ -249,6 +251,11 @@ mod tests {
     type F = KoalaBear;
     type Perm16 = Poseidon2KoalaBear<16>;
     type Perm24 = Poseidon2KoalaBear<24>;
+    type Perm32 = Poseidon2KoalaBear<32>;
+
+    fn arb_f() -> impl Strategy<Value = F> {
+        prop::num::u32::ANY.prop_map(F::from_u32)
+    }
 
     /// Test that the output is the same as the scalar version on a random input.
     #[test]
@@ -292,24 +299,24 @@ mod tests {
         assert_eq!(avx512_output, expected);
     }
 
-    /// Test that the output is the same as the scalar version on a random input.
-    #[test]
-    fn test_avx512_poseidon2_width_32() {
-        let mut rng = SmallRng::seed_from_u64(1);
+    // Test that the output is the same as the scalar version on random width-32 inputs.
+    proptest! {
+        #[test]
+        fn prop_avx512_poseidon2_width_32(input in prop::array::uniform32(arb_f())) {
+            let mut rng = SmallRng::seed_from_u64(1);
 
-        // Our Poseidon2 implementation.
-        let poseidon2 = Poseidon2KoalaBear::<32>::new_from_rng_128(&mut rng);
+            // Our Poseidon2 implementation.
+            let poseidon2 = Perm32::new_from_rng_128(&mut rng);
 
-        let input: [F; 32] = rng.random();
+            let mut expected = input;
+            poseidon2.permute_mut(&mut expected);
 
-        let mut expected = input;
-        poseidon2.permute_mut(&mut expected);
+            let mut avx512_input = input.map(Into::<PackedKoalaBearAVX512>::into);
+            poseidon2.permute_mut(&mut avx512_input);
 
-        let mut avx512_input = input.map(Into::<PackedKoalaBearAVX512>::into);
-        poseidon2.permute_mut(&mut avx512_input);
+            let avx512_output = avx512_input.map(|x| x.0[0]);
 
-        let avx512_output = avx512_input.map(|x| x.0[0]);
-
-        assert_eq!(avx512_output, expected);
+            prop_assert_eq!(avx512_output, expected);
+        }
     }
 }
