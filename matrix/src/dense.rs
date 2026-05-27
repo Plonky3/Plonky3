0a7d4bd7abdc1964cb91669d179ff2a988f50e6f
peter941221 <peter941221@gmail.com>
2026-05-27 10:06:29 +0800

    perf(matrix): specialize dense vertical packing

--- matrix/src/dense.rs
+++ matrix/src/dense.rs
@@ -519,6 +519,62 @@ impl<T: Clone + Send + Sync, S: DenseStorage<T>> Matrix<T> for DenseMatrix<T, S>
             (!sfx.is_empty()).then(|| P::from_fn(|i| sfx.get(i).cloned().unwrap_or_default())),
         )
     }
+
+    #[inline]
+    fn vertically_packed_row<P>(&self, r: usize) -> impl Iterator<Item = P>
+    where
+        T: Copy,
+        P: PackedValue<Value = T>,
+    {
+        let values = self.values.borrow();
+        let width = self.width;
+        let height = self.height();
+        let row = r % height;
+
+        (0..width).map(move |c| {
+            if P::WIDTH == 1 {
+                unsafe { P::broadcast(*values.get_unchecked(row * width + c)) }
+            } else {
+                P::from_fn(|i| unsafe { *values.get_unchecked(((row + i) % height) * width + c) })
+            }
+        })
+    }
+
+    #[inline]
+    fn vertically_packed_row_pair<P>(&self, r: usize, step: usize) -> Vec<P>
+    where
+        T: Copy,
+        P: PackedValue<Value = T>,
+    {
+        let values = self.values.borrow();
+        let width = self.width;
+        let height = self.height();
+        let row = r % height;
+        let next_row = (r + step) % height;
+
+        let mut out = Vec::with_capacity(width * 2);
+
+        if P::WIDTH == 1 {
+            out.extend(
+                (0..width).map(|c| unsafe { P::broadcast(*values.get_unchecked(row * width + c)) }),
+            );
+            out.extend(
+                (0..width)
+                    .map(|c| unsafe { P::broadcast(*values.get_unchecked(next_row * width + c)) }),
+            );
+        } else {
+            out.extend((0..width).map(|c| {
+                P::from_fn(|i| unsafe { *values.get_unchecked(((row + i) % height) * width + c) })
+            }));
+            out.extend((0..width).map(|c| {
+                P::from_fn(|i| unsafe {
+                    *values.get_unchecked(((next_row + i) % height) * width + c)
+                })
+            }));
+        }
+
+        out
+    }
 }
 
 impl<T: Clone + Default + Send + Sync> DenseMatrix<T> {
@@ -1711,6 +1767,26 @@ mod tests {
     }
 
     #[test]
+    fn test_vertically_packed_row_scalar_width_1() {
+        type Packed = BabyBear;
+
+        let matrix = RowMajorMatrix::new((1..17).map(BabyBear::new).collect::<Vec<_>>(), 4);
+        let packed = matrix
+            .vertically_packed_row::<Packed>(2)
+            .collect::<Vec<_>>();
+
+        assert_eq!(
+            packed,
+            vec![
+                BabyBear::new(9),
+                BabyBear::new(10),
+                BabyBear::new(11),
+                BabyBear::new(12),
+            ]
+        );
+    }
+
+    #[test]
     fn test_vertically_packed_row_pair() {
         type Packed = FieldArray<BabyBear, 2>;
 
@@ -1740,6 +1816,28 @@ mod tests {
                 .chain(9..13)
                 .map(|i| [BabyBear::new(i), BabyBear::new(i + 4)].into())
                 .collect::<Vec<_>>(),
+        );
+    }
+
+    #[test]
+    fn test_vertically_packed_row_pair_scalar_width_1() {
+        type Packed = BabyBear;
+
+        let matrix = RowMajorMatrix::new((1..17).map(BabyBear::new).collect::<Vec<_>>(), 4);
+        let packed = matrix.vertically_packed_row_pair::<Packed>(1, 2);
+
+        assert_eq!(
+            packed,
+            vec![
+                BabyBear::new(5),
+                BabyBear::new(6),
+                BabyBear::new(7),
+                BabyBear::new(8),
+                BabyBear::new(13),
+                BabyBear::new(14),
+                BabyBear::new(15),
+                BabyBear::new(16),
+            ]
         );
     }
 
